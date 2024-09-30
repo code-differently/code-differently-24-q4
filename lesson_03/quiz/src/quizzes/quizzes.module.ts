f3f93798153af1251a598f57032a5f589b6717f8
AmiyahJo <ajones244@gmail.com>
2024-09-30 20:17:29 +0000

    restart

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, AmiyahJonesQuiz];
+const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz];
 
 @Module({
   providers: [
