900bc88e1e8f6d0800e205916c6219b0c635d01a
AmiyahJo <ajones244@gmail.com>
2024-09-30 18:12:29 +0000

    modified test module and yami file

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { AmiyahJonesQuiz } from  './amiyah_jones_quiz.js'
 
 export const Quizzes = Symbol.for('Quizzes');
 
