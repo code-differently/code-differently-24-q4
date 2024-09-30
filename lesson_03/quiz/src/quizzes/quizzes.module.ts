0998e16b17da2bc0af57d53e0dc22e4e080812cf
Yafiaha <ynadbullah244@gmail.com>
2024-09-30 20:28:59 +0000

    Feat : Lesson-03 added yafiah_abdullah_quiz.ts

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
+
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz];
+const QUIZ_PROVIDERS = [AnthonyMaysQuiz,YafiahAbdullahQuiz,  AnotherQuiz];
 
 @Module({
   providers: [
