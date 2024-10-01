78f10ff0824aba5bd68f40ee8b779a7720c8c499
Dasiame <dmenglish244@gmail.com>
2024-10-01 14:20:39 +0000

    fix: duplicate declaration

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
-import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
 import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
+import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
-
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, DasiaEnglishQuiz];
 
 const QUIZ_PROVIDERS = [
   AnthonyMaysQuiz,
@@ -18,8 +16,8 @@ const QUIZ_PROVIDERS = [
   AnotherQuiz,
   JosephCaballeroQuiz,
   OyeyemiJimohQuiz,
+  DasiaEnglishQuiz,
 ];
-
 
 @Module({
   providers: [
