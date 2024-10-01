eb53f3acb51e6893e9908cef2003356e3240f0f9
haldanek <khaldane244@gmail.com>
2024-10-01 13:13:29 +0000

    fixed kimberlee lesson 03

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
-import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
-import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 import { KimberleeHaldaneQuiz } from './kimberlee_haldane_quiz.js';
+import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
+import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -16,7 +16,8 @@ const QUIZ_PROVIDERS = [
   AnotherQuiz,
   JosephCaballeroQuiz,
   OyeyemiJimohQuiz,
-, KimberleeHaldaneQuiz];
+  KimberleeHaldaneQuiz,
+];
 
 @Module({
   providers: [
