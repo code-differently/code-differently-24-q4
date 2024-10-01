6d36cd04dcdaec65fc213f1cd5ea4fa9bb29d3d0
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-09-30 21:18:55 -0400

    feat: added quiz file

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
-import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+//import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz];
+//const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, OyeyemiJimohQuiz];
+const QUIZ_PROVIDERS = [AnotherQuiz, OyeyemiJimohQuiz];
 
 @Module({
   providers: [
