3039ecdcb03c1cc04c2ac97d544be07cc98babbb
Dasia <dmenglish244@gmail.com>
2024-10-01 12:39:34 -0400

    feat: adds Dasia's quiz questions (#114)
    * feat: Dasia quizz questions
    
    * fix: duplicate declaration
    
    * Fix: capitalization
    
    * Forgot to delete a line
    
    * duplicate name

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
-import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
+import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 
@@ -17,6 +18,7 @@ const QUIZ_PROVIDERS = [
   AnotherQuiz,
   JosephCaballeroQuiz,
   OyeyemiJimohQuiz,
+  DasiaEnglishQuiz,
   ChigazoGrahamsQuiz,
   AmiyahJonesQuiz
 ];
