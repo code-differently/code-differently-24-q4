aa38a08e6455cdba2d26efbd7be8e638ab909040
A1-4U2T1NN <125928831+A1-4U2T1NN@users.noreply.github.com>
2024-10-01 10:46:16 -0400

    Lesson 03 Quiz w/Merge Conflicts Resolved (#106)
    * feat: added my lesson 03 quiz; updated quiz.yaml; modified quizzes.module.ts;
    
    * feat: added chigazo_graham_quiz; edited quizzes.module.ts and quiz.yaml;
    
    * fix: fixed merge conflicts;
    
    ---------
    
    Co-authored-by: “A1-4U2T1NN” <“a1.austinn19@gmail.com”>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 
@@ -15,7 +16,7 @@ const QUIZ_PROVIDERS = [
   AnotherQuiz,
   JosephCaballeroQuiz,
   OyeyemiJimohQuiz,
-];
+  ChigazoGrahamsQuiz];
 
 @Module({
   providers: [
