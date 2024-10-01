6b5e5cc45ec3001469fe7f7b658cf1fc3e27ee57
PabLim-Par <plimon244@gmail.com>
2024-10-01 14:54:14 +0000

    Provider names

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 import { AngelicaCQuiz } from './angelica_c_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
-import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
+import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
+import { PabloLimonParedesQuiz } from './pablo_limon_paredes_quiz.js';
+import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
-import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -23,7 +24,8 @@ const QUIZ_PROVIDERS = [
   DasiaEnglishQuiz,
   ChigazoGrahamsQuiz,
   AmiyahJonesQuiz,
-  XavierCruzQuiz
+  XavierCruzQuiz,
+  PabloLimonParedesQuiz,
 ];
 
 @Module({
