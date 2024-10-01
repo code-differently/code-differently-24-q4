aa65ed15f6ebd5de0b1eea920d60a6b03179f545
jimoye244 <oyeyemijimoh244@gmail.com>
2024-09-30 21:38:51 -0400

    feat: added Oyeyemi quiz (#103)

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
+import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
@@ -7,11 +8,13 @@ import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
+
 const QUIZ_PROVIDERS = [
   AnthonyMaysQuiz,
   YafiahAbdullahQuiz,
   AnotherQuiz,
   JosephCaballeroQuiz,
+  OyeyemiJimohQuiz,
 ];
 
 @Module({
