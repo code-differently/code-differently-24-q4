c565a4f583eee5ef8a44267502108ac62ea7f3a6
AmiyahJo <86239804+AmiyahJo@users.noreply.github.com>
2024-10-01 11:12:24 -0400

    feat: adds Amiyah Jones custom quiz (#108)
    * Create README.md
    
    Accidentally removed my work the 1st time by merging
    
    * Delete lesson_00/amiyahjones/README.md
    
    * Good as new!!
    
    * deleted the quiz feature installation in lesson_03 folder. meant to do that in quiz
    
    * test runs on fixing my issue
    
    * resubmitted encrypted answers
    
    * tests run works!!! I'm free
    
    * Forgot to put unanswered from my last commit
    
    * Good as new!!
    
    * deleted the quiz feature installation in lesson_03 folder. meant to do that in quiz
    
    * test runs on fixing my issue
    
    * resubmitted encrypted answers
    
    * tests run works!!! I'm free
    
    * Forgot to put unanswered from my last commit
    
    * fixed merge conflict
    
    ---------
    
    Co-authored-by: AmiyahJo <ajones244@gmail.com>

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -5,6 +5,7 @@ import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
+import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -16,7 +17,9 @@ const QUIZ_PROVIDERS = [
   AnotherQuiz,
   JosephCaballeroQuiz,
   OyeyemiJimohQuiz,
-  ChigazoGrahamsQuiz];
+  ChigazoGrahamsQuiz,
+  AmiyahJonesQuiz
+];
 
 @Module({
   providers: [
