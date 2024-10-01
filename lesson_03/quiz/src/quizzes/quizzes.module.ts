66a71bc06f039be2afcab05b1357440a8dbe994d
txtran224 <txtran224@gmail.com>
2024-10-01 20:07:27 +0000

    feat: create a Tommy_Tran_quiz.ts and added answers

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
+import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 import { AngelicaCQuiz } from './angelica_c_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
+import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
+import { TommyTranQuiz } from './Tommy_Tran_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
-import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -22,7 +23,8 @@ const QUIZ_PROVIDERS = [
   OyeyemiJimohQuiz,
   DasiaEnglishQuiz,
   ChigazoGrahamsQuiz,
-  AmiyahJonesQuiz
+  AmiyahJonesQuiz,
+  TommyTranQuiz,
 ];
 
 @Module({
