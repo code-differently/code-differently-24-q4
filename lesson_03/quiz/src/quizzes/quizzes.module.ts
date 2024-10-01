dd6e25c3fc54b4b579ae2da4bc77f321b9ec5fd6
PabLim-Par <plimon244@gmail.com>
2024-10-01 14:54:14 +0000

    Provider names

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
+import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
+import { PabloLimonParedesQuiz } from './pablo_limon_paredes_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
-import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
@@ -20,7 +21,8 @@ const QUIZ_PROVIDERS = [
   OyeyemiJimohQuiz,
   DasiaEnglishQuiz,
   ChigazoGrahamsQuiz,
-  AmiyahJonesQuiz
+  AmiyahJonesQuiz,
+  PabloLimonParedesQuiz,
 ];
 
 @Module({
