8c9ae604ed7076ff5e3ed82573b0c24d43c0cd58
Dasiame <dmenglish244@gmail.com>
2024-10-01 14:05:29 +0000

    feat: Dasia quizz questions

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
 
 // Add your quiz provider here.
-const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz];
+const QUIZ_PROVIDERS = [AnthonyMaysQuiz, AnotherQuiz, DasiaEnglishQuiz];
 
 @Module({
   providers: [
