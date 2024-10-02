0989aeb00f3eee36ef4ea5c3c0b65e700e27e5cb
zionbuch <zabuchanan244@gmail.com>
2024-10-02 14:05:10 +0000

    fixed duplicate errors

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -3,15 +3,13 @@ import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 import { AngelicaCQuiz } from './angelica_c_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { JamesCapparellQuiz } from './james_capparell_quiz.js';
-import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
-import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
 import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
+import { JamesCapparellQuiz } from './james_capparell_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
-import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
 import { ShawnDunsmoreQuiz } from './shawn_dunsmore_quiz.js';
+import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 import { ZionBuchananQuiz } from './zion_buchanan_quiz.js';
 
