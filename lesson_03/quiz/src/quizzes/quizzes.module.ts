4f302ee2111ec459ed933568e43f89e10e3291e9
marleomac3 <mlmcwilliams244@gmail.com>
2024-10-02 13:06:02 +0000

    removed duplicate variable instances

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -3,15 +3,12 @@ import { AmiyahJonesQuiz } from './amiyah_jones_quiz.js';
 import { AngelicaCQuiz } from './angelica_c_quiz.js';
 import { AnotherQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
-import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
-import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { ChigazoGrahamsQuiz } from './chigazo_graham_quiz.js';
 import { DasiaEnglishQuiz } from './dasia_english_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { LjMcwilliamsQuiz } from './lj_mcwilliams_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { XavierCruzQuiz } from './xavier_cruz_quiz.js';
-import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
 
 export const Quizzes = Symbol.for('Quizzes');
@@ -27,36 +24,8 @@ const QUIZ_PROVIDERS = [
   DasiaEnglishQuiz,
   ChigazoGrahamsQuiz,
   AmiyahJonesQuiz,
-  LjMcwilliamsQuiz,
-];
-
-@Module({
-  providers: [
-    ...QUIZ_PROVIDERS,
-    {
-      provide: Quizzes,
-      useFactory: (...args) => [...args],
-      inject: QUIZ_PROVIDERS,
-    },
-  ],
-})
-export class QuizzesModule {}
-
-export const Quizzes = Symbol.for('Quizzes');
-
-// Add your quiz provider here.
-const QUIZ_PROVIDERS = [
-  AnthonyMaysQuiz,
-  YafiahAbdullahQuiz,
-  AnotherQuiz,
-  JosephCaballeroQuiz,
-  AngelicaCQuiz,
-  OyeyemiJimohQuiz,
-  DasiaEnglishQuiz,
-  ChigazoGrahamsQuiz,
-  AmiyahJonesQuiz,
-  LjMcwilliamsQuiz,
   XavierCruzQuiz,
+  LjMcwilliamsQuiz,
 ];
 
 @Module({
