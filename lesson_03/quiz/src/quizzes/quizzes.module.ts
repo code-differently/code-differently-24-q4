a64dbd1f7a4615427d2675270da803a69f8d7b73
dxsmith244 <dxsmith244@gmail.com>
2024-10-02 13:28:32 +0000

    check failed

--- lesson_03/quiz/src/quizzes/quizzes.module.ts
+++ lesson_03/quiz/src/quizzes/quizzes.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { DavidSmithQuiz } from './another_quiz.js';
 import { AnthonyMaysQuiz } from './anthony_mays_quiz.js';
+import { DavidSmithQuiz } from './david_smith_quiz.js';
 import { JosephCaballeroQuiz } from './joseph_caballero_quiz.js';
 import { OyeyemiJimohQuiz } from './oyeyemi_jimoh_quiz.js';
 import { YafiahAbdullahQuiz } from './yafiah_abdullah_quiz.js';
