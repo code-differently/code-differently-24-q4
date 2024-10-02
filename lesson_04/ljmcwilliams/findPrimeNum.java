f7d66e02134d3d5eb25a3704a4fbaf5581b06a82
marleomac3 <mlmcwilliams244@gmail.com>
2024-10-02 20:28:59 +0000

    Initial commit of program files. Created java scanner. WIP

---
+++ lesson_04/ljmcwilliams/findPrimeNum.java
@@ -0,0 +1,7 @@
+import java.util.Scanner; //This allows us to use the scanner to take in user input
+public class findPrimeNum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a whole number");
+    }
+}
