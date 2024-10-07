e422bcece66cedf354d4d0ef2540bc6900d90db1
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2024-10-07 10:22:21 -0700

    feat: adds lesson_06 homework details and lesson_07 prework (#234)
    * feat: adds lesson_06 homework
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * docs: adds lesson_07 prework
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: moves interface to its own file
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: adds missing interface
    
    * feat: adds cli app
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: renaming files
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: adding lesson details
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    * chore: removes nodemon
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>
    
    ---------
    
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

---
+++ lesson_06/expression/src/expression_calculator.ts
@@ -0,0 +1,11 @@
+export class ExpressionCalculator {
+  /** Returns the calculation of ((a + b) * c) / d^e */
+  calculate(a: number, b: number, c: number, d: number, e: number): number {
+    // Implement your code here to return the correct value.
+    return 0;
+  }
+
+  pow(base: number, exponent: number): number {
+    return Math.pow(base, exponent);
+  }
+}
