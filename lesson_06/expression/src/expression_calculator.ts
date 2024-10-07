0b0d1fc09091df1343ab161897f2c99636f72811
Joseph <jxcaballero244@gmail.com>
2024-10-07 19:54:53 +0000

    modifies expression_calculator.ts

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,10 +1,21 @@
 export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+    const add = this.add(a, b);
+    const multiply = this.multiply(add, c);
+    const pow = this.pow(d, e);
+    const total = this.divide(multiply, pow);
+    return total;
   }
-
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    return a / b;
+  }
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
