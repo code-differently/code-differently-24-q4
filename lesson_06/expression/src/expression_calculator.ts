f2541cf0a6f822b09e88179ff0e81dad223ed9cf
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-07 19:26:28 -0400

    feat: added three functions to the expression calculator class

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,11 +1,28 @@
-export class ExpressionCalculator {
+
+export class ExpressionCalculator{
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const additionResult : number = this.add(a, b);
+    const multiplyResult : number = this.multiply(additionResult, c);
+    const exponentResult : number = this.pow(d, e);
+    const finalResult : number = this.divide(multiplyResult,exponentResult);
+    return finalResult;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
+
+  add(firstNumber: number, secondNumber: number): number{
+     return firstNumber + secondNumber;
+  }
+  multiply(firstNumber: number, secondNumber: number): number{
+     return firstNumber * secondNumber;
+  }
+
+  divide(firstNumber: number, secondNumber: number): number{
+     return firstNumber / secondNumber;
+  }
+
 }
