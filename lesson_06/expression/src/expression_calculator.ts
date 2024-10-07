6e4006c8e87f4a4e6ad54b35755d13eeb68a2136
AmiyahJo <ajones244@gmail.com>
2024-10-07 20:41:32 +0000

    thought proccess in comments

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,22 +2,16 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    function addHere(a:number,b:number){
-      const add = a + b;
-    };
 
-    function multiplyHere(addHere:number, c: number){
-       const multi = addHere * c; 
-    };
+    //PEMDAS order -> expected : //((a + b) * c) / d^e
 
-    function divideHere(d: number, e: number) {
-        const divi = multiplyHere / d^e;
-    };
- 
-    //For this assignment, you will need to implement the functions and logic required to calculate a mathematical expression.
-    // After implementing the add, divide, and multiply functions, you will combine these functions to compute the final result.
+    // Caculate addition with an add function '(a + b)'
 
-    return (addHere * multiplyHere) / divideHere;
+    // caculate multiplication '(add * c)' and  'd^e' so d * (e times)
+
+    // caculate division with divide function which is different variables of the multiplication values '(add * c)' and  'd^e' so d * (e times)
+
+    return 0;
   }
 
   pow(base: number, exponent: number): number {
