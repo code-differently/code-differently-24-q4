ec8261786c458712cb4243dba724f0370791676d
txtran224 <txtran224@gmail.com>
2024-10-07 20:23:55 +0000

    fix:lesson_06 added 3 additional functions that return the solution

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -3,11 +3,19 @@ export class ExpressionCalculator {
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
 
-    const add = a + b;
-    const multiply = add * c;
-    const divide = multiply / this.pow(d, e);
+    return this.divide(this.multiply(this.add(a, b), c), this.pow(d, e));
+  }
 
-    return divide;
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  multiply(add: number, c: number): number {
+    return add * c;
+  }
+
+  divide(multiply: number, d: number): number {
+    return multiply / d;
   }
 
   pow(base: number, exponent: number): number {
@@ -15,4 +23,7 @@ export class ExpressionCalculator {
   }
 }
 
-/**(a + b) * c) / this.pow(d, e) */
+/*      onst add = a + b;
+        const multiply = add * c;
+        const divide = multiply / this.pow(d, e);*/
+/**     (a + b) * c) / this.pow(d, e) */
