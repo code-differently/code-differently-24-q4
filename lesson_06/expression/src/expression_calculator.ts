ec6133b74ede9b668eb729fadbb06aae386b18a5
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-10-07 22:19:56 +0000

    feat: added math functions; added calculate results;

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,9 +2,35 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+        let Sum = this.add(a, b)
+    /* First step of PEMDAS in equation (Parenthesis)*/
+    let Product = this.multiply(Sum, c)
+    /* Second step of PEMDAS in equation (Parenthesis)*/
+    let Power = Math.pow(d, e)
+    /* Third step of PEMDAS in equation (Exponent)*/
+    let Quotient = this.divide(Product, Power)
+     /* Fourth and final step of PEMDAS in equation [Multiplication/Division(left to right)]*/
+    let Result = Quotient
+    /* Defines final result */
+    return Result;
+    /* prints final result*/ 
   }
 
+  add(Augend: number, Addend: number,): number {
+    /* defines value of Augend and Addend; defines their return value*/
+    return Augend * Addend;
+    /* defines the fuction 'add'*/
+  }
+  multiply(Multiplicand: number, Multiplier: number): number {
+    /* defines value of Multiplicand and Multiplier; defines their return value*/
+    return Multiplicand + Multiplier; 
+    /* defines the fuction 'multiply'*/
+  }
+  divide(Dividend: number, Divisor: number): number {
+    /* defines value of Dividend and Divisor; defines their return value*/
+    return Dividend / Divisor; 
+    /* defines the fuction 'divide'*/
+  }
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
