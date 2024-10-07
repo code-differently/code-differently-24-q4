056f84a8ce8baa995b0c22b04be16be258739e53
AmiyahJo <ajones244@gmail.com>
2024-10-07 20:33:44 +0000

    create seperate functions

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,17 +2,22 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    const add = caculateFormula(a ,b);
-    const expo = d^e;
-    const multi = add * c;
-    const divi = multi / expo;
+    function addHere(a:number,b:number){
+      const add = a + b;
+    };
 
-    const caculate = divi;
+    function multiplyHere(addHere:number, c: number){
+       const multi = addHere * c; 
+    };
 
+    function divideHere(d: number, e: number) {
+        const divi = multiplyHere / d^e;
+    };
+ 
     //For this assignment, you will need to implement the functions and logic required to calculate a mathematical expression.
     // After implementing the add, divide, and multiply functions, you will combine these functions to compute the final result.
 
-    return caculate;
+    return (addHere * multiplyHere) / divideHere;
   }
 
   pow(base: number, exponent: number): number {
