cf0121ec5a865575329cf8c9c57fc5b2df1ca47f
AmiyahJo <ajones244@gmail.com>
2024-10-07 20:24:30 +0000

    confused and testing

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,24 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const add = caculateFormula(a ,b);
+    const expo = d^e;
+    const multi = add * c;
+    const divi = multi / expo;
+
+    const caculate = divi;
+
+    //For this assignment, you will need to implement the functions and logic required to calculate a mathematical expression.
+    // After implementing the add, divide, and multiply functions, you will combine these functions to compute the final result.
+
+    return caculate;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
-  }
+  } 
 }
+
+//npm run compile = update code
+//npm start = check your code 
+//npm run check = check it;
