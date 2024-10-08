267c13a4ed124d46605e513dab970a3de610092b
jjcapparell <jjcapparell244@gmail.com>
2024-10-08 19:19:43 +0000

    added 3 variables in ExpressionCalculator to seperate function further

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,23 +1,23 @@
 export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
-    const divide = this.mult(this.add(a, b), c)/this.pow(d, e);
+    const adding = this.add(a,b);
+    const multiply = this.mult(adding,c);
+    const power = this.pow(d,e);
+    const divide = multiply/power;
     return divide;
   }
-  pow(base: number, exponent: number): number
-  {
+  pow(base: number, exponent: number): number {
     const power = Math.pow(base, exponent);
     return power;
   }
-  add(num1: number, num2: number): number
-  {
-    const adding = num1 + num2
+  add(num1: number, num2: number): number {
+    const adding = num1 + num2;
     return adding;
   }
-  mult(numA: number, numB: number): number
-  {
+  mult(numA: number, numB: number): number {
     const multiply = numA * numB;
-    return multiply
+    return multiply;
   }
 }
 //divide(multiply(add(a, b), c), pow(d, e))
