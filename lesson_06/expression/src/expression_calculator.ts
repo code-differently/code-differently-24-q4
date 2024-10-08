289f57e63dab102a429758019f29169d8d18fbdb
AmiyahJo <ajones244@gmail.com>
2024-10-08 13:40:25 +0000

    it works!!!

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -3,35 +3,34 @@ export class ExpressionCalculator {
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
 
-    //PEMDAS order -> expected : //((a + b) * c) / d^e
     // Caculate addition with an add function '(a + b)'
-      const originalSum =  this.add(a,b);
-    // caculate multiplication '(add * c)' and  'd^e' so d * (e times)
-      const MultiplyMySum = this.multiply(originalSum, c)
-    // caculate division with divide function which is different variables of the multiplication values '(add * c)' and  'd^e' so d * (e times)
-      const FinalResult = this.divide(MultiplyMySum, d ^ e);
-    // needs caculate something...
+    const originalSum = this.add(a, b);
+    // caculate multiplication '(add * c)'
+    const MultiplyMySum = this.multiply(originalSum, c);
+    //Specify the exponent 'd^e'
+    const myExponent = this.pow(d, e);
+    // caculate division 'multiply / exponent'
+    const DivideSum = this.divide(MultiplyMySum, myExponent);
+    // needs caculate it all...
+    const calculate = DivideSum;
+    return calculate;
 
-    return FinalResult;
   }
 
-  add(x: number , y: number){
+  add(x: number, y: number) {
     return x + y;
   }
 
-   multiply(x: number , y: number){
+  multiply(x: number, y: number) {
     return x * y;
   }
 
-   divide(x: number, y: number){
+  divide(x: number, y: number) {
     return x / y;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
-  } 
+  }
 }
 
-//npm run compile = update code
-//npm start = check your code 
-//npm run check = check it;
