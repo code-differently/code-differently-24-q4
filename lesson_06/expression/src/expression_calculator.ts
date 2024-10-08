b4e3360b2573d32fcd41ca41fd8edb9c8cf8e6a7
marleomac3 <mlmcwilliams244@gmail.com>
2024-10-08 13:47:03 +0000

    Initial commit, created a working function, trying to make it more efficient, WIP

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,21 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    return this.divide(this.multiply(this.add(a, b), c), this.pow(d, e));
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
+  add(addendA: number, addendB: number): number {
+    return addendA + addendB;
+  }
+  divide(dividend: number, divisor: number): number {
+    return dividend / divisor;
+  }
+  multiply(factorA: number, factorB: number): number {
+    return factorA * factorB;
+  }
 }
+
+//divide(multiply(add(a, b), c), pow(d, e))
