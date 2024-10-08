1626ef5042902f7ac49208f8f5f7c27da40e32ed
AmiyahJo <ajones244@gmail.com>
2024-10-08 12:40:02 +0000

    function working now

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -4,26 +4,26 @@ export class ExpressionCalculator {
     // Implement your code here to return the correct value.
 
     //PEMDAS order -> expected : //((a + b) * c) / d^e
-
     // Caculate addition with an add function '(a + b)'
       const originalSum =  you.add(a,b);
     // caculate multiplication '(add * c)' and  'd^e' so d * (e times)
       const multiply = you.multiply(originalSum, c)
     // caculate division with divide function which is different variables of the multiplication values '(add * c)' and  'd^e' so d * (e times)
       const lastOne = you.divide(multiply, d^e)
+      
     return 0;
   }
 
-function add(a: number , b: number){
-    return a + b;
+  add(x: number , y: number){
+    return x + y;
   }
 
-  function multiply(originalSum: number , c: number){
-    return originalSum * c;
+   multiply(x: number , y: number){
+    return x * y;
   }
 
-  function divide(a: number, b: number){
-    return a / b;
+   divide(x: number, y: number){
+    return x / y;
   }
 
   pow(base: number, exponent: number): number {
