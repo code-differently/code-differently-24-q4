4eb7d7d000b1a627eafdc0e0f96bddfed5ec9cb1
jjcapparell <jjcapparell244@gmail.com>
2024-10-08 13:55:39 +0000

    edited file in expression_calculator.ts to add 3 functions and a variable to solve expression in ExpressionCalculator

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,11 +1,20 @@
 export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+    const divide = this.mult(this.add(a, b), c)/this.pow(d, e);
+    return divide;
   }
-
-  pow(base: number, exponent: number): number {
+  pow(base: number, exponent: number): number
+  {
     return Math.pow(base, exponent);
   }
+  add(num1: number, num2: number): number
+  {
+    return (num1 + num2);
+  }
+  mult(numA: number, numB: number): number
+  {
+    return (numA*numB)
+  }
 }
+//divide(multiply(add(a, b), c), pow(d, e))
