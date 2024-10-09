21437558a55697bc4c47e57189e22761d2396506
angie-3 <acastilloruiz244@gmail.com>
2024-10-09 14:09:25 +0000

    fix:issues with return statements

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,7 +2,7 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    let divide = this.multiply(this.add(a, b), c) / this.pow(d, e)
+    const divide = this.multiply(this.add(a, b), c) / this.pow(d, e)
     return divide;
   }
 
@@ -10,19 +10,19 @@ export class ExpressionCalculator {
     return Math.pow(base, exponent);
   }
 
-  add(a: number, b: number){
-    let sum = a + b;
+   add(a: number, b: number): number {
+    const sum = a + b;
     return sum;
   }
-
-  divide(a: number, b: number){
-    let sum = a / b;
-    return sum;
+  
+  divide(a: number, b: number): number {
+    const num = a / b;
+    return num;
   }
 
   multiply(a: number, b: number){
-    let sum = a * b;
-    return sum;
+    const num = a * b;
+    return num;
   }
   
   }
