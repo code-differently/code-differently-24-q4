c1afafb14d6324d2de42934a840471f403b28930
“dlipscomb244” <dlipscomb244@gmail.com>
2024-10-09 14:19:38 +0000

    fix: readd work into correct file

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,10 +1,23 @@
 export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
-    // Implement your code here to return the correct value.
-    return 0;
+    const sum = this.add(a, b);
+    const product = this.multiply(sum, c);
+    const expo = this.pow(d, e);
+    const quotion = this.divide(product, expo);
+    return quotion;
   }
-
+  // Implement your code here to return the correct value.
+  // fuction called add a&b are numbers that will return a number
+  add(a: number, b: number): number {
+    return a + b;
+  }
+  multiply(a: number, b: number): number {
+    return a * b;
+  }
+  divide(a: number, b: number): number {
+    return a / b;
+  }
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
