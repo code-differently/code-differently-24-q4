28b0cd5cd5cc45031c3b4d755e5d6e2514417f3c
marleomac3 <mlmcwilliams244@gmail.com>
2024-10-09 13:43:28 +0000

    Recommitting Lesson 6, original branch made incorrectly

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -2,10 +2,25 @@ export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
     // Implement your code here to return the correct value.
-    return 0;
+    const sum = this.add(a, b);
+    const product = this.multiply(sum, c);
+    const exp = this.pow(d, e);
+    const quotient = this.divide(product, exp);
+    return quotient;
   }
 
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
+  add(addendA: number, addendB: number): number {
+    return addendA + addendB;
+  }
+  divide(dividend: number, divisor: number): number {
+    return dividend / divisor;
+  }
+  multiply(factorA: number, factorB: number): number {
+    return factorA * factorB;
+  }
 }
+
+//divide(multiply(add(a, b), c), pow(d, e))
