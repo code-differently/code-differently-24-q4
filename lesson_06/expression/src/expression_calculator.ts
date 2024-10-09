10cd918e1fce43fa58285690d5b350ea01a4f974
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2024-10-08 21:51:23 -0700

    Revert "feat: adds Pablo's Lesson06 expression calculator (#252)" (#255)
    This reverts commit 8518c24b4ae5d91dc586fce733a278a347a3df55.

--- lesson_06/expression/src/expression_calculator.ts
+++ lesson_06/expression/src/expression_calculator.ts
@@ -1,23 +1,10 @@
 export class ExpressionCalculator {
   /** Returns the calculation of ((a + b) * c) / d^e */
   calculate(a: number, b: number, c: number, d: number, e: number): number {
-    const addition = this.add(a, b);
-    const multi = this.multiply(addition, c);
-    const exponent = this.pow(d, e);
-    const division = this.divide(multi, exponent);
-    const mytotal = division;
-    return mytotal;
+    // Implement your code here to return the correct value.
+    return 0;
   }
-  // Implement your code here to return the correct value.
-  add(a: number, b: number): number {
-    return a + b;
-  }
-  multiply(a: number, b: number): number {
-    return a * b;
-  }
-  divide(a: number, b: number): number {
-    return a / b;
-  }
+
   pow(base: number, exponent: number): number {
     return Math.pow(base, exponent);
   }
