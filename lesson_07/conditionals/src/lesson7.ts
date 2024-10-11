58193ddb0e173bb27e50615524b93f7433fd0e32
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-10-11 20:40:52 +0000

    fix: corrected A to A+ scale;

--- lesson_07/conditionals/src/lesson7.ts
+++ lesson_07/conditionals/src/lesson7.ts
@@ -48,7 +48,7 @@ export function compareStrings(a: string, b: string): number {
  */
 export function convertGpaToLetterGrade(gpa: number): string {
   
-  if(gpa >= 4.0){
+  if(gpa > 4.0){
     return "A+"
   }else if(gpa >= 3.9){
      return "A"
