87887b1da08a338b0a9f448b48aa606983bdafce
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-10-12 23:24:00 +0000

    fix: added additional parameters in 'else-if' statements to correct errors;

--- lesson_07/conditionals/src/lesson7.ts
+++ lesson_07/conditionals/src/lesson7.ts
@@ -48,25 +48,25 @@ export function compareStrings(a: string, b: string): number {
  */
 export function convertGpaToLetterGrade(gpa: number): string {
   
-  if(gpa > 3.9){
+  if(gpa > 4.0){
     return "A"
-  }else if(gpa >= 3.3){
+  }else if(gpa >= 3.7 && gpa < 4.0) {
      return "A-"
-  }else if(gpa >= 3.0){
+  }else if(gpa >= 3.3 && gpa < 3.7){
      return "B+"
-  }else if(gpa >= 2.7){
+  }else if(gpa >= 3 && gpa < 3.3){
      return "B"
-  }else if(gpa >= 2.3){
+  }else if(gpa >= 2.7 && gpa < 3){
       return "B-"
-  }else if(gpa >= 2.0){
+  }else if(gpa >= 2.3 && gpa < 3.7){
      return "C+"
-  }else if(gpa >= 1.7){  
+  }else if(gpa >= 2 && gpa < 2.3){  
     return "C"
-  }else if(gpa >= 1.3){
+  }else if(gpa >= 1.7 && gpa < 2){
     return "C-";
-  }else if(gpa >= 1.0){
+  }else if(gpa >= 1.3 && gpa < 1.7){
     return "D+";
-  }else if(gpa >= 0.7){
+  }else if(gpa >= 1.0 && gpa < 1.3){
     return "D";
   }else{
     return "F"
