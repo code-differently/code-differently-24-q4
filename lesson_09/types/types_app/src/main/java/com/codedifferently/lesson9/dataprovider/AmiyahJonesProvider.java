5b451ba4ab8b3328353acee241e7b1a703fbefd1
AmiyahJo <ajones244@gmail.com>
2024-10-15 12:40:00 +0000

    fix: example code added feat: changed provider name to my own (using my name)

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/AmiyahJonesProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/AmiyahJonesProvider.java
@@ -1,46 +1,23 @@
 package com.codedifferently.lesson9.dataprovider;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
-public abstract class AmiyahJonesProvider {
+import org.springframework.stereotype.Service;
 
-  public abstract String getProviderName();
-
-  public abstract Map<String, Class> getColumnTypeByName();
-
-  /**
-   * Given a list of data objects, returns the list with values converted to the appropriate type.
-   *
-   * @param data A list of data objects containing values keyed by column name.
-   * @return A new list with object values converted to the appropriate type.
-   */
-  public List<Map<String, Object>> parseData(List<Map<String, String>> data) throws Exception {
-    Map<String, Class> columnTypeByName = getColumnTypeByName();
-    return data.stream().map(row -> parseRow(columnTypeByName, row)).collect(Collectors.toList());
+@Service
+public class AmiyahJonesProvider extends DataProvider {
+  public String getProviderName() {
+    return "amiyahjones";
   }
 
-  /**
-   * Parses a single row of data using the provided column types mapping.
-   *
-   * @param columnTypeByName A map containing the type of a column keyed by its name.
-   * @param row A bag of values keyed by column name.
-   * @return A new list of data values converted to the appropriate type.
-   */
-  private Map<String, Object> parseRow(
-      Map<String, Class> columnTypeByName, Map<String, String> row) {
-    var parsedRow = new java.util.HashMap<String, Object>();
-    row.forEach(
-        (key, value) -> {
-          Class type = columnTypeByName.get(key);
-          try {
-            parsedRow.put(key, type.getConstructor(String.class).newInstance(value));
-          } catch (Exception e) {
-            throw new RuntimeException(
-                "Error parsing data for " + key + " as type " + type.getName());
-          }
-        });
-    return parsedRow;
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Integer.class,
+        "column2", String.class,
+        "column3", Boolean.class,
+        "column4", Float.class,
+        "column5", Double.class,
+        "column6", Long.class,
+        "column7", Short.class);
   }
 }
