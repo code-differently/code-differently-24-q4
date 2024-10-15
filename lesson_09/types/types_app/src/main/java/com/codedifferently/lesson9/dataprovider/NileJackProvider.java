74a28d70ed13de08a894ba440d7ba10586739fdb
nilejack <njacksonnorthrup244@gmail.com>
2024-10-15 13:08:24 +0000

    Feat:adds/NileJack.ProviderFileHW

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/NileJackProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/NileJackProvider.java
@@ -1,22 +1,23 @@
 package com.codedifferently.lesson9.dataprovider;
 
 import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class AnthonyMaysProvider extends DataProvider {
+public class NileJackProvider extends DataProvider {
   public String getProviderName() {
-    return "anthonymays";
+    return "nilejack";
   }
 
   public Map<String, Class> getColumnTypeByName() {
     return Map.of(
-        "column1", Integer.class,
-        "column2", String.class,
-        "column3", Boolean.class,
-        "column4", Float.class,
-        "column5", Double.class,
-        "column6", Long.class,
-        "column7", Short.class);
+        "column1", Float.class,
+        "column2", Integer.class,
+        "column3", Short.class,
+        "column4", String.class,
+        "column5", Boolean.class,
+        "column6", Double.class,
+        "column7", Long.class);
   }
 }
