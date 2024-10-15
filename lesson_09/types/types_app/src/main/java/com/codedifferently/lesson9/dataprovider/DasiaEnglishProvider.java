056db02ab858c1f8b4d63cf191e728ae00a68808
Dasiame <dmenglish244@gmail.com>
2024-10-15 13:54:53 +0000

    feat: Dasias data type answers for homeowrk lesson_09

---
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/DasiaEnglishProvider.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.codedifferently.lesson9.dataprovider.DataProvider;
+
+@Service
+public class DasiaEnglishProvider extends DataProvider {
+  public String getProviderName() {
+    return "dasiaenglish";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Short.class,
+        "column2", Boolean.class,
+        "column3", Double.class,
+        "column4", Float.class,
+        "column5", Integer.class,
+        "column6", String.class,
+        "column7", Long.class);
+  }
+}
