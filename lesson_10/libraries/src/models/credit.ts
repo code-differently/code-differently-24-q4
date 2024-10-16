78805f97680f143d23f400d857486090f254acac
Anthony D. Mays <anthonydmays@users.noreply.github.com>
2024-10-16 12:31:04 -0700

    chore: changes credit model to proper class (#330)
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_10/libraries/src/models/credit.ts
+++ lesson_10/libraries/src/models/credit.ts
@@ -1,7 +1,13 @@
 import { Role } from './role.js';
 
-export interface Credit {
+export class Credit {
   mediaItemId: string;
   name: string;
   role: Role;
+
+  constructor(mediaItemId: string, name: string, role: Role) {
+    this.mediaItemId = mediaItemId;
+    this.name = name;
+    this.role = role;
+  }
 }
