8f59e8dbb31a930e148a3a7ff6cba9274f74a9d5
haldanek <khaldane244@gmail.com>
2024-10-17 00:44:40 +0000

    feat: adds created file for Kimberlee's Lesson_10 Loading the media collection

---
+++ lesson_10/libraries/src/loaders/kimberlee_haldane_loader.ts
@@ -0,0 +1,38 @@
+import csv from 'csv-parser';
+import fs from 'fs';
+import { Credit, MediaItem } from '../models/index.js';
+import { Loader } from './loader.js';
+
+export class KimberleeHaldaneLoader implements Loader {
+  getLoaderName(): string {
+    return 'kimberleehaldane';
+  }
+
+  async loadData(): Promise<MediaItem[]> {
+    const credits = await this.loadCredits();
+    const mediaItems = await this.loadMediaItems();
+
+    console.log(
+      `Loaded ${credits.length} credits and ${mediaItems.length} media items`,
+    );
+
+    return [...mediaItems.values()];
+  }
+
+  async loadMediaItems(): Promise<MediaItem[]> {
+    // TODO: Implement this method.
+    return [];
+  }
+
+  async loadCredits(): Promise<Credit[]> {
+    const credits = [];
+    const readable = fs
+      .createReadStream('data/credits.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { media_item_id, role, name } = row;
+      credits.push(new Credit(media_item_id, name, role));
+    }
+    return credits;
+  }
+}
