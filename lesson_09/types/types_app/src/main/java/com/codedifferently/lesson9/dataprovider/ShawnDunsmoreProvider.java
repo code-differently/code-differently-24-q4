433b7ed31c140b4d3f6720793060cc74693e7d73
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-10-17 12:56:26 +0000

    Feat: Adds Shawn Dunsmore Lesson09 Upadted File.

--- lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/ShawnDunsmoreProvider.java
+++ lesson_09/types/types_app/src/main/java/com/codedifferently/lesson9/dataprovider/ShawnDunsmoreProvider.java
@@ -1,1 +1,25 @@
+package main.java.com.codedifferently.lesson9.dataprovider;
 
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.codedifferently.lesson9.dataprovider.DataProvider;
+
+@Service
+public class ShawnDunsmoreProvider extends DataProvider {
+  public String getProviderName() {
+    return "shawmdunsmore";
+  }
+
+  public Map<String, Class> getColumnTypeByName() {
+    return Map.of(
+        "column1", Float.class,
+        "column2", Integer.class,
+        "column3", Float.class,
+        "column4", Integer.class,
+        "column5", String.class,
+        "column6", Integer.class,
+        "column7", Boolean.class);
+  }
+}
