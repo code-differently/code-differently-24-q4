be348c1121f93071b1adaf009ba325159f0b3f42
haldanek <khaldane244@gmail.com>
2024-10-17 19:55:41 +0000

    feat: added Kimberlee as a loader

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
-import { XavierCruzLoader } from './xavier_cruz_loader.js';
+import { KimberleeHaldaneLoader } from './kimberlee_haldane_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader, XavierCruzLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, KimberleeHaldaneLoader];
 
 @Module({
   providers: [
