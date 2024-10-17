2163ca5cfb7ed50b1bc41bf0747862b116519fe0
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-10-17 17:29:12 +0000

    Feat: Adds Shawn Dunsmore Lesson010, Included Loader.

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
-
+import { ShawnDunsmoreLoader } from './shawn_dunsmore_loader.js';
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, ShawnDunsmoreLoader];
 
 @Module({
   providers: [
