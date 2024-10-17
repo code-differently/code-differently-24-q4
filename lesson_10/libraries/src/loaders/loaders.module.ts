d537950035f6da0b9c8ed3246e56d8171deb92bf
Angelica Castillo <acastilloruiz244@gmail.com>
2024-10-17 17:55:35 -0400

    feat: adds Angie's lesson 10 csv loader (#345)
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { NileJacksonLoader } from './nile_jackson_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
+
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+  AngelicaCastilloLoader,
   XavierCruzLoader,
   NileJacksonLoader,
 ];
