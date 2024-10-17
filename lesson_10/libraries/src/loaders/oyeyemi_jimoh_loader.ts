ad0956ac5e0551adf2d704e1fda6f01900249d3b
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-17 13:35:40 +0000

    feat: Adding homework10 - load media item implementation - Yemi

---
+++ lesson_10/libraries/src/loaders/oyeyemi_jimoh_loader.ts
@@ -0,0 +1,47 @@
+import csv from 'csv-parser';
+import fs from 'fs';
+import { Credit, MediaItem } from '../models/index.js';
+import { Loader } from './loader.js';
+
+export class OyeyemiJimohLoader implements Loader {
+  getLoaderName(): string {
+    return 'oyeyemijimoh';
+  }
+
+  async loadData(): Promise<MediaItem[]> {
+    const credits = await this.loadCredits();
+    const mediaItems = await this.loadMediaItems();
+
+    console.log(
+      `Loaded ${credits.length} credits and ${mediaItems.length} media items`,
+    );
+
+    return [...mediaItems.values()];
+  }
+
+  async loadMediaItems(): Promise<MediaItem[]> {
+    // TODO: Implement this method.
+    const mediaItems = [];
+    //const credits[] = this.loadCredits();
+    const readable = fs
+      .createReadStream('data/media_items.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { id, type, title, year } = row;
+      mediaItems.push(new MediaItem(id, title, type, year, []));
+    }
+    return mediaItems;
+  }
+
+  async loadCredits(): Promise<Credit[]> {
+    const credits = [];
+    const readable = fs
+      .createReadStream('data/credits.csv', 'utf-8')
+      .pipe(csv());
+    for await (const row of readable) {
+      const { media_item_id, role, name } = row;
+      credits.push(new Credit(media_item_id, name, role));
+    }
+    return credits;
+  }
+}
