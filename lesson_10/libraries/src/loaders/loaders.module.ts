4adab3cdac0fc36be3ab72952e01a5f1766ef0f4
marleomac3 <mlmcwilliams244@gmail.com>
2024-10-17 20:47:41 +0000

    feat: solved merge conflict and generated loader file

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { LjMcwilliamsLoader } from './lj_mcwilliams_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader, XavierCruzLoader];
+const LOADER_PROVIDERS = [
+  AnthonyMaysLoader,
+  XavierCruzLoader,
+  LjMcwilliamsLoader,
+];
 
 @Module({
   providers: [
