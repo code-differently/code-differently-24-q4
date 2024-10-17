d362d428d7afb189010ada3914122e39f3af1b71
angie-3 <acastilloruiz244@gmail.com>
2024-10-17 19:57:51 +0000

    feat:homework:lesson 10

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
+import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader, XavierCruzLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, XavierCruzLoader, AngelicaCastilloLoader];
 
 @Module({
   providers: [
