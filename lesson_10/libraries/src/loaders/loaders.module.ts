1a1c339a9f570d8078d94e45a8d7d3b0edffe2dd
AmiyahJo <ajones244@gmail.com>
2024-10-17 18:29:41 +0000

    fix: added quiz provider

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
+import { AmiyahJonesLoader } from './amiyah_jones.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader, XavierCruzLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, XavierCruzLoader, AmiyahJonesLoader];
 
 @Module({
   providers: [
