3e8bd384d521db9968aa74612e159ebd32a925bc
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-10-17 18:53:19 +0000

    feat: added chigazo_graham_loader.ts file with completed media loader; added Loader Provider to loader.module.ts;

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { ChigazoGrahamLoader } from './chigazo_graham_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, ChigazoGrahamLoader];
 
 @Module({
   providers: [
