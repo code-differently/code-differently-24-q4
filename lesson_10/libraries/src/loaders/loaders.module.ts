d6f8a0b19a1ab8e9655b7b7530e2d5473fa79d44
txtran224 <txtran224@gmail.com>
2024-10-18 04:07:45 +0000

    feat:lesson 10 added Tommy Tran loader solution

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -3,8 +3,8 @@ import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
 import { JamesCapparellLoader } from './james_capparell_loader.js';
 import { NileJacksonLoader } from './nile_jackson_loader.js';
+import { TommyTranLoader } from './tommy_tran_loarder.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
-
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
@@ -14,6 +14,7 @@ const LOADER_PROVIDERS = [
   JamesCapparellLoader,
   NileJacksonLoader,
   XavierCruzLoader,
+  TommyTranLoader,
 ];
 
 @Module({
