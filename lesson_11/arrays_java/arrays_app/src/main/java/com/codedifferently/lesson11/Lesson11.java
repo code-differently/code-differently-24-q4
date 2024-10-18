349f96094df899b8264a95e68da671c3d61da8d3
AmiyahJo <ajones244@gmail.com>
2024-10-18 21:05:52 +0000

    fix: instructions provided by github to run a gradle check
    used the ./gradlew :arrays_app:spotlessApply as reccommended.

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -6,26 +6,26 @@ import java.util.List;
 public class Lesson11 {
 
   public int[] getConcatenation(int[] nums) {
-    int n  = nums.length;
+    int n = nums.length;
 
-       int[] ans = new int[2 * n];
+    int[] ans = new int[2 * n];
 
-       for(int i = 0; i < n; i++){
-        ans[i] = nums[i];
-        ans[i + n] = nums[i];
-       }
-       
-       return ans;
+    for (int i = 0; i < n; i++) {
+      ans[i] = nums[i];
+      ans[i + n] = nums[i];
+    }
+
+    return ans;
   }
 
   public List<Integer> findWordsContaining(String[] words, char x) {
     List<Integer> indices = new ArrayList<>();
 
-        for (int i = 0; i < words.length; i++){
-            if(words[i].indexOf(x) >= 0){
-                indices.add(i);
-            }
-        }
-        return indices;
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) >= 0) {
+        indices.add(i);
+      }
+    }
+    return indices;
   }
 }
