7b2605bf7f51c8cc82c2d548e5b2ac710de7705d
dxsmith244 <dxsmith244@gmail.com>
2024-10-18 13:35:08 +0000

    feat: adds david_smith_loader.ts from lesson_10 homework

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { DavidSmithLoader } from './david_smith_loader.js';
 import { JamesCapparellLoader } from './james_capparell_loader.js';
 import { NileJacksonLoader } from './nile_jackson_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
@@ -14,6 +15,7 @@ const LOADER_PROVIDERS = [
   JamesCapparellLoader,
   NileJacksonLoader,
   XavierCruzLoader,
+  DavidSmithLoader,
 ];
 
 @Module({
