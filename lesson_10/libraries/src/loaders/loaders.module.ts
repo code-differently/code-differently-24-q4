f22a87d5ba3aebae80cc712f40ffd903ced5692d
cdbluejr <cdblue244@gmail.com>
2024-10-18 15:54:22 +0000

    feat: lesson 10 Extra Credit by Dwight Blue

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { DwightBlueLoader } from './dwight_blue_loader.js';
 import { JamesCapparellLoader } from './james_capparell_loader.js';
 import { NileJacksonLoader } from './nile_jackson_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
@@ -10,10 +11,13 @@ export const Loaders = Symbol.for('Loaders');
 // Add your quiz provider here.
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
+
   AngelicaCastilloLoader,
   JamesCapparellLoader,
   NileJacksonLoader,
   XavierCruzLoader,
+  DwightBlueLoader,
+  ,
 ];
 
 @Module({
