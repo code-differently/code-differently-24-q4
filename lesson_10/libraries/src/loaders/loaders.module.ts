d0e62ba4c02ec48aa9d53100fb54739b52e3c015
haldanek <khaldane244@gmail.com>
2024-10-18 01:15:52 +0000

    chore: fixed loader module

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { JamesCapparellLoader } from './james_capparell_loader.js';
 import { KimberleeHaldaneLoader } from './kimberlee_haldane_loader.js';
+import { NileJacksonLoader } from './nile_jackson_loader.js';
+import { XavierCruzLoader } from './xavier_cruz_loader.js';
 
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader, KimberleeHaldaneLoader];
+
+const LOADER_PROVIDERS = [
+  AnthonyMaysLoader,
+  AngelicaCastilloLoader,
+  JamesCapparellLoader,
+  NileJacksonLoader,
+  XavierCruzLoader,
+  KimberleeHaldaneLoader,
+];
 
 @Module({
   providers: [
