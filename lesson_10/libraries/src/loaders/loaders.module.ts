883e8757c3ad50d54e906d03a59f35c82721e3cb
zionbuch <zabuchanan244@gmail.com>
2024-10-18 14:19:26 +0000

    feat: adds Zion's loader file for lesson_10

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
-
+import { ZionBuchananLoader } from './zion_buchanan_loader.js';
 export const Loaders = Symbol.for('Loaders');
 
 // Add your quiz provider here.
-const LOADER_PROVIDERS = [AnthonyMaysLoader];
+const LOADER_PROVIDERS = [AnthonyMaysLoader, ZionBuchananLoader];
 
 @Module({
   providers: [
