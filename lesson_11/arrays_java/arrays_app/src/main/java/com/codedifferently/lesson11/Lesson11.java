10d76bc8e8ef06b33976709b7da1f6d4d4a21b31
Joseph <jxcaballero244@gmail.com>
2024-10-18 20:51:01 +0000

    fix: fixes failing check by applying gradlew spotless apply to arrays_app -Joseph Caballero

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -10,26 +10,27 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    int[] ans = new int[(nums.length + nums.length)] ; 
-      System.arraycopy(nums, 0, ans, 0, nums.length);
-      System.arraycopy(nums, 0, ans, nums.length, nums.length);
-        return ans;  
-      }
+    int[] ans = new int[(nums.length + nums.length)];
+    System.arraycopy(nums, 0, ans, 0, nums.length);
+    System.arraycopy(nums, 0, ans, nums.length, nums.length);
+    return ans;
+  }
 
   /**
    * Provide the solution to LeetCode 2942 here:
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-        List<Integer> num = new ArrayList<>();
-        for(var i = 0; i<words.length; i++){
-            var word = String.valueOf(words[i]);
-            for( var j = 0; j < word.length(); j++){
-            if(word.contains(String.valueOf(x))){
-                num.add(i);
-                break;
-            }
-            }
+    List<Integer> num = new ArrayList<>();
+    for (var i = 0; i < words.length; i++) {
+      var word = String.valueOf(words[i]);
+      for (var j = 0; j < word.length(); j++) {
+        if (word.contains(String.valueOf(x))) {
+          num.add(i);
+          break;
         }
-        return num;  }
+      }
+    }
+    return num;
+  }
 }
