b726523c1c8444881c0ae2bbf9f2f4889d901e57
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-18 21:53:30 +0000

    feat: adds brute-force implementation of leetcode 1929 and 2942 for homework11 by Yemi

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,22 +1,46 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
-
+ 
   /**
-   * Provide the solution to LeetCode 1929 here:
+   * Implementation of the solution to LeetCode 1929 below:
    * https://leetcode.com/problems/concatenation-of-array
    */
-  public int[] getConcatenation(int[] nums) {
-    return null;
+  public static int[] getConcatenation(int[] nums) {
+    int lenOriginalArray = nums.length;
+    int[] concatenatedArrays = new int[lenOriginalArray * 2];
+     
+    for(int i= 0; i < lenOriginalArray; i++){
+      concatenatedArrays[i] = nums[i];
+    }
+    int nextIndex = lenOriginalArray;
+    for(int i= 0; i < lenOriginalArray; i++){
+      concatenatedArrays[nextIndex+i] = nums[i];
+    }
+    return concatenatedArrays;
   }
 
   /**
-   * Provide the solution to LeetCode 2942 here:
+   * Implementation of the solution to LeetCode 2942 below:
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    var indexList = new ArrayList<Integer>();
+    int index = -1;
+    for(String word : words){
+      index = index + 1;
+      var charactersOfWord = new ArrayList<Character>();
+      char charactersOfCurrentWord[] = word.toCharArray();
+      for(Character xter : charactersOfCurrentWord){
+         charactersOfWord.add(xter);
+      }
+      if (charactersOfWord.contains(x)){
+        indexList.add(index);
+      }
+    }
+    return indexList;
   }
 }
