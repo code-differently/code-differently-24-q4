6e709a2d2de25ebcb64a9fa3b96a04d9a6fd00b9
AmiyahJo <ajones244@gmail.com>
2024-10-18 20:33:39 +0000

    feat: adds answer to leetcode 1929
    copy & paste solution made from leetcode where I originally submitted

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -9,7 +9,16 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int n  = nums.length;
+
+       int[] ans = new int[2 * n];
+
+       for(int i = 0; i < n; i++){
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+       }
+       
+       return ans;
   }
 
   /**
