575e48fb1c70fb1e2145a6d3bc140de2a87bec9b
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-19 20:32:59 +0000

    feat: Fewer code line implementations for lesson11

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -4,40 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
+
   public int[] getConcatenation(int[] nums) {
-    if (nums.length == 0) {
-      return new int[] {};
-    }
     int lenOriginalArray = nums.length;
     int[] concatenatedArrays = new int[lenOriginalArray * 2];
+    int pos = 0;
 
-    for (int i = 0; i < lenOriginalArray; i++) {
-      concatenatedArrays[i] = nums[i];
-    }
-    int nextIndex = lenOriginalArray;
-    for (int i = 0; i < lenOriginalArray; i++) {
-      concatenatedArrays[nextIndex + i] = nums[i];
-    }
+    System.arraycopy(nums, pos, concatenatedArrays, pos, lenOriginalArray);
+    System.arraycopy(nums, pos, concatenatedArrays, lenOriginalArray, lenOriginalArray);
     return concatenatedArrays;
   }
 
   public List<Integer> findWordsContaining(String[] words, char x) {
-    var indexList = new ArrayList<Integer>();
-    if (words.length == 0) {
-      return indexList;
-    }
-
-    int index = -1;
+    var indexList = new ArrayList<Integer>(0);
+    int index = 0;
     for (String word : words) {
-      index = index + 1;
-      var charactersOfWord = new ArrayList<Character>();
-      char charactersOfCurrentWord[] = word.toCharArray();
-      for (Character xter : charactersOfCurrentWord) {
-        charactersOfWord.add(xter);
-      }
-      if (charactersOfWord.contains(x)) {
+      if (word.indexOf(x) != -1) {
         indexList.add(index);
       }
+      index = index + 1;
     }
     return indexList;
   }
