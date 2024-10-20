be4fc985aab7c0b567f66f58bb4900af7a52365d
nilejack <njacksonnorthrup244@gmail.com>
2024-10-20 01:15:58 +0000

    fix

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -9,15 +9,14 @@ public class Lesson11 {
    * Provide the solution to LeetCode 1929 here:
    * https://leetcode.com/problems/concatenation-of-array
    */
-    public int[] getConcatenation(int[] nums) {
-      int answer[] = new int[2 * nums.length];
-      for (int i = 0; i < nums.length; i++) {
-        answer[i] = nums[i];
-        answer[i + nums.length] = nums[i];
-      }
-      return answer;
+  public int[] getConcatenation(int[] nums) {
+    int answer[] = new int[2 * nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      answer[i] = nums[i];
+      answer[i + nums.length] = nums[i];
     }
-  
+    return answer;
+  }
 
   /**
    * Provide the solution to LeetCode 2942 here:
