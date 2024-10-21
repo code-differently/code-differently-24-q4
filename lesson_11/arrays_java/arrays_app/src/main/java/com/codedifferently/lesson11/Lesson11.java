695b63540787689a43633b62be01bf8a61efe67c
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-10-21 14:12:25 +0000

    Feat: adds Shawn Dunsmore Lesson 11 SpotlessApply.

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,4 +1,5 @@
 package com.codedifferently.lesson11;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,32 +10,30 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-        int n = nums.length; 
-        int[] ans = new int[2 * n];
-        
-        
-        for (int i = 0; i < n; i++) {
-            ans[i] = nums[i];         
-            ans[i + n] = nums[i];     
-        }
-      
-        return ans; 
-}
+    int n = nums.length;
+    int[] ans = new int[2 * n];
 
+    for (int i = 0; i < n; i++) {
+      ans[i] = nums[i];
+      ans[i + n] = nums[i];
+    }
+
+    return ans;
+  }
 
   /**
    * Provide the solution to LeetCode 2942 here:
    * https://leetcode.com/problems/find-words-containing-character/
    */
-   public List<Integer> findWordsContaining(String[] words, char x) {
-        List<Integer> indices = new ArrayList<>(); 
+  public List<Integer> findWordsContaining(String[] words, char x) {
+    List<Integer> indices = new ArrayList<>();
 
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].indexOf(x) != -1) {
-                indices.add(i); 
-            }
-        }
-
-        return indices; 
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
+        indices.add(i);
+      }
     }
+
+    return indices;
+  }
 }
