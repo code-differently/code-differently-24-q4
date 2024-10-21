c50f263d40f6f592b4c9e88f94e6cdaa55ebd64c
Dasiame <dmenglish244@gmail.com>
2024-10-21 16:02:36 +0000

    feat: add Dasias leet code answers to lesson11 in java

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,15 +1,20 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
-
   /**
    * Provide the solution to LeetCode 1929 here:
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int[] answer = new int[2 * nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      answer[i] = nums[i];
+      answer[i + nums.length] = nums[i];
+    }
+    return answer;
   }
 
   /**
@@ -17,6 +22,14 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    List<Integer> answer = new ArrayList<>();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].contains(Character.toString(x))) {
+        answer.add(i);
+      }
+    }
+    return answer;
   }
 }
+// I got help from Joesphs to talk things out and explain what this code is doing and how to go
+// about applying code and what does it mean
