cd61a61d5c097fb1835ddbed58b3ce14b3c7aa31
angie-3 <acastilloruiz244@gmail.com>
2024-10-21 12:17:37 +0000

    feat:add solutions on leetcode.

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,5 +1,6 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
@@ -9,14 +10,28 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    int ans[] = new int[2 * nums.length];
+
+    System.arraycopy(nums, 0, ans, 0, nums.length);
+    System.arraycopy(nums, 0, ans, nums.length, nums.length);
+    return ans;
   }
 
+  // worked on the solutions with Pablo, Joseph, & Nile via jitsi. //
   /**
    * Provide the solution to LeetCode 2942 here:
    * https://leetcode.com/problems/find-words-containing-character/
    */
-  public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+  class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+      List<Integer> indices = new ArrayList<>();
+      for (int i = 0; i < words.length; i++) {
+        String str = String.valueOf(x);
+        if (words[i].contains(str)) {
+          indices.add(i);
+        }
+      }
+      return indices;
+    }
   }
 }
