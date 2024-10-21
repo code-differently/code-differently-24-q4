08fb8932d0967431f51e330b78a461b73af6a32f
Yafiaha <ynadbullah244@gmail.com>
2024-10-21 14:52:49 +0000

    Feat: Yafiah Lesson_11 extra credit TS solutions

--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -3,7 +3,13 @@
  * https://leetcode.com/problems/concatenation-of-array
  */
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+  const n = nums.length;
+  const ans: number[] = new Array(2 * n);
+  for (let i = 0; i < n; i++) {
+    ans[i] = nums[i];
+    ans[n + i] = nums[i];
+  }
+  return ans;
 }
 
 /**
@@ -11,5 +17,15 @@ export function getConcatenation(nums: number[]): number[] {
  * https://leetcode.com/problems/find-words-containing-character/
  */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+  const list: number[] = [];
+  for (let i = 0; i < words.length; i++) {
+    const str = words[i];
+    for (let j = 0; j < str.length; j++) {
+      if (str.charAt(j) === x) {
+        list.push(i);
+        break;
+      }
+    }
+  }
+  return list;
 }
