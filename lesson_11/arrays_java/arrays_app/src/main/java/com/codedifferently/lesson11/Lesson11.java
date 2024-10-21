7efa6a89401eb7e0dc72b003497b0923a22d56f5
htanweer244 <htanweer244@gmail.com>
2024-10-21 21:27:43 +0000

    Feat: Fixed commits, gradle and modified java lesson 11  by Hummad Tanweer

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,5 +1,6 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
@@ -9,7 +10,15 @@ public class Lesson11 {
    * https://leetcode.com/problems/concatenation-of-array
    */
   public int[] getConcatenation(int[] nums) {
-    return null;
+    //  int len = nums.size();
+    // return null;
+    int n = nums.length;
+    int ans[] = new int[2 * n];
+    for (int i = 0; i < n; i++) {
+      ans[i] = nums[i];
+      ans[i + n] = nums[i];
+    }
+    return ans;
   }
 
   /**
@@ -17,6 +26,13 @@ public class Lesson11 {
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+
+    List result = new ArrayList();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
+    }
+    return result;
   }
 }
