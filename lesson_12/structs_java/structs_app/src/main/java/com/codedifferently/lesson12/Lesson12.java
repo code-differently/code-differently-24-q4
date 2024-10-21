672ce73fe4d33608b1952b79aeda95eca91c92f8
AmiyahJo <ajones244@gmail.com>
2024-10-21 20:44:33 +0000

    feat: adds comments
    Breaking down the objective explained in a way for me to understand

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -8,5 +8,21 @@ public class Lesson12 {
    */
   public String gameResult(ListNode head) {
     return null;
+    //make a linked list 
+
+    //create two variables to count points for "Odd" and "Even"
+
+    // Check each pair of nodes (odd and even):
+      // compare their values
+
+      // add a point to "Odd" if the odd-indexed node is higher 
+
+      //add a point to "Even" if the even-indexed node is higher 
+
+    // Decide the winner 
+      // compare the points after going through both pairs
+      
+      // Return the name of the winning team or "Tie" if points are equal
+    
   }
 }
