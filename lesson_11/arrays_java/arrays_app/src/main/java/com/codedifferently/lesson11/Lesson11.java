7f4802ec8096ab724946ddb3350c0671df7c1fca
jjcapparell <jjcapparell244@gmail.com>
2024-10-21 15:06:04 +0000

    chore: gradle fix for Lesson11.java and change to lesson11.ts

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -5,25 +5,23 @@ import java.util.List;
 
 public class Lesson11 {
 
-  
   public int[] getConcatenation(int[] nums) {
-    int[] answer = new int[2*nums.length];
+    int[] answer = new int[2 * nums.length];
 
-    for(int i=0;i<nums.length;i++) {
+    for (int i = 0; i < nums.length; i++) {
       answer[i] = nums[i];
-      answer[i+nums.length] = nums[i];
+      answer[i + nums.length] = nums[i];
     }
     return answer;
   }
-  
+
   public List<Integer> findWordsContaining(String[] words, char x) {
     List answer = new ArrayList();
-    for(int i=0;i<words.length;i++){
-      if (words[i].indexOf(x)!=-1){
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
         answer.add(i);
       }
     }
     return answer;
   }
 }
-
