925fadf6b9ce8c39e145033560104f375a480c14
jjcapparell <jjcapparell244@gmail.com>
2024-10-21 14:26:40 +0000

    feat: Edited lesson11.ts Lesson11.java to complete functions/methods

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,22 +1,29 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
 
-  /**
-   * Provide the solution to LeetCode 1929 here:
-   * https://leetcode.com/problems/concatenation-of-array
-   */
+  
   public int[] getConcatenation(int[] nums) {
-    return null;
-  }
+    int[] answer = new int[2*nums.length];
 
-  /**
-   * Provide the solution to LeetCode 2942 here:
-   * https://leetcode.com/problems/find-words-containing-character/
-   */
+    for(int i=0;i<nums.length;i++) {
+      answer[i] = nums[i];
+      answer[i+nums.length] = nums[i];
+    }
+    return answer;
+  }
+  
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
+    List answer = new ArrayList();
+    for(int i=0;i<words.length;i++){
+      if (words[i].indexOf(x)!=-1){
+        answer.add(i);
+      }
+    }
+    return answer;
   }
 }
+
--- lesson_11/arrays_ts/src/lesson11.ts
+++ lesson_11/arrays_ts/src/lesson11.ts
@@ -1,15 +1,20 @@
-/**
- * Provide the solution to LeetCode 1929 here:
- * https://leetcode.com/problems/concatenation-of-array
- */
+
 export function getConcatenation(nums: number[]): number[] {
-  return [];
+   const answer = [2*nums.length];
+
+   for (let i=0;i<nums.length;i++){
+    answer[i]=nums[i];
+    answer[i+nums.length]=nums[i];
+   }
+   return answer;
 }
 
-/**
- * Provide the solution to LeetCode 2942 here:
- * https://leetcode.com/problems/find-words-containing-character/
- */
 export function findWordsContaining(words: string[], x: string): number[] {
-  return [];
+  let answer = [];
+  for(let i=0;i<words.length;i++){
+    if (words[i].indexOf(x)!=-1){
+      answer.push(i);
+    }
+  }
+  return answer;
 }
