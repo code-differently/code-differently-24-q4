bf6dee494ff0b54aeaac7d7f646fd921e6b34883
XavierCruz5106 <superguyman121@gmail.com>
2024-10-21 20:49:17 +0000

    feat: added Stack impl/ LeetCode 3062 finished in Java

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,25 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int e_pts = 0;
+    int o_pts = 0;
+
+    ListNode curr = head;
+
+    while (curr != null) {
+      ListNode next = curr.next;
+      if (curr.val > next.val) {
+        e_pts++;
+      } else if (curr.val < next.val) {
+        o_pts++;
+      }
+      curr = next.next;
+    }
+
+    if (e_pts == o_pts) {
+      return "Tie";
+    }
+
+    return e_pts > o_pts ? "Even" : "Odd";
   }
 }
