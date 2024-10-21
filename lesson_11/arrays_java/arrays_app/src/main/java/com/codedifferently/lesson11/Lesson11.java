9cc18218ab7ba352820d7777ee89b3a3e455a12e
htanweer244 <htanweer244@gmail.com>
2024-10-21 18:06:38 +0000

    Feat: Modified lesson 11 java by Hummad Tanweer

--- lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
+++ lesson_11/arrays_java/arrays_app/src/main/java/com/codedifferently/lesson11/Lesson11.java
@@ -1,5 +1,6 @@
 package com.codedifferently.lesson11;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lesson11 {
@@ -8,15 +9,33 @@ public class Lesson11 {
    * Provide the solution to LeetCode 1929 here:
    * https://leetcode.com/problems/concatenation-of-array
    */
-  public int[] getConcatenation(int[] nums) {
-    return null;
-  }
+  public 
+    int[] getConcatenation(int[] nums) {
+    //  int len = nums.size();
+    //return null;
+     int n = nums.length;
+    int ans[] = new int[2 * n];
+    for (int i = 0; i < n; i++)  {
+      ans[i] = nums[i];
+      ans[i+n]= nums[i];
+    } return ans;
+    }
+
 
   /**
    * Provide the solution to LeetCode 2942 here:
    * https://leetcode.com/problems/find-words-containing-character/
    */
   public List<Integer> findWordsContaining(String[] words, char x) {
-    return null;
-  }
+
+    List result = new ArrayList();
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].indexOf(x) != -1) {
+        result.add(i);
+      }
+    }
+    return result;
+        }
 }
+
+
