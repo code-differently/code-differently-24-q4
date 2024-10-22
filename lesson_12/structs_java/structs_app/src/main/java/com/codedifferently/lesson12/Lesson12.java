511e12170f0e37135808293cea1870f181c4d616
PabLim-Par <plimon244@gmail.com>
2024-10-22 23:02:58 +0000

    Feat: adds Pablo Limon-Paredes Lesson 12 Java game result linked list, and class Stack methods

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,23 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    ListNode current = head;
+    int evenSide = 0;
+    int oddSide = 0;
+    while (current != null && current.next != null) {
+      if (current.val > current.next.val) {
+        evenSide++;
+      } else if (current.val < current.next.val) {
+        oddSide++;
+      }
+      current = current.next.next;
+    }
+    if (evenSide > oddSide) {
+      return "Even";
+    } else if (evenSide < oddSide) {
+      return "Odd";
+    } else {
+      return "Tie";
+    }
   }
 }
--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -9,18 +9,21 @@ public class Stack {
   }
 
   public void push(int value) {
-    // Your code here
+    ListNode tophat = new ListNode(value);
+    tophat.next = top;
+    top = tophat;
   }
 
   public int pop() {
-    return 0;
+    top = top.next;
+    return top.val;
   }
 
   public int peek() {
-    return 0;
+    return top.val;
   }
 
   public boolean isEmpty() {
-    return true;
+    return top == null;
   }
 }
