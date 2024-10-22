c0545ad66dd7259785cadce211a95ce33f5cf787
AmiyahJo <ajones244@gmail.com>
2024-10-22 12:51:36 +0000

    feat: adds if else statement on whether one of the teams win or if its a tie

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson12;
 
+import java.util.Queue;
+
 public class Lesson12 {
 
   /**
@@ -7,7 +9,6 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
     //make a linked list 
     Queue<Integer> queue = new LinkedList<Integer>();
     
@@ -24,10 +25,15 @@ public class Lesson12 {
       //add a point to "Even" if the even-indexed node is higher 
 
     // Decide the winner 
-      // compare the points after going through both pairs
-
-      // Return the name of the winning team or "Tie" if points are equal
-    
+      // compare the points and choose the winner or tie
+      if (Odd > Even) {
+        return("Team odd wins!");
+      } else if (Even > Odd){
+        return("Team Even wins");
+      } else {
+        return ("It's a tie");
+      }
+      
   }
 }
 
