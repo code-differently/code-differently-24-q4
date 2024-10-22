59bfb07176e115e6b90cdda57e55b4297417420a
AmiyahJo <ajones244@gmail.com>
2024-10-22 13:25:40 +0000

    chore: remove comments in lesson12.java file

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -2,21 +2,14 @@ package com.codedifferently.lesson12;
 
 public class Lesson12 {
 
-  /**
-   * Provide the solution to LeetCode 3062 here:
-   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
-   */
   public String gameResult(ListNode head) {
-    //create two variables to count points for "Odd" and "Even"
+
     int oddPoints = 0;
     int evenPoints = 0;
 
-    // Loop through the linked list to get pairs (so two nodes at a time)
     ListNode current = head;
-    // Check each pair of nodes (odd and even):
-      // compare their values
-      while (current != null && current.next != null) {
-      // add a point to "Even" if the odd-indexed node is higher and vice versa 
+
+      while (current != null && current.next != null) { 
         if (current.val > current.next.val){
           evenPoints++;
         } else if (current.val < current.next.val){
@@ -24,8 +17,7 @@ public class Lesson12 {
         }
         current = current.next.next;
       }
-    // Decide the winner 
-      // compare the points and choose the winner or tie
+
       if (oddPoints > evenPoints) {
         return "Odd";
       } else if (evenPoints > oddPoints){
