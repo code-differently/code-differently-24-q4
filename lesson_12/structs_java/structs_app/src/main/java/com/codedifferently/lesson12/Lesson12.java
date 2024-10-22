eedad00508651589f4547c8b16b3fa94727ff065
AmiyahJo <ajones244@gmail.com>
2024-10-22 13:17:09 +0000

    feat: adds code to solve the problem

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -1,7 +1,5 @@
 package com.codedifferently.lesson12;
 
-import java.util.Queue;
-
 public class Lesson12 {
 
   /**
@@ -9,26 +7,31 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    //make a linked list 
-    Queue<Integer> queue = new LinkedList<Integer>();
-    
     //create two variables to count points for "Odd" and "Even"
-    int Odd = 0;
-    int Even = 0;
-    // Loop through the linked list to get pairs (so two nodes at a time)
+    int oddPoints = 0;
+    int evenPoints = 0;
 
+    // Loop through the linked list to get pairs (so two nodes at a time)
+    ListNode current = head;
     // Check each pair of nodes (odd and even):
       // compare their values
+      while (current != null && current.next != null) {
+      // add a point to "Even" if the odd-indexed node is higher 
+        if (current.val < current.next.val){
+          evenPoints++;
+        }
+      //add a point to "Odd" if the even-indexed node is higher 
+      else if (current.val > current.next.val){
+          oddPoints++;
+        }
 
-      // add a point to "Odd" if the odd-indexed node is higher 
-
-      //add a point to "Even" if the even-indexed node is higher 
-
+        current = current.next.next;
+      }
     // Decide the winner 
       // compare the points and choose the winner or tie
-      if (Odd > Even) {
+      if (oddPoints > evenPoints) {
         return("Team odd wins!");
-      } else if (Even > Odd){
+      } else if (evenPoints > oddPoints){
         return("Team Even wins");
       } else {
         return ("It's a tie");
@@ -40,5 +43,3 @@ public class Lesson12 {
 // Constraints:
 // The number of nodes in the list is in the range [2, 100]. 
 // The number of nodes in the list is even. 1 <= Node.val <= 100 
-// The value of each odd-indexed node is odd. 
-// The value of each even-indexed node is even.
