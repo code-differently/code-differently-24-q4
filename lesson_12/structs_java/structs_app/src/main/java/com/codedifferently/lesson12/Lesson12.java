9c63e5fa6ab6998f3a126762a7d36f95deaa58ff
Cogobonnia <244cogbonnia@gmail.com>
2024-10-22 14:47:02 +0000

    feat: added gameResult and Stack methods for Chelsea

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,26 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int oddPoints = 0;
+    int evenPoints = 0;
+
+    ListNode current = head;
+
+    while (current != null && current.next != null) {
+      if (current.val > current.next.val) {
+        evenPoints++;
+      } else {
+        oddPoints++;
+      }
+      current = current.next.next;
+    }
+
+    if (oddPoints > evenPoints) {
+      return "Odd";
+    } else if (evenPoints > oddPoints) {
+      return "Even";
+    } else {
+      return "Tie";
+    }
   }
 }
