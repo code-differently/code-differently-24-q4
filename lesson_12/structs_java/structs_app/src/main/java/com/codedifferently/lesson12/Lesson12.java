e9c5deb8328a389158e34bdeb482e0f5e6bd95e8
Yafiaha <ynadbullah244@gmail.com>
2024-10-22 20:24:19 +0000

    Feat : Yafiah Lesson-12 Game Result LeetCode & Implement Stack.java

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,24 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+    int even = 0;
+    int odd = 0;
+
+    while(head != null && head.next != null){
+      if (head.val > head.next.val) {
+        even++; 
+      } else if(head.val < head.next.val) {
+        odd++;  
+      }
+      head = head.next.next;
+    }
+      
+    if(even > odd){
+       return "Even";
+    }
+    else if(even < odd ){
+       return "Odd";
+    }
+    return "Tie";
   }
 }
--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -9,18 +9,20 @@ public class Stack {
   }
 
   public void push(int value) {
-    // Your code here
+    top = new ListNode(value, top); 
   }
 
   public int pop() {
-    return 0;
+   int value = top.val;
+   top = top.next;
+   return value;
   }
 
   public int peek() {
-    return 0;
+    return top.val;
   }
 
   public boolean isEmpty() {
-    return true;
+    return top == null;
   }
 }
