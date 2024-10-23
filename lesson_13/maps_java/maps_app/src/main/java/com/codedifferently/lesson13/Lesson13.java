1b08d0fc27cc1c6e969b3bf9b86780c3463d7f5d
cdbluejr <cdblue244@gmail.com>
2024-10-23 20:51:05 +0000

    feat: Lesson 13 Java by Dwight Blue

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -7,6 +7,17 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    var sum = 0;
+    char[] charArray1 = s.toCharArray();
+    char[] charArray2 = t.toCharArray();
+
+    for (int i = 0; i < s.length(); i++) {
+      for (int j = 0; j < t.length(); j++) {
+        if (charArray1[i] == charArray2[j]) {
+          sum = sum + Math.abs(i - j);
+        }
+      }
+    }
+    return sum;
   }
 }
