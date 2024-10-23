7ca9a499b0ed227280a5b5466e5b759af4769855
AmiyahJo <ajones244@gmail.com>
2024-10-23 14:01:25 +0000

    vhore: remove .java implementation comments of stack.ts

--- lesson_12/structs_ts/src/stack.ts
+++ lesson_12/structs_ts/src/stack.ts
@@ -8,22 +8,12 @@ export class Stack {
   }
 
   push(value: number): void {
-    // ListNode newNode = new ListNode(value);
     const newNode = new ListNode(value)
-    // newNode.next = top;
     newNode.next = this.top;
-    // top = newNode;
     this.top = newNode;
   }
 
   pop(): number | undefined {
-    // if (this.isEmpty()) {
-    //   throw new EmptyStackException();
-    // } else {
-    //   int value = top.val;
-    //   top = top.next;
-    //   return value;
-    // }
     if (this.isEmpty()) {
       return undefined;
     }
@@ -34,10 +24,6 @@ export class Stack {
   }
 
   peek(): number | null {
-    // if (this.isEmpty()) {
-      // throw new EmptyStackException();
-    // }
-    // return top.val;
     if (this.isEmpty()) {
       return undefined;
     } 
