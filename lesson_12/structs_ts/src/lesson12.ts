d3ce414925bf45729a714565e3fcb7018265f949
jjcapparell <jjcapparell244@gmail.com>
2024-10-23 15:34:42 +0000

    chore: removed unneeded methods

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -34,32 +34,4 @@ export class Lesson12 {
       return 'Tie';
     }
   }
-
-    
-
-//   getElementAt(head: ListNode | undefined, position: number): ListNode | undefined {
-//     let current = head;
-//     let index = 0;
-
-//     while (current !== undefined) {
-//       if (index === position) {
-//         return current; // Return the node at the specified position
-//       }
-//       current = current.next; // Move to the next node
-//       index++; // Increment the index
-//     }
-
-//     return undefined; // Return null if the position is out of bounds
-//   }
-
-//   getLength(head: ListNode | undefined): number {
-//     let length = 0;
-//     let current = head;
-
-//     while (current !== undefined) {
-//       length++; // Increment to keep count through each element
-//       current = current.next; // Move on to the next element
-//     }
-//     return length;
-//   }
 }
