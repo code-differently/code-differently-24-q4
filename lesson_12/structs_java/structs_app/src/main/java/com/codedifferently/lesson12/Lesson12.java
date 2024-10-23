cc3510f9308c16b1efed802802e79c0f5d836491
zionbuch <zabuchanan244@gmail.com>
2024-10-23 15:08:32 +0000

    feat: added Zion's GameResult and Stack Results to lesson_12 hw

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,29 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
+    ListNode current = head;
+    int even = 0;
+    int odd = 0;
+    while (current.next != null) {
+      ListNode nextNode = current.next;
+      if (current.val < nextNode.val) {
+        odd++;
+      }
+      if (current.val > nextNode.val) {
+        even++;
+      }
+      current = nextNode.next;
+    }
+    if (even > odd) {
+      return "Even";
+    }
+    if (even < odd) {
+      return "Odd";
+    }
+
+    if (even == odd) {
+      return "Tie";
+    }
     return null;
   }
 }
