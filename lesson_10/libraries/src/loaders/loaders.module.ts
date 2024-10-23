86decd101ffb431923547adfc6246103667082c8
cdbluejr <cdblue244@gmail.com>
2024-10-22 20:05:42 -0400

    feat: adds csv loader for Dwight (#362)
    * feat: lesson 10 Extra Credit by Dwight Blue
    
    * feat: loaders module file By Dwight Blue
    
    * Update loaders.module.ts
    
    ---------
    
    Co-authored-by: Anthony D. Mays <anthonydmays@users.noreply.github.com>

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { DwightBlueLoader } from './dwight_blue_loader.js';
 import { JamesCapparellLoader } from './james_capparell_loader.js';
 import { NileJacksonLoader } from './nile_jackson_loader.js';
 import { XavierCruzLoader } from './xavier_cruz_loader.js';
@@ -11,6 +12,7 @@ export const Loaders = Symbol.for('Loaders');
 const LOADER_PROVIDERS = [
   AnthonyMaysLoader,
   AngelicaCastilloLoader,
+  DwightBlueLoader,
   JamesCapparellLoader,
   NileJacksonLoader,
   XavierCruzLoader,
