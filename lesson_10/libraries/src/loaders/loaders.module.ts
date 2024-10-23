7a30a9af2b581cb9ddee0f9258bdf57daf0169fc
Anthony D. Mays <anthony@morganlatimer.com>
2024-10-23 00:31:43 +0000

    fix: correcting bad merge
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_10/libraries/src/loaders/loaders.module.ts
+++ lesson_10/libraries/src/loaders/loaders.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AngelicaCastilloLoader } from './angelica_castillo_loader.js';
 import { AnthonyMaysLoader } from './anthony_mays_loader.js';
+import { DavidSmithLoader } from './david_smith_loader.js';
 import { DwightBlueLoader } from './dwight_blue_loader.js';
 import { JamesCapparellLoader } from './james_capparell_loader.js';
 import { NileJacksonLoader } from './nile_jackson_loader.js';
