c5d795cb9c86faf7fb1ad5bd7c3bf10a09ca7263
AmiyahJo <ajones244@gmail.com>
2024-10-23 20:43:35 +0000

    chore: remove comments

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -4,10 +4,6 @@ import java.util.HashMap;
 
 public class Lesson13 {
 
-  /**
-   * Provide the solution to LeetCode 3146 here:
-   * https://leetcode.com/problems/permutation-difference-between-two-strings
-   */
   public int findPermutationDifference(String s, String t) {
     var charIndexMap = new HashMap<Character, Integer>();
 
@@ -15,14 +11,11 @@ public class Lesson13 {
 
     for (int sIndex = 0; sIndex < s.length(); sIndex++) {
         charIndexMap.put(s.charAt(sIndex), sIndex);
-        //keeps track of the position for each character in 's'
     }
 
     for (int tIndex = 0; tIndex < t.length(); tIndex++) {
       int indexInS = charIndexMap.get(t.charAt(tIndex)); 
-      //Same as sIndex , but this loop keeps track of each character in t
       totalDifference += Math.abs(indexInS - tIndex); 
-      //adds all the differences 
     }
      
     return totalDifference;
--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,26 +1,19 @@
-/**
- * Provide the solution to LeetCode 3146 here:
- * https://leetcode.com/problems/permutation-difference-between-two-strings
- */
 export function findPermutationDifference(s: string, t: string): number {
-  //Create a way to find the index of each character in string s.
-  // This could be a list or a dictionary where each character points to its index in s.
     let charPositionMap = new Map<string, number>();
-  //Start with a variable to keep track of the total permutation difference. Set it to 0.
+  
     let totalDifference = 0;
-  //For each character in t, find its index in s using the mapping you created.
+  
     for (let sIndex = 0; sIndex < s.length; sIndex++) {
       charPositionMap.set(s.charAt(sIndex), sIndex)
     }
       
     for (let tIndex = 0; tIndex < t.length; tIndex++) {
-  //Calculate the absolute difference between the index in s and the current index in t.
       let indexInS = charPositionMap.get(t.charAt(tIndex));
-    //Add the absolute difference you calculated to the total difference.
+    
       if (indexInS != undefined) {
         totalDifference += Math.abs(indexInS - tIndex);
       }
     }
-  //After going through all characters in t, return the total difference.
+  
     return totalDifference;
 }
