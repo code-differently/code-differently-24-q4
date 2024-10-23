d598b705f8eb2e7067366b023a0738ec0c25f601
AmiyahJo <ajones244@gmail.com>
2024-10-23 13:40:19 +0000

    feat: adds while loop using my current pointer

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -7,17 +7,16 @@ export class Lesson12 {
    */
   public gameResult(head: ListNode | null): string {
     let [oddPoints, evenPoints] = [0,0];
-
     let current = head;
    
-    // while(current != null && current.next != null) {
-    //   if (current.val > current.next.val) {
-    //     evenPoints++;
-    //   } else if (current.val < current.next.val) {
-    //     oddPoints++;
-    //   }
-    //    current = current.next.next;
-    // }
+   while (current != null && current.next != null) {
+      if (current.val > current.next.val) {
+        evenPoints++;
+      } else if (current.val < current.next.val) {
+        oddPoints++;
+      }
+      current = current.next ? current.next.next || null : null; 
+    }
 
     if (oddPoints > evenPoints) {
       return "Odd";
