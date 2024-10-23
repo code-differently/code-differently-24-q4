e234bf5c9176ff6b1de8c1e1d151e33cfd616366
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-23 21:10:26 +0000

    feat: handling spotless issue by Yemi

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -4,9 +4,9 @@ public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
     int result = 0;
     int strLen = s.length();
-    for(int i= 0; i < strLen ; i++){
-        char ch = s.charAt(i);
-        result = result + ( Math.abs(s.indexOf(ch) - t.indexOf(ch)));
+    for (int i = 0; i < strLen; i++) {
+      char ch = s.charAt(i);
+      result = result + (Math.abs(s.indexOf(ch) - t.indexOf(ch)));
     }
     return result;
   }
