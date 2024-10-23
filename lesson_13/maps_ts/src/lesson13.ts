f4d679cc0abb3d10ea8beeb435b3cbcabf7d0576
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-23 21:26:43 +0000

    feat: Added typescript implementation of lesson13 by Yemi

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,11 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+    let result = 0;
+    const strLen: number = s.length;
+    for (let i = 0; i < strLen; i++) {
+        const ch: string = s.charAt(i);
+        result = result + Math.abs(s.indexOf(ch) - t.indexOf(ch));
+    }
+    return result;
 }
