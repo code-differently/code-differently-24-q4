47f79a3b68b6c6c2c0b8a71ebea4d18a99ce7807
AmiyahJo <ajones244@gmail.com>
2024-10-23 14:06:23 +0000

    chore: remove lesson12.ts comments

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -1,10 +1,7 @@
 import { ListNode } from './list_node.js';
 
 export class Lesson12 {
-  /**
-   * Provide the solution to LeetCode 3062 here:
-   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
-   */
+  
   public gameResult(head: ListNode | null): string {
     let [oddPoints, evenPoints] = [0,0];
     let current = head;
