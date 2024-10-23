84e285ad0f38558c57fb2fb91e4a82412cf716c0
angie-3 <acastilloruiz244@gmail.com>
2024-10-23 11:50:40 +0000

    feat: fixed changes

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -2,10 +2,7 @@ package com.codedifferently.lesson12;
 
 public class Lesson12 {
 
-  /**
-   * Provide the solution to LeetCode 3062 here:
-   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
-   */
+   
   public String gameResult(ListNode head) {
 
     ListNode current = head;
