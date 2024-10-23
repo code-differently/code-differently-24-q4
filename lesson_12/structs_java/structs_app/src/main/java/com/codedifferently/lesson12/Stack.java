567346dc01f910db49ef0bec6576e22bbbb5bb90
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-10-23 16:47:27 +0000

    Feat: Adds Shawn Dunsmore Jr java/stack.

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Stack.java
@@ -1,26 +1,36 @@
 package com.codedifferently.lesson12;
 
 /** Implement the below Stack by providing code for the class methods. */
-public class Stack {
-  private ListNode top;
+  public class Stack {
+    private ListNode top;
 
-  public Stack() {
-    this.top = null;
-  }
+    public Stack() {
+        this.top = null;
+    }
 
-  public void push(int value) {
-    // Your code here
-  }
+    public void push(int value) {
+        ListNode newNode = new ListNode(value);
+        newNode.next = top;
+        top = newNode;
+    }
 
-  public int pop() {
-    return 0;
-  }
+    public int pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        int value = top.val;
+        top = top.next;
+        return value;
+    }
 
-  public int peek() {
-    return 0;
-  }
+    public int peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.val;
+    }
 
-  public boolean isEmpty() {
-    return true;
-  }
+    public boolean isEmpty() {
+        return top == null;
+    }
 }
