70725b114685fba64916fd0631c3700dcf5bb1ba
nilejack <njacksonnorthrup244@gmail.com>
2024-10-23 01:16:43 +0000

    fix

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,33 +7,27 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
-  }
 
+    int[] scores = new int[2];
 
-  class Solution {
-    public String gameResult(ListNode head) {
-      int[] scores = new int[2];
-
-      while (head.next != null) {
-        // Used ChatGpt to help complete this if statement-starting at the logical & operand
-        if (head.val != head.next.val && head.val % 2 == 0) {
-          if (head.val > head.next.val) {
-            scores[head.val % 2]++;
-            // Gpt help ended at the line above
-          } else {
-            scores[head.next.val % 2]++;
-          }
+    while (head.next != null) {
+      // Used ChatGpt to help complete this if statement-starting at the logical & operand
+      if (head.val != head.next.val && head.val % 2 == 0) {
+        if (head.val > head.next.val) {
+          scores[head.val % 2]++;
+          // Gpt help ended at the line above
+        } else {
+          scores[head.next.val % 2]++;
         }
-        head = head.next;
       }
-      if (scores[0] == scores[1]) {
-        return "Tie";
-      } else if (scores[0] > scores[1]) {
-        return "Even";
-      } else {
-        return "odd";
-      }
+      head = head.next;
+    }
+    if (scores[0] == scores[1]) {
+      return "Tie";
+    } else if (scores[0] > scores[1]) {
+      return "Even";
+    } else {
+      return "odd";
     }
   }
 }
