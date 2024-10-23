ed246e298eac7d197ecf41246f03b045b4df97ad
angie-3 <acastilloruiz244@gmail.com>
2024-10-23 11:42:20 +0000

    changes made to lesson 12 stack and java.

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -7,6 +7,23 @@ public class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-    return null;
+
+    ListNode current = head;
+    int evenTeam = 0;
+    int oddTeam = 0;
+    while (current != null) {
+      if (current.val < current.next.val) {
+        oddTeam++;
+      } else if (current.val > current.next.val) {
+        evenTeam++;
+      }
+      current = current.next.next;
+    }
+
+    if (evenTeam < oddTeam) {
+      return "oddTeamwins";
+    } else if (evenTeam > oddTeam) {
+      return "eveTeamwins";
+    } else return "tie";
   }
 }
