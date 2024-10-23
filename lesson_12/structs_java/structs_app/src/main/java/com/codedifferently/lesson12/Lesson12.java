7cf7a9b5d72e8ec17b51e779105aed418bfd2b27
angie-3 <acastilloruiz244@gmail.com>
2024-10-23 12:12:18 +0000

    fix: bug in code

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -2,7 +2,10 @@ package com.codedifferently.lesson12;
 
 public class Lesson12 {
 
-   
+  /**
+   * Provide the solution to LeetCode 3062 here:
+   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
+   */
   public String gameResult(ListNode head) {
 
     ListNode current = head;
@@ -20,7 +23,7 @@ public class Lesson12 {
     if (evenTeam < oddTeam) {
       return "oddTeamwins";
     } else if (evenTeam > oddTeam) {
-      return "eveTeamwins";
+      return "evenTeamwins";
     } else return "tie";
   }
 }
