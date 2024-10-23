68d267c7d4d35d78cd199c0cc48d99daf1483487
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-10-23 17:02:58 +0000

    Feat: Adds Shawn Dunsmore Jr Added spotlessapply

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -2,29 +2,30 @@ package com.codedifferently.lesson12;
 
 public class Lesson12 {
 
-    /**
-     * Provide the solution to LeetCode 3062 here:
-     * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
-     */
+  /**
+   * Provide the solution to LeetCode 3062 here:
+   * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
+   */
+  public String gameResult(ListNode head) {
+    int oddPoints = 0;
+    int evenPoints = 0;
 
-    public String gameResult(ListNode head) {
-        int oddPoints = 0;
-        int evenPoints = 0;
-
-        for (ListNode current = head; current != null && current.next != null; current = current.next.next) {
-            if (current.val < current.next.val) {
-                oddPoints++;
-            } else if (current.val > current.next.val) {
-                evenPoints++;
-            }
-        }
-
-        if (oddPoints > evenPoints) {
-            return "Odd";
-        } else if (evenPoints > oddPoints) {
-            return "Even";
-        } else {
-            return "Tie";
-        }
+    for (ListNode current = head;
+        current != null && current.next != null;
+        current = current.next.next) {
+      if (current.val < current.next.val) {
+        oddPoints++;
+      } else if (current.val > current.next.val) {
+        evenPoints++;
+      }
     }
+
+    if (oddPoints > evenPoints) {
+      return "Odd";
+    } else if (evenPoints > oddPoints) {
+      return "Even";
+    } else {
+      return "Tie";
+    }
+  }
 }
