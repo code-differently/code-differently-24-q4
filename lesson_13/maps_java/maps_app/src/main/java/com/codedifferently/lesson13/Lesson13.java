017835bdd0485944cf35cef0daf3c99c53f511f5
txtran224 <txtran224@gmail.com>
2024-10-24 04:00:16 +0000

    fix: apply gradle spotlessApply

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -9,20 +9,20 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-HashMap<Character,Integer> smap = new HashMap<>();
-HashMap<Character,Integer> tmap = new HashMap<>();
-int sum = 0;
-  for (int i =0; i <s.length(); i++){
-    smap.put(s.charAt(i),i);
-    tmap.put(t.charAt(i),i);
-  }
-  for (Character key:smap.keySet()){
-    if (tmap.containsKey(key)) {
-      Integer val1 = smap.get(key);
-      Integer val2 = tmap.get(key);
-      sum += Math.abs(val1-val2);
-            }
-        }
-        return sum;
+    HashMap<Character, Integer> smap = new HashMap<>();
+    HashMap<Character, Integer> tmap = new HashMap<>();
+    int sum = 0;
+    for (int i = 0; i < s.length(); i++) {
+      smap.put(s.charAt(i), i);
+      tmap.put(t.charAt(i), i);
+    }
+    for (Character key : smap.keySet()) {
+      if (tmap.containsKey(key)) {
+        Integer val1 = smap.get(key);
+        Integer val2 = tmap.get(key);
+        sum += Math.abs(val1 - val2);
+      }
+    }
+    return sum;
   }
 }
