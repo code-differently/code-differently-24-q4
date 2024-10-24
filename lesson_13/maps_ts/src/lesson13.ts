c84cbd49f43e599669cf480af92652f8d5956812
cdbluejr <cdblue244@gmail.com>
2024-10-24 20:15:08 +0000

    feat: LeetCode 3145 Typescript by Dwight Blue

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,15 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+    let sum = 0;
+    const strStone = new Map<string, number>();
+
+    for (let i = 0; i < s.length; i++) {
+      strStone.set(s.charAt(i),i);
+    }
+    for (const key of strStone.keys()) {
+      const indexSTR = t.indexOf(key);
+        sum += Math.abs(s.indexOf(key) - indexSTR);
+     }
+     return sum;
 }
