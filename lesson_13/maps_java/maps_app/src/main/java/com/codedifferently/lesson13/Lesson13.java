16b4caa7964a3e9c66b574c741cf4074ca3d9ac4
Dasiame <dmenglish244@gmail.com>
2024-10-24 19:28:44 +0000

    feat: Adds Hash Map

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,21 +1,25 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
-
-  /**
-   * Provide the solution to LeetCode 3146 here:
-   * https://leetcode.com/problems/permutation-difference-between-two-strings
-   */
   public int findPermutationDifference(String s, String t) {
-
-    int sumDifference = 0;
+    HashMap<Character, Integer> letters = new HashMap<>();
+    int permDifferences = 0;
     for (int i = 0; i < s.length(); i++) {
-      char currentChar = s.charAt(i);
-      int indexInT = t.indexOf(currentChar);
-      int difference = Math.abs(i - indexInT);
-      sumDifference += difference;
+      letters.put(s.charAt(i), i);
     }
-    System.out.println("Here is the Permutation Difference is: + letters.get");
-    return sumDifference;
+    for (char key : letters.keySet()) {
+      int indexInT = t.indexOf(Character.toString(key));
+      permDifferences += Math.abs(s.indexOf(key) - indexInT);
+    }
+    // int sumDifference = 0;
+    // for (int i = 0; i < s.length(); i++) {
+    //   char currentChar = s.charAt(i);
+    //   int indexInT = t.indexOf(currentChar);
+    //   int difference = Math.abs(i - indexInT);
+    //   sumDifference += difference;
+
+    return permDifferences;
   }
 }
