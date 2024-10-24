83c7a80d64bc15a0975cee82721e47d30b2d60d9
zionbuch <zabuchanan244@gmail.com>
2024-10-24 19:57:26 +0000

    feat: adds Zion's PermutationDifference method in lesson_13.ts extra credit hw

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,15 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+      let charLocation = new Map<string, number>();
+    let charSum = 0;
+    for (let i = 0; i < s.length; i++) {
+      charLocation.set(s[i], i);
+    }
+    for (let i = 0; i < t.length; i++) {
+      let charResult = charLocation.get(t.charAt(i));
+      charSum += Math.abs(charResult - i);
+    }
+    return charSum;
+
 }
