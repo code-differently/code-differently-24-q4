b3ec7aa17d13c82da0b4e2fa4be25fa2b178eff5
jjcapparell <jjcapparell244@gmail.com>
2024-10-24 17:12:25 +0000

    fix: simplified code and made shorter

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,25 +1,17 @@
 export function findPermutationDifference(s: string, t: string): number {
   const stringS = new Map<string, number>();
-  for (let i = 0; i < s.length; i++) {
-    stringS.set(s.charAt(i), i);
-  }
-
   const stringT = new Map<string, number>();
+  let diffSum = 0;
   for (let i = 0; i < t.length; i++) {
+    stringS.set(s.charAt(i), i);
     stringT.set(t.charAt(i), i);
   }
-
-  return compareMaps(stringS, stringT);
-}
-
-function compareMaps(map1: Map<string, number>, map2: Map<string, number>): number {
-  let diffSum = 0;
-  for (const key of map1.keys()) {
-    if (map2.has(key)) {
+  for (const key of stringS.keys()) {
+    if (stringT.has(key)) {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const value1 = map1.get(key)!; // Non-null assertion because we checked with has()
+      const value1 = stringS.get(key)!; // Non-null assertion because we checked with has()
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const value2 = map2.get(key)!; // Non-null assertion
+      const value2 = stringT.get(key)!; // Non-null assertion
       const difference = Math.abs(value1 - value2);
       diffSum += difference;
     }
