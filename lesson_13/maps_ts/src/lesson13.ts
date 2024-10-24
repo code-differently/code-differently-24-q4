ef98f71269fbebbcb021e4d3062401a97be79f8f
txtran224 <txtran224@gmail.com>
2024-10-24 13:38:52 +0000

    feat: lesson_13 leet code solution in ts

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,16 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  const smap = new Map<string, number>();
+  let sum = 0;
+  for (let i = 0; i < s.length; i++) {
+    smap.set(s.charAt(i), i);
+  }
+  for (const key of smap.keys()) {
+    const val1 = smap.get(key);
+    if (val1 !== undefined) {
+      sum += Math.abs(val1 - t.indexOf(key));
+    }
+  }
+  return sum;
 }
