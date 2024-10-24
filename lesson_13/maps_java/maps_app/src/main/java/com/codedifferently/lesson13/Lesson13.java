1acecaf396fa969a9712f7e52a4e2fb2fffd6ab7
Kotangora1 <oyeyemijimoh244@gmail.com>
2024-10-24 01:26:31 +0000

    feat: spotless fix for HashMap implementation  by Yemi

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -6,14 +6,14 @@ public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
     var indexMap = new HashMap<Character, Integer>();
     for (int i = 0; i < t.length(); i++) {
-        indexMap.put(t.charAt(i), i);
+      indexMap.put(t.charAt(i), i);
     }
 
     int result = 0;
     for (int i = 0; i < s.length(); i++) {
-        // Default to -1 if char not found
-        result += Math.abs(i - indexMap.getOrDefault(s.charAt(i), -1)); 
+      // Default to -1 if char not found
+      result += Math.abs(i - indexMap.getOrDefault(s.charAt(i), -1));
     }
     return result;
- }
+  }
 }
