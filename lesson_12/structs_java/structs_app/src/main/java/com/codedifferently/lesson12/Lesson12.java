40ac10fde6c0c4de885c7f43a90d3064ef0d44b6
Joseph <jxcaballero244@gmail.com>
2024-10-24 02:24:28 +0000

    fix: Fixes build run using spotless apply

--- lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
+++ lesson_12/structs_java/structs_app/src/main/java/com/codedifferently/lesson12/Lesson12.java
@@ -2,34 +2,28 @@ package com.codedifferently.lesson12;
 
 public class Lesson12 {
 
-    public Lesson12() {
-      
-    }
+  public Lesson12() {}
 
   /**
    * Provide the solution to LeetCode 3062 here:
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public String gameResult(ListNode head) {
-      ListNode current = head;
-      int oddPoints = 0;
-      int evenPoints = 0;         
-      while (current != null){
-        if (current.val > current.next.val){
-          evenPoints++;
-          }
-        else{
-          oddPoints++;
-        }
+    ListNode current = head;
+    int oddPoints = 0;
+    int evenPoints = 0;
+    while (current != null) {
+      if (current.val > current.next.val) {
+        evenPoints++;
+      } else {
+        oddPoints++;
+      }
       current = current.next.next;
-      }
-      if ( evenPoints > oddPoints) {
-        return "Even";
-      } else if ( evenPoints<oddPoints) {
-        return "Odd";
-      }
-        else return "Tie";
-        
-}       
-
-  } 
+    }
+    if (evenPoints > oddPoints) {
+      return "Even";
+    } else if (evenPoints < oddPoints) {
+      return "Odd";
+    } else return "Tie";
+  }
+}
