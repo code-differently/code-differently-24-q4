1c44f447bdfd16766de8a070c147755c8397f997
zionbuch <zabuchanan244@gmail.com>
2024-10-24 20:19:13 +0000

    feat: fixed syntax error for lesson_13.ts

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,15 +3,16 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-    let charLocation = new Map<string, number>();
-    let charSum = 0;
-    for (let i = 0; i < s.length; i++) {
-      charLocation.set(s[i], i);
-    }
-    for (let i = 0; i < t.length; i++) {
-      let charResult = charLocation.get(t.charAt(i));
+  const charLocation = new Map<string, number>();
+  let charSum = 0;
+  for (let i = 0; i < s.length; i++) {
+    charLocation.set(s[i], i);
+  }
+  for (let i = 0; i < t.length; i++) {
+    const charResult = charLocation.get(t.charAt(i));
+    if (charResult !== undefined) {
       charSum += Math.abs(charResult - i);
     }
-    return charSum;
-
+  }
+  return charSum;
 }
