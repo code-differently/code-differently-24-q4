f5872dda2725967d034e9c636240ece0549c97a4
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-10-24 01:58:26 +0000

    fix: ran ./gradlew :maps_app:spotlessApply to correct formating;

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -4,15 +4,14 @@ public class Lesson13 {
 
   /**
    * Provide the solution to LeetCode 3146 here:
-   * https://leetcode.com/problems/permutation-difference-between-two-strings
-   * You are given two strings s and t such that every character occurs at most once in s and t is a permutation of s. 
-   * The permutation difference between s and t is defined as 
-   * the sum of the absolute difference between the index of the occurrence 
-   * of each character in s and the index of the occurrence of the same character in t. 
-   * Return the permutation difference between s and t.
+   * https://leetcode.com/problems/permutation-difference-between-two-strings You are given two
+   * strings s and t such that every character occurs at most once in s and t is a permutation of s.
+   * The permutation difference between s and t is defined as the sum of the absolute difference
+   * between the index of the occurrence of each character in s and the index of the occurrence of
+   * the same character in t. Return the permutation difference between s and t.
    */
   public int findPermutationDifference(String s, String t) {
-    
+
     int permuDifference = 0;
     // Created a counter to track Permutation Difference
     for (int i = 0; i < s.length(); i++) {
