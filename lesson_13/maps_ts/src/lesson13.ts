a1637c4c110c68fba74bde444cd5102f55bcb740
jjcapparell <jjcapparell244@gmail.com>
2024-10-24 15:15:59 +0000

    Feat: added content to lesson13.ts

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,7 +1,26 @@
-/**
- * Provide the solution to LeetCode 3146 here:
- * https://leetcode.com/problems/permutation-difference-between-two-strings
- */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  const stringS: Map<string, number> = new Map();
+  for (let i = 0; i < s.length; i++) {
+    stringS.set(s.charAt(i), i);
+  }
+
+  const stringT: Map<string, number> = new Map();
+  for (let i = 0; i < t.length; i++) {
+    stringT.set(t.charAt(i), i);
+  }
+
+  return compareMaps(stringS, stringT);
+}
+
+function compareMaps(map1: Map<string, number>, map2: Map<string, number>): number {
+  let diffSum = 0;
+  for (const key of map1.keys()) {
+    if (map2.has(key)) {
+      const value1 = map1.get(key)!; // Non-null assertion because we checked with has()
+      const value2 = map2.get(key)!; // Non-null assertion
+      const difference = Math.abs(value1 - value2);
+      diffSum += difference;
+    }
+  }
+  return diffSum;
 }
