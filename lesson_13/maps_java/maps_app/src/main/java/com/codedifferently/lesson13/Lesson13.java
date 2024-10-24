29cc80cb3fcc6dd46f939a8de1978b992ffbd35b
jjcapparell <jjcapparell244@gmail.com>
2024-10-24 15:02:41 +0000

    fix: gradle command edit

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,28 +1,29 @@
 package com.codedifferently.lesson13;
+
 import java.util.HashMap;
+
 public class Lesson13 {
 
   public int findPermutationDifference(String s, String t) {
     HashMap<Character, Integer> stringS = new HashMap<>();
-    for (int i=0; i<s.length(); i++){
+    for (int i = 0; i < s.length(); i++) {
       stringS.put(s.charAt(i), i);
     }
     HashMap<Character, Integer> stringT = new HashMap<>();
-    for (int i=0; i<t.length(); i++){
+    for (int i = 0; i < t.length(); i++) {
       stringT.put(t.charAt(i), i);
     }
     return compareMaps(stringS, stringT);
-    
   }
 
   public int compareMaps(HashMap<Character, Integer> map1, HashMap<Character, Integer> map2) {
-    int diffSum=0;
+    int diffSum = 0;
     for (Character key : map1.keySet()) {
       if (map2.containsKey(key)) {
         int value1 = map1.get(key);
         int value2 = map2.get(key);
         int difference = Math.abs(value1 - value2);
-        diffSum+=difference;
+        diffSum += difference;
       }
     }
     return diffSum;
