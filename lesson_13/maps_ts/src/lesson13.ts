c22c3d04feef961402ba21bc06abb4b78830e731
jjcapparell <jjcapparell244@gmail.com>
2024-10-24 15:49:41 +0000

    fix:  lesson13.ts edits to pass npm testing

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,10 +1,10 @@
 export function findPermutationDifference(s: string, t: string): number {
-  const stringS: Map<string, number> = new Map();
+  const stringS = new Map<string, number>();
   for (let i = 0; i < s.length; i++) {
     stringS.set(s.charAt(i), i);
   }
 
-  const stringT: Map<string, number> = new Map();
+  const stringT = new Map<string, number>();
   for (let i = 0; i < t.length; i++) {
     stringT.set(t.charAt(i), i);
   }
@@ -16,7 +16,9 @@ function compareMaps(map1: Map<string, number>, map2: Map<string, number>): numb
   let diffSum = 0;
   for (const key of map1.keys()) {
     if (map2.has(key)) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const value1 = map1.get(key)!; // Non-null assertion because we checked with has()
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const value2 = map2.get(key)!; // Non-null assertion
       const difference = Math.abs(value1 - value2);
       diffSum += difference;
