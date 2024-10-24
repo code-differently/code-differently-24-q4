4bb2ad7d3912cc8e69fa1ac336f05cc93ff7f240
Dasiame <dmenglish244@gmail.com>
2024-10-24 17:55:21 +0000

    feat:Adds PermutationDifference to Dasias Lesson_13

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -7,6 +7,15 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    
+    int sumDifference = 0;
+    for (int i = 0; i < s.length(); i++) {
+      char currentChar = s.charAt(i);
+      int indexInT = t.indexOf(currentChar);
+      int difference = Math.abs(i - indexInT);
+      sumDifference += difference;
+    }
+    System.out.println("Here is the Permutation Difference is: + letters.get");
+    return sumDifference;
   }
 }
