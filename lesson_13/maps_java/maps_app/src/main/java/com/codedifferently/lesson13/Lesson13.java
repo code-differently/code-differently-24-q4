9b0c6c141e0dcd48c32d71afe1c99d6469e0e156
AmiyahJo <ajones244@gmail.com>
2024-10-24 13:11:23 +0000

    fix: .gradlew format
    fixed violations from githubs reccomendation

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -7,17 +7,17 @@ public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
     var charIndexMap = new HashMap<Character, Integer>();
 
-    int totalDifference = 0;  
+    int totalDifference = 0;
 
     for (int sIndex = 0; sIndex < s.length(); sIndex++) {
-        charIndexMap.put(s.charAt(sIndex), sIndex);
+      charIndexMap.put(s.charAt(sIndex), sIndex);
     }
 
     for (int tIndex = 0; tIndex < t.length(); tIndex++) {
-      int indexInS = charIndexMap.get(t.charAt(tIndex)); 
-      totalDifference += Math.abs(indexInS - tIndex); 
+      int indexInS = charIndexMap.get(t.charAt(tIndex));
+      totalDifference += Math.abs(indexInS - tIndex);
     }
-     
+
     return totalDifference;
   }
 }
