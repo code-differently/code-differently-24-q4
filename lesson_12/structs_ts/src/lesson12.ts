e5c25a88767853be846646c05e0bc1b2aab09d61
marleomac3 <mlmcwilliams244@gmail.com>
2024-10-24 01:47:58 +0000

    fix: removed lesson12.ts from PR due to failing tests

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -6,27 +6,6 @@ export class Lesson12 {
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
   public gameResult(head: ListNode | null): string {
-    let oddPoints: number = 0;
-    let evenPoints: number = 0;
-
-    let curr = head;
-
-    while (curr != null && curr.next != null) {
-      if (curr.val > curr.next.val) {
-        evenPoints++;
-      } else if (curr.val < curr.next.val) {
-        oddPoints++;
-      }
-      curr = curr.next.next as ListNode;
-    }
-
-    if (oddPoints > evenPoints) {
-      return "Odd";
-    } else if (oddPoints < evenPoints) {
-      return "Even";
-    } else {
-      return "Tie";
-    }
+    return '';
   }
 }
- 
