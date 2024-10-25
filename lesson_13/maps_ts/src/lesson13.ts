3d439d30728176dcc207b04bdd4d8d218c1aa693
jjcapparell <jjcapparell244@gmail.com>
2024-10-25 18:42:41 +0000

    chore: line deletions for java file

--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -9,7 +9,7 @@ export function findPermutationDifference(s: string, t: string): number {
   for (const key of stringS.keys()) {
     if (stringT.has(key)) {
       const value1 = stringS.get(key)!; // Non-null assertion because we checked with has()
-      const value2 = stringT.get(key)!; // Non-null assertion
+      const value2 = stringT.get(key)!; // Non-null assertion 
       const difference = Math.abs(value1 - value2);
       diffSum += difference;
     }
