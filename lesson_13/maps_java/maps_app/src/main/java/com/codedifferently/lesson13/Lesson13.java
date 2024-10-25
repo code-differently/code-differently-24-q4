cb44a2c8d86ac4771cfd73732bf7153066ebfce0
Joseph <jxcaballero244@gmail.com>
2024-10-25 03:47:50 +0000

    feat: modifies lesson13.java and lesson 13.ts for decent time complexity and basic hash map utilization to complete the leetcode problem for lesson 13 - Joseph Caballero

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +9,16 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    HashMap<Character, Integer> map = new HashMap<>();
+
+    int sum = 0;
+    for (char letter : s.toCharArray()) {
+      map.put(letter, s.indexOf(letter));
+    }
+    for (int i = 0; i < s.length(); i++) {
+      int indexOfT = map.get(t.charAt(i));
+      sum += Math.abs(i - indexOfT);
+    }
+    return sum;
   }
 }
--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -3,5 +3,14 @@
  * https://leetcode.com/problems/permutation-difference-between-two-strings
  */
 export function findPermutationDifference(s: string, t: string): number {
-  return 0;
+  const map = new Map<string, number>();
+  let sum = 0;
+  for (const char of s) {
+    map.set(char, s.indexOf(char));
+  }
+  for (const [key, value] of map) {
+    const indexOfT = t.indexOf(key);
+    sum += Math.abs(value - indexOfT);
+  }
+  return sum;
 }
