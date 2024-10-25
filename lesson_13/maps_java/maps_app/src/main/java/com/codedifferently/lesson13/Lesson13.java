c17bcae3467da411077c2dbe934e334137d2f688
Dasiame <dmenglish244@gmail.com>
2024-10-25 13:46:46 +0000

    fix: Changed variable name to be more discribitive

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -4,22 +4,15 @@ import java.util.HashMap;
 
 public class Lesson13 {
   public int findPermutationDifference(String s, String t) {
-    HashMap<Character, Integer> letters = new HashMap<>();
+    HashMap<Character, Integer> hashMap = new HashMap<>();
     int permDifferences = 0;
     for (int i = 0; i < s.length(); i++) {
-      letters.put(s.charAt(i), i);
+      hashMap.put(s.charAt(i), i);
     }
-    for (char key : letters.keySet()) {
+    for (char key : hashMap.keySet()) {
       int indexInT = t.indexOf(Character.toString(key));
       permDifferences += Math.abs(s.indexOf(key) - indexInT);
     }
-    // int sumDifference = 0;
-    // for (int i = 0; i < s.length(); i++) {
-    //   char currentChar = s.charAt(i);
-    //   int indexInT = t.indexOf(currentChar);
-    //   int difference = Math.abs(i - indexInT);
-    //   sumDifference += difference;
-
     return permDifferences;
   }
 }
--- lesson_13/maps_ts/src/lesson13.ts
+++ lesson_13/maps_ts/src/lesson13.ts
@@ -1,24 +1,14 @@
-/**
- * Provide the solution to LeetCode 3146 here:
- * https://leetcode.com/problems/permutation-difference-between-two-strings
- */
+
 export function findPermutationDifference(s: string, t: string): number {
-  const letters = new Map<string,number>();
+  const map = new Map<string,number>();
     for (let i = 0; i < s.length; i++) {
-      letters.set(s.charAt(i), i);
+      map.set(s.charAt(i), i);
     }
     let permDifferences = 0;
-    for (const key of letters.keys()) {
+    for (const key of map.keys()) {
       const indexInT = t.indexOf(String(key));
        permDifferences += Math.abs(s.indexOf(key) - indexInT);
     }
-    // int sumDifference = 0;
-    // for (int i = 0; i < s.length(); i++) {
-    //   char currentChar = s.charAt(i);
-    //   int indexInT = t.indexOf(currentChar);
-    //   int difference = Math.abs(i - indexInT);
-    //   sumDifference += difference;
-
     return permDifferences;
   }
 
