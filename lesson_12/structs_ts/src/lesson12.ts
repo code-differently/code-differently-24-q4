0cc24ba14003d864b55dd97d81fc83cf35be57c3
nilejack <njacksonnorthrup244@gmail.com>
2024-10-25 10:40:45 +0000

    Feat: fix to .ts files

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -1,34 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { ListNode } from './list_node.js';
 
 export class Lesson12 {
+  push(scores: number): void {
+    throw new Error('Not implemented');
+  }
   /**
    * Provide the solution to LeetCode 3062 here:
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
-  public gameResult(head: ListNode | null): String {
-    if (!head) return "Tie";
-     
-    
-      let current: ListNode | null = head;
-      const scores: number[] = [0, 0];
-    
-      while (current && current.next) {
+  public gameResult(head: ListNode | null): string {
+    if (!head) return 'Tie';
+
+    let current: ListNode | null = head;
+    const scores: number[] = [0, 0];
+
+    while (current && current.next) {
       if (current.val !== current.next.val && current.val % 2 == 0) {
         if (current.val > current.next.val) {
           scores[0]++;
-          } else {
-            scores[1]++;
-          }
+        } else {
+          scores[1]++;
+        }
       }
       current = current.next;
     }
-       if(scores[0] > scores[1]) {
-       return "Even";
-    }  else if (scores[0] < scores[1]) {
-       return "Odd";
-    }  else {
-       return "Tie";
+    if (scores[0] > scores[1]) {
+      return 'Even';
+    } else if (scores[0] < scores[1]) {
+      return 'Odd';
+    } else {
+      return 'Tie';
+    }
   }
 }
-}   
-
