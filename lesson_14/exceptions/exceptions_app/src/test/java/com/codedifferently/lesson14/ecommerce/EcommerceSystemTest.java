771d84b5d19e6a8636588c92bfb45f2717e3d3b3
Anthony D. Mays <anthony@morganlatimer.com>
2024-10-25 02:32:28 +0000

    feat: adds lesson_14 homework and lesson_15 pre-work
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

---
+++ lesson_14/exceptions/exceptions_app/src/test/java/com/codedifferently/lesson14/ecommerce/EcommerceSystemTest.java
@@ -0,0 +1,74 @@
+package com.codedifferently.lesson14.ecommerce;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EcommerceSystemTest {
+  EcommerceSystem ecommerceSystem;
+
+  @BeforeEach
+  void setUp() {
+    ecommerceSystem = new EcommerceSystem();
+  }
+
+  @Test
+  void testAddProduct() {
+    // Act
+    ecommerceSystem.addProduct("1", "Laptop");
+  }
+
+  @Test
+  void testPlaceOrder() {
+    // Arrange
+    ecommerceSystem.addProduct("1", "Laptop");
+
+    // Act
+    String orderId = ecommerceSystem.placeOrder("1", 1);
+
+    // Assert
+    String actual = ecommerceSystem.checkOrderStatus(orderId);
+    assertThat(actual).isEqualTo("Order ID: " + orderId + ", Product: Laptop, Quantity: 1");
+  }
+
+  @Test
+  void testPlaceOrder_productDoesNotExist() {
+    // Act
+    assertThatThrownBy(() -> ecommerceSystem.placeOrder("1", 1))
+        .isInstanceOf(ProductNotFoundException.class)
+        .hasMessage("Product with ID 1 not found");
+  }
+
+  @Test
+  void testCancelOrder() {
+    // Arrange
+    ecommerceSystem.addProduct("1", "Laptop");
+    String orderId = ecommerceSystem.placeOrder("1", 1);
+
+    // Act
+    ecommerceSystem.cancelOrder(orderId);
+  }
+
+  @Test
+  void testCheckOrderStatus_orderDoesNotExist() {
+    // Act
+    assertThatThrownBy(() -> ecommerceSystem.checkOrderStatus("1"))
+        .isInstanceOf(OrderNotFoundException.class)
+        .hasMessage("Order with ID 1 not found");
+  }
+
+  @Test
+  void testCheckOrderStatus_orderCancelled() {
+    // Arrange
+    ecommerceSystem.addProduct("1", "Laptop");
+    String orderId = ecommerceSystem.placeOrder("1", 1);
+    ecommerceSystem.cancelOrder(orderId);
+
+    // Act
+    assertThatThrownBy(() -> ecommerceSystem.checkOrderStatus("1"))
+        .isInstanceOf(OrderNotFoundException.class)
+        .hasMessage("Order with ID 1 not found");
+  }
+}
