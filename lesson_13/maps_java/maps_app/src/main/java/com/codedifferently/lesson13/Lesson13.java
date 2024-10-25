13a1a25c369f29bb0ddf7e84b10bf1d335b90564
nilejack <njacksonnorthrup244@gmail.com>
2024-10-25 00:48:28 +0000

    Feat:Adds Lesson13.java/ts for NileJackson

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +9,17 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    HashMap<Character, Integer> ABC = new HashMap<>();
+    int permDiff = 0;
+    for (int i = 0; i < s.length(); i++) {
+      ABC.put(s.charAt(i), i);
+    }
+
+    for (char key : ABC.keySet()) {
+      int indexInT = t.indexOf(Character.toString(key));
+
+      permDiff += Math.abs(s.indexOf(key) - indexInT);
+    }
+    return permDiff;
   }
 }
