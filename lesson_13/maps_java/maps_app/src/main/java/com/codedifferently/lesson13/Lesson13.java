117f0a96f747102c4e11c12ea8670055bc3ae0a9
htanweer244 <htanweer244@gmail.com>
2024-10-25 14:31:27 +0000

    Feat: Implemented Hashmap to lesson 13 by Hummad Tanweer

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,12 +1,27 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
    * Provide the solution to LeetCode 3146 here:
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
+
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    
+        //var charLocation = new HashMap<Character, Integer>();
+
+    HashMap<Character,Integer> map = new HashMap();
+    int sum=0;
+    for(int i =0; i< s.length(); i++) { 
+        map.putIfAbsent(s.charAt(i),i);
+    }
+    for(char key: map.keySet())  {
+        int indexInt = t.indexOf(Character.toString(key));
+        sum += Math.abs (s.indexOf(key)- indexInt);
+    }
+    return sum;
   }
 }
