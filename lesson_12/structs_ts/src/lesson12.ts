3824f054999e00c77847a3852fa283435cb79b41
nilejack <njacksonnorthrup244@gmail.com>
2024-10-25 10:28:14 +0000

    Feat: Adds, NileJackson's extra credit for lesson12.ts

--- lesson_12/structs_ts/src/lesson12.ts
+++ lesson_12/structs_ts/src/lesson12.ts
@@ -5,7 +5,30 @@ export class Lesson12 {
    * Provide the solution to LeetCode 3062 here:
    * https://github.com/yang-su2000/Leetcode-algorithm-practice/tree/master/3062-winner-of-the-linked-list-game
    */
-  public gameResult(head: ListNode | null): string {
-    return '';
+  public gameResult(head: ListNode | null): String {
+    if (!head) return "Tie";
+     
+    
+      let current: ListNode | null = head;
+      const scores: number[] = [0, 0];
+    
+      while (current && current.next) {
+      if (current.val !== current.next.val && current.val % 2 == 0) {
+        if (current.val > current.next.val) {
+          scores[0]++;
+          } else {
+            scores[1]++;
+          }
+      }
+      current = current.next;
+    }
+       if(scores[0] > scores[1]) {
+       return "Even";
+    }  else if (scores[0] < scores[1]) {
+       return "Odd";
+    }  else {
+       return "Tie";
   }
 }
+}   
+
