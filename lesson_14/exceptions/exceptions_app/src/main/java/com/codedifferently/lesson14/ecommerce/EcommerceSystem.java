b7861507e2a35c562e6fb73d58bbb31103ef5cc0
nilejack <njacksonnorthrup244@gmail.com>
2024-10-26 23:08:41 +0000

    Feat: Adds-Changes to Lesson14 .Java files for Nile Jackson

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/EcommerceSystem.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/EcommerceSystem.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+
 public class EcommerceSystem {
   private Map<String, Product> products;
   private Map<String, Order> orders;
@@ -17,19 +18,31 @@ public class EcommerceSystem {
     products.put(productId, new Product(productId, name));
   }
 
-  public String placeOrder(String productId, int quantity) {
+  public String placeOrder(String productId, int quantity)  throws ProductNotFoundException{
     Product product = products.get(productId);
+    if (product == null) {
+      throw new ProductNotFoundException("Product with Id 1 Not found ");
+    }
     String orderId = UUID.randomUUID().toString();
     orders.put(orderId, new Order(orderId, product, quantity));
     return orderId;
   }
 
-  public void cancelOrder(String orderId) {
+  public void cancelOrder(String orderId) throws
+   OrderNotFoundException{
+    Order order = orders.get(orderId);
+    try {
+        if (order == null ) throw new 
+        OrderNotFoundException("No Such order exists");
+    } catch (OrderNotFoundException e) {
+      System.out.println(" order does not exist");
+    }
     orders.remove(orderId);
   }
 
-  public String checkOrderStatus(String orderId) {
+  public String checkOrderStatus(String orderId) throws OrderNotFoundException {
     Order order = orders.get(orderId);
+    if(order == null) throw new OrderNotFoundException("order with ID 1 not found");
     return "Order ID: "
         + orderId
         + ", Product: "
--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
@@ -5,4 +5,12 @@
 
 package com.codedifferently.lesson14.ecommerce;
 
-class OrderNotFoundException {}
+class OrderNotFoundException extends RuntimeException{
+    
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
