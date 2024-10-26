5ef2273ae0d0a203003d8e37defc43314e128782
haldanek <khaldane244@gmail.com>
2024-10-26 22:03:38 +0000

    feat: adds Kimberlee's lesson 13 HW- findPermutationDifference

--- lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
+++ lesson_13/maps_java/maps_app/src/main/java/com/codedifferently/lesson13/Lesson13.java
@@ -1,5 +1,7 @@
 package com.codedifferently.lesson13;
 
+import java.util.HashMap;
+
 public class Lesson13 {
 
   /**
@@ -7,6 +9,27 @@ public class Lesson13 {
    * https://leetcode.com/problems/permutation-difference-between-two-strings
    */
   public int findPermutationDifference(String s, String t) {
-    return 0;
+    HashMap<Character, Integer> indexMap = new HashMap<>();
+
+    for (int i =0; i < s.length(); i++) {
+      indexMap.put(s.charAt(i), i);
+    }
+
+    HashMap<Character, Integer> indexDifferences = new HashMap<>();
+    int sumOfAbsoluteDiff = 0;
+
+    for (int i = 0; i < t.length(); i++) {
+      char c = t.charAt(i);
+      int sIndex = indexMap.get(c);
+      int tIndex = i;
+
+      int indexDiff = Math.abs(sIndex - tIndex);
+
+      indexDifferences.put(c, indexDiff);
+
+      sumOfAbsoluteDiff += indexDiff;
+    }
+
+    return sumOfAbsoluteDiff;
   }
 }
