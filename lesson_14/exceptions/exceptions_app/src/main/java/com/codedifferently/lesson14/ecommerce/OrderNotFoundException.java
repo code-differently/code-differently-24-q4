bd775064c6a90080b848c2a390d1e58a0cb91029
Yafiaha <ynadbullah244@gmail.com>
2024-10-28 09:56:39 -0400

    Feat: Yafiah Lesson-14 Custom Exceptions in Ecommerce System

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/OrderNotFoundException.java
@@ -5,4 +5,9 @@
 
 package com.codedifferently.lesson14.ecommerce;
 
-class OrderNotFoundException {}
+class OrderNotFoundException extends RuntimeException{
+  public OrderNotFoundException(String message) {
+   super(message);
+  }
+}
+
