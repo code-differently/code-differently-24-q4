1fff516bd5a3a76d4ad6fd052adab76fcb30f0ec
Anthony D. Mays <anthony@morganlatimer.com>
2024-10-28 15:42:08 +0000

    chore: add build checks and fix formatting
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_15/tdd/tdd_app/src/main/java/com/codedifferently/lesson15/EmployeeManager.java
+++ lesson_15/tdd/tdd_app/src/main/java/com/codedifferently/lesson15/EmployeeManager.java
@@ -4,29 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EmployeeManager {
-    private Map<Integer, Employee> employees;
+  private Map<Integer, Employee> employees;
 
-    public EmployeeManager() {
-        employees = new HashMap<>();
-    }
+  public EmployeeManager() {
+    employees = new HashMap<>();
+  }
 
-    public void addEmployee(Employee employee) {
-        employees.put(employee.getId(), employee);
-    }
+  public void addEmployee(Employee employee) {
+    employees.put(employee.getId(), employee);
+  }
 
-    public Employee getEmployee(int id) {
-        return employees.get(id);
-    }
+  public Employee getEmployee(int id) {
+    return employees.get(id);
+  }
 
-    public void updateEmployee(Employee employee) {
-        employees.put(employee.getId(), employee);
-    }
+  public void updateEmployee(Employee employee) {
+    employees.put(employee.getId(), employee);
+  }
 
-    public void removeEmployee(int id) {
-        employees.remove(id);
-    }
+  public void removeEmployee(int id) {
+    employees.remove(id);
+  }
 
-    public int getEmployeeCount() {
-        return employees.size();
-    }
+  public int getEmployeeCount() {
+    return employees.size();
+  }
 }
--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
@@ -1,6 +1,7 @@
 package com.codedifferently.lesson15;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
 class Lesson15Test {
