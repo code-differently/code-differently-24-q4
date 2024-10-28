ec2ce623ea27baf720d06f48285829e64d954c7c
AmiyahJo <ajones244@gmail.com>
2024-10-28 14:11:41 +0000

    feat: adds imports fix: moved string orderid in .java outside of try block

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/Lesson14.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/Lesson14.java
@@ -1,13 +1,17 @@
 package com.codedifferently.lesson14;
 
+import com.codedifferently.lesson14.ecommerce.EcommerceSystem;
+import com.codedifferently.lesson14.ecommerce.OrderNotFoundException;
+import com.codedifferently.lesson14.ecommerce.ProductNotFoundException;
+
 public class Lesson14 {
 
   public static void main(String[] args) {
-    // System.out.println("Hello World");
+  String orderId = null;
     try {
-      String orderId = ecommerceSystem.placeOrder("1", 1);  
+      orderId = ecommerceSystem.placeOrder("1", 1);  
     } catch (ProductNotFoundException e) {
-      System.out.println("Product with ID " + orderId + " not found.");
+      System.out.println("Product with ID " + "1" + " not found.");
     } catch (OrderNotFoundException e){
       System.out.println("Order with ID " + orderId + " not found.");
     }
