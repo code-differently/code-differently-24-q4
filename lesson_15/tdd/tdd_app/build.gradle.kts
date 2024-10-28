1f3b6cd1f56c065d824346bbd70bb41e30b8c8d8
Anthony D. Mays <anthony@morganlatimer.com>
2024-10-28 15:58:29 +0000

    chore: add test coverage
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_15/tdd/tdd_app/build.gradle.kts
+++ lesson_15/tdd/tdd_app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
     eclipse
+    jacoco
     id("com.diffplug.spotless") version "6.25.0"
 	  id("org.springframework.boot") version "3.2.2"
     id("com.adarshr.test-logger") version "4.0.0"
@@ -38,6 +39,14 @@ application {
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+  dependsOn(tasks.test)
+  reports {
+    xml.required = true
+  }
 }
 
 
