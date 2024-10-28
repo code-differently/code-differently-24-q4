8f6142f208a2b89f6bbe3cc529fcf26f387b0817
AmiyahJo <ajones244@gmail.com>
2024-10-28 14:37:27 +0000

    feat: adds new exceptoons in ecommerance , adds more code in both exceptions to extend

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/Lesson14.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/Lesson14.java
@@ -1,20 +1,21 @@
 package com.codedifferently.lesson14;
 
-import com.codedifferently.lesson14.ecommerce.EcommerceSystem;
-import com.codedifferently.lesson14.ecommerce.OrderNotFoundException;
-import com.codedifferently.lesson14.ecommerce.ProductNotFoundException;
+// import com.codedifferently.lesson14.ecommerce.EcommerceSystem;
+// import com.codedifferently.lesson14.ecommerce.OrderNotFoundException;
+// import com.codedifferently.lesson14.ecommerce.ProductNotFoundException;
 
 public class Lesson14 {
 
   public static void main(String[] args) {
-  String orderId = null;
-    try {
-      orderId = ecommerceSystem.placeOrder("1", 1);  
-    } catch (ProductNotFoundException e) {
-      System.out.println("Product with ID " + "1" + " not found.");
-    } catch (OrderNotFoundException e){
-      System.out.println("Order with ID " + orderId + " not found.");
-    }
+    System.out.println("Hello world");
+  // String orderId = null;
+  //   try {
+  //     orderId = ecommerceSystem.placeOrder("1", 1);  
+  //   } catch (ProductNotFoundException e) {
+  //     System.out.println("Product with ID " + "1" + " not found.");
+  //   } catch (OrderNotFoundException e){
+  //     System.out.println("Order with ID " + orderId + " not found.");
+  //   }
   }
 }
 
