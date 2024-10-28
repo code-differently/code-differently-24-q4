4afa2555dae40b77bc25c30477b35aaa12511e33
AmiyahJo <ajones244@gmail.com>
2024-10-28 14:41:14 +0000

    fix: gradlew spotless to fix violations

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/Lesson14.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/Lesson14.java
@@ -8,16 +8,16 @@ public class Lesson14 {
 
   public static void main(String[] args) {
     System.out.println("Hello world");
-  // String orderId = null;
-  //   try {
-  //     orderId = ecommerceSystem.placeOrder("1", 1);  
-  //   } catch (ProductNotFoundException e) {
-  //     System.out.println("Product with ID " + "1" + " not found.");
-  //   } catch (OrderNotFoundException e){
-  //     System.out.println("Order with ID " + orderId + " not found.");
-  //   }
+    // String orderId = null;
+    //   try {
+    //     orderId = ecommerceSystem.placeOrder("1", 1);
+    //   } catch (ProductNotFoundException e) {
+    //     System.out.println("Product with ID " + "1" + " not found.");
+    //   } catch (OrderNotFoundException e){
+    //     System.out.println("Order with ID " + orderId + " not found.");
+    //   }
   }
 }
 
-//ProductNotFoundException <- for product does not exist
-//OrderNotFoundException <- for both order does not exist and order cancelled
+// ProductNotFoundException <- for product does not exist
+// OrderNotFoundException <- for both order does not exist and order cancelled
