efbbe3b69693fc8ad82e911db3bec5ca2f435f7c
AmiyahJo <ajones244@gmail.com>
2024-10-29 14:51:03 +0000

    feat: testAddEmployee test is added.
    not finished , still figuring out the assert method

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -1,12 +1,24 @@
 package com.codedifferently.lesson15;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 public class EmployeeManagerTest {
     @Test
     void testAddEmployee() {
+        // employees.put(employee.getId(), employee);
         //Arrange
+        List<Employee> employees = new ArrayList<>();
+        Employee employee = new Employee(12345, "Engineering", "Alice Johnson", 75000);
+        
         //Act
+        employees.add(employee);
+
+        //Assert
+        
+        
     }
 
     @Test
