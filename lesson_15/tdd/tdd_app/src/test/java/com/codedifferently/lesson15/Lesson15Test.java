915b28539fecf72fbd3103ebe20b1a536d5a9936
haldanek <khaldane244@gmail.com>
2024-10-29 19:45:21 +0000

    fix: fixed lesson15test file

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
@@ -1,19 +1,32 @@
 package com.codedifferently.lesson15;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+
+
 
 class Lesson15Test {
 
-  @Test
-  public void testLesson15() {
-    assertThat(new Lesson15()).isNotNull();
-  }
+  private EmployeeManager manager;
+  private Employee emp;
 
-  @Test
-  public void testGetGreeting() {
-    // Act
-    Lesson15.main(null);
+  @BeforeEach
+  public void conStant() {
+    manager = new EmployeeManager();
+     emp = new Employee(1, "Kimberlee Haldane", null, 0);
   }
+  
+  @Test
+   public void testAddEmployee() {
+   // Act
+   manager.addEmployee(emp);
+    // Assert
+    assertEquals(1, manager.getEmployeeCount()); 
+    assertEquals(emp, manager.getEmployee(1)); 
+    }
+
 }
+
