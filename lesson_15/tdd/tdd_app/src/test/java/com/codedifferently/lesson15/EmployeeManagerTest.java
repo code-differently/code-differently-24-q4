61e99da3a5d653afd7d1afd40dc3ed68766423a7
haldanek <khaldane244@gmail.com>
2024-10-29 14:22:00 +0000

    feat:added test files

---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -0,0 +1,16 @@
+package com.codedifferently.lesson15;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeManagerTest {
+   
+    @Test
+    public void testEmployeeManager() {
+        assertThat()
+    }
+}
+
+
+
---
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeTest.java
@@ -0,0 +1,13 @@
+package com.codedifferently.lesson15;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+    
+    @Test
+    public void testEmployee() {
+        assertThat()
+    }
+}
