50f806fa1ebad3470acb472a0dbc7b5d8d76430c
PabLim-Par <plimon244@gmail.com>
2024-10-30 04:06:33 +0000

    Feat: fix EmployeeManager test

--- lesson_15/tdd/tdd_app/src/test/java/EmployeeManagerTest.java
+++ lesson_15/tdd/tdd_app/src/test/java/EmployeeManagerTest.java
@@ -1,12 +1,11 @@
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import com.codedifferently.lesson15.Employee;
 import com.codedifferently.lesson15.EmployeeManager;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class EmployeeManagerTest {
   Employee employee;
@@ -42,13 +41,13 @@ public class EmployeeManagerTest {
     employees.put(employee.getId(), employee);
 
     employeeNew = new Employee(15, "Paul", "President", 200);
-    
+
     employees.put(employeeNew.getId(), employeeNew);
-    
+
     employeeManager.addEmployee(employee);
-    
+
     employeeManager.updateEmployee(employeeNew);
-    
+
     assertEquals(
         employeeManager.getEmployee(employeeNew.getId()), employees.get(employeeNew.getId()));
   }
@@ -56,33 +55,31 @@ public class EmployeeManagerTest {
   @Test
   void testRemoveEmployee() {
     employees.put(employee.getId(), employee);
-    
+
     employees.remove(employee.getId(), employee);
-    
+
     employeeManager.addEmployee(employee);
-    
+
     employeeManager.removeEmployee(employee.getId());
-    
+
     assertEquals(0, employeeManager.getEmployeeCount());
   }
 
   @Test
-  void testAssertEmployeeInCollection() throws IllegalArgumentException {
-    
-  }
+  void testAssertEmployeeInCollection() throws IllegalArgumentException {}
 
   @Test
   void testGetEmployeeCount() {
     employees.put(employee.getId(), employee);
-    
+
     employeeNew = new Employee(16, "Paul", "President", 200);
-    
+
     employees.put(employeeNew.getId(), employeeNew);
-    
+
     employeeManager.addEmployee(employee);
-    
+
     employeeManager.addEmployee(employeeNew);
-    
+
     assertEquals(2, employeeManager.getEmployeeCount());
   }
 }
