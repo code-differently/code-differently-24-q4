878e8f4685794a44cb51352ef1219844ff4e90c0
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-10-30 13:34:18 +0000

    Feat: Adds Shawn Dunsmore Jr Lesson14 Fix SpotlessApply

--- lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/EcommerceSystem.java
+++ lesson_14/exceptions/exceptions_app/src/main/java/com/codedifferently/lesson14/ecommerce/EcommerceSystem.java
@@ -19,22 +19,22 @@ public class EcommerceSystem {
 
   public String placeOrder(String productId, int quantity) throws ProductNotFoundException {
     Product product = products.get(productId);
-    if (product == null){
-      throw new ProductNotFoundException("Product with ID "+ productId +" not found");
+    if (product == null) {
+      throw new ProductNotFoundException("Product with ID " + productId + " not found");
     }
     String orderId = UUID.randomUUID().toString();
     orders.put(orderId, new Order(orderId, product, quantity));
     return orderId;
   }
 
-  public void cancelOrder(String orderId){
+  public void cancelOrder(String orderId) {
     orders.remove(orderId);
   }
 
   public String checkOrderStatus(String orderId) throws OrderNotFoundException {
     Order order = orders.get(orderId);
-    if (order == null){
-      throw new OrderNotFoundException("Order with ID "+ orderId+  " not found");
+    if (order == null) {
+      throw new OrderNotFoundException("Order with ID " + orderId + " not found");
     }
     return "Order ID: "
         + orderId
--- lesson_14/exceptions/exceptions_app/src/test/java/com/codedifferently/lesson14/ecommerce/EcommerceSystemTest.java
+++ lesson_14/exceptions/exceptions_app/src/test/java/com/codedifferently/lesson14/ecommerce/EcommerceSystemTest.java
@@ -2,6 +2,7 @@ package com.codedifferently.lesson14.ecommerce;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
