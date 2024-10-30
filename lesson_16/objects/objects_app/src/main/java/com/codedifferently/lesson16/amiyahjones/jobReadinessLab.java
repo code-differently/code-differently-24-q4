58d784bf929067f3a9cc25748e1c89c5a68ce05e
AmiyahJo <ajones244@gmail.com>
2024-10-30 20:21:51 +0000

    feat: adds 5 variables
    int , string , arrayList as the three different types

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
@@ -1,3 +1,11 @@
 public class JobReadinessLab {
+    //variables contribute towards success like mentoring session , job prep, social support
+    int mentorSession;
+    String teacherAssistant; 
+    String socialSupport;
+    String tutor;
+    //Array students;
+
+    
 
 }
