dac021bd7f53dd11dcf8ac7529ca3e2b7c09391d
AmiyahJo <ajones244@gmail.com>
2024-10-30 20:45:00 +0000

    feat: adds students

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class JobReadinessLab {
     //variables contribute towards success like mentoring session , job prep, social support
     int mentorSession;
@@ -5,7 +7,6 @@ public class JobReadinessLab {
     String socialSupport;
     String jobTutor;
     boolean isSuccessful;
-    //a list of students;
-   
+    ArrayList<String> students;
 }
 
