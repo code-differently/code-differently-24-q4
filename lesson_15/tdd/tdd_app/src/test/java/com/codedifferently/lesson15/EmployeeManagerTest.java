73db7560e24f8c77443112a1407610ea2ecc049b
cdbluejr <cdblue244@gmail.com>
2024-10-30 16:06:42 +0000

    feat: Lesson 15 Util Tests by Dwight Blue

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -1,72 +1,69 @@
 package com.codedifferently.lesson15;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class EmployeeManagerTest {
 
-    EmployeeManager manager;
+  EmployeeManager manager;
 
-    @BeforeEach
-    void setUp() {
+  @BeforeEach
+  void setUp() {
     manager = new EmployeeManager();
     Map<Integer, Employee> employee1 = new HashMap<>();
-    Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
+    Employee employee = new Employee(1234, "Pierson", "Tech Support", 5.00);
+  }
 
-}
-@Test
-    public void testGetEmployee () { 
-        // Given
-        Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
-        // When
-        manager.addEmployee(employee);
+  @Test
+  public void testGetEmployee() {
+    // Given
+    Employee employee = new Employee(1234, "Pierson", "Tech Support", 5.00);
+    // When
+    manager.addEmployee(employee);
 
-       // Then
-       assertNotNull(employee.getId());
-    }
-    
-@Test
-    public void testUpdateEmployee () { 
-        // Given
-        Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
-        // When
-        manager.addEmployee(employee);
-        manager.updateEmployee(employee);
+    // Then
+    assertNotNull(employee.getId());
+  }
 
-        // Then
-        assertEquals(1234,employee.getId());
-        
-    }
+  @Test
+  public void testUpdateEmployee() {
+    // Given
+    Employee employee = new Employee(1234, "Pierson", "Tech Support", 5.00);
+    // When
+    manager.addEmployee(employee);
+    manager.updateEmployee(employee);
 
-@Test
+    // Then
+    assertEquals(1234, employee.getId());
+  }
 
-    public void testGetEmployeeCount() {
-        // Given
-        Employee employee1 = new Employee(1234,"Pierson","Tech Support",5.00);
-        Employee employee2 = new Employee(2345,"George","President",10);
-        // When
-        manager.addEmployee(employee1);
-        manager.addEmployee(employee2);
-        
-        // Then
-        assertEquals(2,manager.getEmployeeCount());
-    }
+  @Test
+  public void testGetEmployeeCount() {
+    // Given
+    Employee employee1 = new Employee(1234, "Pierson", "Tech Support", 5.00);
+    Employee employee2 = new Employee(2345, "George", "President", 10);
+    // When
+    manager.addEmployee(employee1);
+    manager.addEmployee(employee2);
 
-@Test
-    public void testRemoveEmployee () { 
-        // Given
-        Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
-        // When
-        manager.addEmployee(employee);
-        manager.removeEmployee(employee.getId());
+    // Then
+    assertEquals(2, manager.getEmployeeCount());
+  }
 
-        // Then
-        assertEquals(1234,employee.getId());
-        
-    }
+  @Test
+  public void testRemoveEmployee() {
+    // Given
+    Employee employee = new Employee(1234, "Pierson", "Tech Support", 5.00);
+    // When
+    manager.addEmployee(employee);
+    manager.removeEmployee(employee.getId());
+
+    // Then
+    assertEquals(1234, employee.getId());
+  }
 }
--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeTest.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeTest.java
@@ -1,88 +1,80 @@
 package com.codedifferently.lesson15;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class EmployeeTest {
-    
-    Employee employee;
 
-    @BeforeEach
+  Employee employee;
+
+  @BeforeEach
   void setUp() {
-    employee = new Employee(1234,"Pierson","Tech Support",5.00);
+    employee = new Employee(1234, "Pierson", "Tech Support", 5.00);
   }
-  
+
   @Test
-    public void testGetId () {
-        // Given
-        // int expectedId = 1234;
-        employee.setId(1234);
+  public void testGetId() {
+    // Given
+    // int expectedId = 1234;
+    employee.setId(1234);
 
-        //when
-        // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
+    // when
+    // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
 
-        // Then
-        int actualId = employee.getId();
-        int expectedId = 1234;
-        
+    // Then
+    int actualId = employee.getId();
+    int expectedId = 1234;
 
-        assertEquals(expectedId,actualId);
+    assertEquals(expectedId, actualId);
+  }
 
-    }
+  @Test
+  public void testGetName() {
+    // Given
+    // String expectedName = "Pierson";
+    employee.setName("Pierson");
 
-    @Test
-    public void testGetName () {
-        // Given
-        // String expectedName = "Pierson";
-        employee.setName("Pierson");
+    // when
+    // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
 
-        //when
-        // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
+    // Then
+    String actualName = employee.getName();
+    String expectedName = "Pierson";
 
-        // Then
-        String actualName = employee.getName();
-        String expectedName = "Pierson";
+    assertEquals(expectedName, actualName);
+  }
 
+  @Test
+  public void testGetDepartment() {
+    // Given
+    // String expectedDept = "Tech Support";
+    employee.setDepartment("Tech Support");
 
-        assertEquals(expectedName,actualName);
+    // when
+    // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
 
-    }
+    // Then
+    String actualDept = employee.getDepartment();
+    String expectedDept = "Tech Support";
 
-       @Test
-    public void testGetDepartment () {
-        // Given
-        // String expectedDept = "Tech Support";
-        employee.setDepartment("Tech Support");
+    assertEquals(expectedDept, actualDept);
+  }
 
-        //when
-        // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
+  @Test
+  public void testGetSalary() {
+    // Given
+    // double expectedSalary = 5.00;
+    employee.setSalary(5.00);
 
-        // Then
-        String actualDept = employee.getDepartment();
-        String expectedDept = "Tech Support";
+    // when
+    // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
 
-        assertEquals(expectedDept,actualDept);
+    // Then
+    double actualSalary = employee.getSalary();
+    double expectedSalary = 5.00;
 
-    }
-
-       @Test
-    public void testGetSalary () {
-        // Given
-        // double expectedSalary = 5.00;
-        employee.setSalary(5.00);
-
-        //when
-        // Employee employee = new Employee(1234,"Pierson","Tech Support",5.00);
-
-        // Then
-        double actualSalary = employee.getSalary();
-        double expectedSalary = 5.00;
-
-        assertEquals(expectedSalary,actualSalary);
-
-    }
-
-
+    assertEquals(expectedSalary, actualSalary);
+  }
 }
