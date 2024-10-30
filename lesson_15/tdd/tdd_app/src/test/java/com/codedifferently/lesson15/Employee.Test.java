1472f2182d519330c8cfe096aa38bd15ccc60d90
nilejack <njacksonnorthrup244@gmail.com>
2024-10-30 16:07:23 +0000

    Feat: Changes to Lesson15.java files

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Employee.Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Employee.Test.java
@@ -1,79 +1,80 @@
 package com.codedifferently.lesson15;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EmployeeTest {
 
-    Employee employee;
+  Employee employee;
 
-    // Set up
-    @BeforeEach
-    void setUp() {
-        employee = new Employee(7, "Nile", "SWE", 65000.00);
-    }
+  // Set up
+  @BeforeEach
+  void setUp() {
+    employee = new Employee(7, "Nile", "SWE", 65000.00);
+  }
 
-    @Test
-    public void testGetId() {
-        int actualId = employee.getId();
-        int expectedId = 7;
-        assertEquals(expectedId, actualId);
-    }
+  @Test
+  public void testGetId() {
+    int actualId = employee.getId();
+    int expectedId = 7;
+    assertEquals(expectedId, actualId);
+  }
 
-    @Test
-    public void testSetID() {
-        int expectedSetId = 0;
-        employee.setId(expectedSetId);
-        assertEquals(expectedSetId, employee.getId());
-    }
+  @Test
+  public void testSetID() {
+    int expectedSetId = 0;
+    employee.setId(expectedSetId);
+    assertEquals(expectedSetId, employee.getId());
+  }
 
-    @Test
-    public void testGetName() {
-        String expectedName = "Nile";
-        String actualName = employee.getName();
-        assertEquals(expectedName, actualName);
-    }
+  @Test
+  public void testGetName() {
+    String expectedName = "Nile";
+    String actualName = employee.getName();
+    assertEquals(expectedName, actualName);
+  }
 
-    @Test
-    public void testSetName() {
-        String expectedSetName = "Nile";
-        employee.setName(expectedSetName);
-        assertEquals(expectedSetName, employee.getName());
-    }
+  @Test
+  public void testSetName() {
+    String expectedSetName = "Nile";
+    employee.setName(expectedSetName);
+    assertEquals(expectedSetName, employee.getName());
+  }
 
-    @Test
-    public void testGetDepartment() {
-        String expectedDepartment = "SWE";
-        String actualDepartment = employee.getDepartment();
-        assertEquals(expectedDepartment, actualDepartment);
-    }
+  @Test
+  public void testGetDepartment() {
+    String expectedDepartment = "SWE";
+    String actualDepartment = employee.getDepartment();
+    assertEquals(expectedDepartment, actualDepartment);
+  }
 
-    @Test
-    public void testSetDepartment() {
-        String expectedSetDepartment = "Tech";
-        employee.setDepartment(expectedSetDepartment);
-        assertEquals(expectedSetDepartment, employee.getDepartment());
-    }
+  @Test
+  public void testSetDepartment() {
+    String expectedSetDepartment = "Tech";
+    employee.setDepartment(expectedSetDepartment);
+    assertEquals(expectedSetDepartment, employee.getDepartment());
+  }
 
-    @Test
-    public void testGetSalary() {
-        double expectedSalary = 65000.00;
-        double actualSalary = employee.getSalary();
-        assertEquals(expectedSalary, actualSalary);
-    }
+  @Test
+  public void testGetSalary() {
+    double expectedSalary = 65000.00;
+    double actualSalary = employee.getSalary();
+    assertEquals(expectedSalary, actualSalary);
+  }
 
-    @Test
-    public void testSetSalary() {
-        double expectedSetSalary = 100000.00;
-        employee.setSalary(expectedSetSalary);
-        assertEquals(expectedSetSalary, employee.getSalary());
-    }
+  @Test
+  public void testSetSalary() {
+    double expectedSetSalary = 100000.00;
+    employee.setSalary(expectedSetSalary);
+    assertEquals(expectedSetSalary, employee.getSalary());
+  }
 
-    @Test
-    public void testGetDetails() {
-        String actualDetails = employee.getDetails();
-        String expectedDetails = "id: 7 name: Nile department: SWE salary: 65000.0";
-        assertEquals(expectedDetails, actualDetails);
-    }
+  @Test
+  public void testGetDetails() {
+    String actualDetails = employee.getDetails();
+    String expectedDetails = "id: 7 name: Nile department: SWE salary: 65000.0";
+    assertEquals(expectedDetails, actualDetails);
+  }
 }
--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -1,91 +1,89 @@
 package com.codedifferently.lesson15;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class EmployeeManagerTest {
-    private EmployeeManager employeeManager;
-    Employee employee1;
-    private int count;
+  private EmployeeManager employeeManager;
+  private Map<Integer, Employee> employees;
+  Employee employee1;
+  Employee employee2;
+  
 
-    @BeforeEach
-    public void setup() {
-        Employee employee1 = new Employee(1, "Nile", "SWE", 65000.00);
-        Employee employee2 = new Employee(2, "Chino", "Marketing", 70000.00);
-        employeeManager = new EmployeeManager();
-    }
+  @BeforeEach
+  public void setup() {
+    employees = new HashMap<Integer, Employee>();
+    employees.put(1, new Employee(1, "Nile", "SWE", 65000.00));
+   
+    employeeManager = new EmployeeManager();
+  }
 
-    @Test
-    public void testEmployeeManagement() {
-    }
-        // Create Employee objects
-        
-        @Test
-        public void addEmployee() { // Use addEmployee to add employees
-        employeeManager.addEmployee(employee1);
-        employeeManager.addEmployee(employee2);
-        }
+  @Test
+  public void testEmployeeManagement() {}
 
-        @Test// Invoke getEmployeeCount
-        public void getEmployee() {
-        int count = employeeManager.getEmployeeCount();
-        assertThat(count).isEqualTo(2); 
-        
+  // Create Employee objects
 
-    
-         
-        Employee retrievedEmployee = employeeManager.getEmployee(1);
-        assertThat(retrievedEmployee.getName()).isEqualTo("Nile");
-        }
+  @Test
+  public void addEmployee() { // Use addEmployee to add employees
+   assertThat(employees).hasSize(1);
+    employees.put(2,new Employee(2, "Chino", "Marketing", 70000.00));
+    assertThat(employees).hasSize(2);
+  }
+   
 
-        @Test
-        public void updateEmployee() {
-        employee1.setSalary(70000.00); 
-        employeeManager.updateEmployee(employee1);
+  @Test // Invoke getEmployeeCount
+  public void getEmployee() {
+    int count = employeeManager.getEmployeeCount();
+    assertThat(employees).hasSize(2);
 
-    
-        Employee updatedEmployee = employeeManager.getEmployee(1);
-        assertThat(updatedEmployee.getSalary()).isEqualTo(70000.00); 
-        }
+    Employee retrievedEmployee = employeeManager.getEmployee(2);
+    assertThat(retrievedEmployee.getName()).hasSize(2);
+  }
 
-        @Test
-        public void getEmployeeCount() {
-        employeeManager.removeEmployee(2);
-        count = employeeManager.getEmployeeCount();
-        }
-        
-         
-        @Test
-        public void getEmployees() {
-        assertThat(count).isEqualTo(1); 
+  @Test
+  public void updateEmployee() {
+     employee1.put(new Employee (1, "Nile", "SWE", 65000.00));
+    employee1.setSalary(70000.00);
+    employeeManager.updateEmployee(employee1);
 
-        
-        Map<Integer, Employee> employeeMap = employeeManager.getEmployees();
-        assertThat(employeeMap.size()).isEqualTo(1);
-        } 
+    Employee updatedEmployee = employeeManager.getEmployee(1);
+    assertThat(updatedEmployee.getSalary()).isEqualTo(70000.00);
+  }
 
-        @Test 
-        public void setEmployees() {
-        Map<Integer, Employee> newEmployees = new HashMap<>();
-        newEmployees.put(3, new Employee(3, "Charlie", "HR", 75000.00));
-        employeeManager.setEmployees(newEmployees);
-        
+  @Test
+  public void getEmployeeCount() {
+    employeeManager.removeEmployee(2);
+    count = employeeManager.getEmployeeCount();
+  }
 
-        
-        assertThat(employeeManager.getEmployees().size()).isEqualTo(1); 
-    }
-    @Test
-    public void testGetDetails() {
-     String actualDetails = employee1.getDetails();
-        String expectedDetails = "id: 7 name: Nile department: SWE salary: 65000.0";
-        assertEquals(expectedDetails, actualDetails);
+  @Test
+  public void getEmployees() {
+    employees.put(1, new Employee(1, "Nile", "SWE", 65000.00));
+    assertThat(employees).hasSize(1);
+    Map<Integer, Employee> employeeMap = employeeManager.getEmployees();
+    employeeMap.put(count, employee1);
+    assertThat(employeeMap.size()).isEqualTo(1);
+  }
 
-    }
+  @Test
+  public void setEmployees() {
+    Map<Integer, Employee> newEmployees = new HashMap<>();
+    newEmployees.put(3, new Employee(3, "Charlie", "HR", 75000.00));
+    employeeManager.setEmployees(newEmployees);
+
+    assertThat(employeeManager.getEmployees().size()).isEqualTo(1);
+  }
+
+  @Test
+  public void testGetDetails() {
+    String actualDetails = employee1.getDetails();
+    String expectedDetails = "id: 7 name: Nile department: SWE salary: 65000.0";
+    assertEquals(expectedDetails, actualDetails);
+  }
 }
-        
-       
