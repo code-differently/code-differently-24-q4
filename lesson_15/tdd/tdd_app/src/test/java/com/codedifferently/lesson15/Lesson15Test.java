3b4a5e5f5405e9dc966215234d90890245e18a3f
haldanek <khaldane244@gmail.com>
2024-10-30 02:22:46 +0000

    chore: added second test

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Lesson15Test.java
@@ -5,28 +5,35 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-
-
 class Lesson15Test {
 
   private EmployeeManager manager;
   private Employee emp;
 
   @BeforeEach
-  public void conStant() {
+  public void setUp() {
     manager = new EmployeeManager();
-     emp = new Employee(1, "Kimberlee Haldane", null, 0);
+    emp = new Employee(1, "Edgar Allen Poe");
   }
-  
+
   @Test
-   public void testAddEmployee() {
-   // Act
-   manager.addEmployee(emp);
+  public void testAddEmployee() {
+    // Act
+    manager.addEmployee(emp);
     // Assert
-    assertEquals(1, manager.getEmployeeCount()); 
-    assertEquals(emp, manager.getEmployee(1)); 
-    }
+    assertEquals(1, manager.getEmployeeCount());
+    assertEquals(emp, manager.getEmployee(1));
+  }
 
+  @Test
+  public void testGetEmployee() {
+    // Arrange
+    int key = 1;
+    Employee expectedValue = new Employee(1, "Edgar Allen Poe");
+    manager.addEmployee(expectedValue);
+    // Act
+    Employee actualValue = manager.getEmployee(key);
+    // Assert
+    assertEquals(expectedValue, actualValue, "The value retrieved should match the value added.");
+  }
 }
-
