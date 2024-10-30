475329e947085fd067802bdd66ed85d9de5ea6b9
AmiyahJo <ajones244@gmail.com>
2024-10-30 20:50:45 +0000

    chore: remove comments

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 
 public class JobReadinessLab {
-    //variables contribute towards success like mentoring session , job prep, social support
     int mentorSession;
     String teacherAssistant; 
     String socialSupport;
     String jobTutor;
     boolean isSuccessful;
     ArrayList<String> students;
+
 }
 
