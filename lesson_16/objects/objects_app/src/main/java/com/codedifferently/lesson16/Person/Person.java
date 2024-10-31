44faa1c775afc406aef52b2aaac339b59f049744
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-10-31 14:34:56 +0000

    feat: added 7 member variables of 5 different types; added Person function; added 'get' function for each member variable; added set function for each variable; added throw arguement to getHairColor to distinguish if person is bald; added getNaturalHairColor function with loop to distinguish original hair color from dyed; added getPersonInfo function that returns the information of the person;

---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Person/Person.java
@@ -0,0 +1,119 @@
+package com.codedifferently.lesson16;
+
+import java.util.ArrayList;
+
+public class Person {
+    
+    private String name;
+    private String sex;
+    private String race;
+    private int age;
+    private boolean alive;
+    private double height;
+    private ArrayList<String> hairColor;
+ 
+    
+
+    public Person(String name, String sex, String race, int age, double height, ArrayList hairColor, boolean alive){
+        this.name = name;
+        this.sex = sex;
+        this.race = race;
+        this.age = age;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.alive = alive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public ArrayList<String> getHairColor() {
+        if (hairColor == null) {
+            throw new IllegalArgumentException("No hair color? This person must be bald!");
+        }
+        return hairColor;
+    }
+
+    public void setHairColor(ArrayList hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public ArrayList<String> getNaturalHairColor() {
+        ArrayList<String> naturalHairColor = new ArrayList<>();
+        ArrayList<String> possibleNaturalColor = new ArrayList<>();
+        possibleNaturalColor.add("Black");
+        possibleNaturalColor.add("Brown");
+        possibleNaturalColor.add("Blonde");
+        possibleNaturalColor.add("Ginger");
+        possibleNaturalColor.add("Auburn");
+        possibleNaturalColor.add("Albino");
+        possibleNaturalColor.add("Grey");
+        
+        for (String color : hairColor) {
+            if (possibleNaturalColor.contains(color)) {
+                naturalHairColor.add(color);
+                break;
+            }
+        }
+        return naturalHairColor;
+    }
+
+    public String getLifeStatus() {
+        if (alive == true) {
+            return "alive";
+        } else {
+            return "deceased";
+        }
+    }
+
+    public void setLifeStatus(boolean alive) {
+       this.alive = alive;
+    }
+
+    public String getPersonInfo() {
+        return "Name: " + name +
+         "Gender Identity: " + sex +
+         "Race: " + race +
+         "Age: " + age +
+         "Height(In meters): " +  height +
+         "Hair Color: " + hairColor +
+         "Status: " + getLifeStatus();
+    }
+}
