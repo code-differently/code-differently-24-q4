10ccc03d11e1375b853dd90b3bb5538cb99d1672
AmiyahJo <ajones244@gmail.com>
2024-10-31 13:52:29 +0000

    feat: adds two member functions

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
@@ -3,20 +3,19 @@ package main.java.com.codedifferently.lesson16.amiyahjones;
 import java.util.ArrayList;
 
 public class JobReadinessLab {
-    int mentorSession;
-    String teacherAssistant, socialSupport, jobTutor;
-    boolean isSuccessful; 
-    ArrayList<String> students;
-    level readinessLevel;
+    private int mentorSession;
+    private String teacherAssistant, socialSupport, jobTutor;
+    private boolean isSuccessful; 
+    private ArrayList<String> students;
+    private level readinessLevel;
 
     enum level {
         BEGINNER , 
         INTERMEDIATE , 
         ADVANCED
-
     }
 
-    JobReadinessLab(String teacherAssistant, int mentorSession){
+    public JobReadinessLab(String teacherAssistant, int mentorSession){
         this.mentorSession = mentorSession;
         this.teacherAssistant = teacherAssistant;
         socialSupport = "Estelle";
@@ -25,6 +24,15 @@ public class JobReadinessLab {
         readinessLevel = level.BEGINNER;
     }
 
-   
+   public Boolean verifyisSuccessful() {
+    if (!isSuccessful){
+        System.out.println("At least you got to know what software engineering is like!");
+    }
+    return isSuccessful;
+   }
+
+   public int getStudentCount() {
+    return students.size();
+   }
 }
 
