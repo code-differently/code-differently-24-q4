939fea9afad17d13acfb9a96dc851b933a288170
AmiyahJo <ajones244@gmail.com>
2024-10-31 14:50:12 +0000

    feat: adds tests

---
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessLabTest.java
@@ -0,0 +1,52 @@
+package com.codedifferently.lesson16.amiyahjones;
+
+import org.junit.jupiter.api.Test;
+
+public class JobReadinessLabTest {
+
+    @Test
+    void testAddStudent() {
+        
+    }
+
+    @Test
+    void testCheckReadiness() {
+        
+    }
+
+    @Test
+    void testCountStudents() {
+        
+    }
+
+    @Test
+    void testGetJobTutor() {
+        
+    }
+
+    @Test
+    void testGetMentorSession() {
+        
+    }
+
+    @Test
+    void testGetSocialSupport() {
+        
+    }
+
+    @Test
+    void testGetTeacherAssistant() {
+        
+    }
+
+    @Test
+    void testPrintStudents() {
+        
+    }
+
+    @Test
+    void testVerifyIsSuccessful() {
+        
+    }
+    
+}
