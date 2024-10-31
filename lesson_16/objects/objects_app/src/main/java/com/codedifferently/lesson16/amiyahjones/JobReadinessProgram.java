15d505149287c45fc5f058480c46046b178f663c
AmiyahJo <ajones244@gmail.com>
2024-10-31 18:01:34 +0000

    feat: adds tests and corresponding variables in my class

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgram.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgram.java
@@ -1,7 +1,16 @@
 package com.codedifferently.lesson16.amiyahjones;
 
+import java.util.ArrayList;
+
 public class JobReadinessProgram {
-    
+    ArrayList<String> students;
+    private final level readinessLevel = null;
+
+    enum level {
+        BEGINNER , 
+        INTERMEDIATE , 
+        ADVANCED
+    }
 
     public JobReadinessProgram(){
         students = new ArrayList<>();
@@ -10,4 +19,14 @@ public class JobReadinessProgram {
     public void addStudent(String studentName){
     students.add(studentName);
    }
+
+    public int getStudentCount() {
+        int count = students.size();
+        System.out.println("Total students enrolled: " + count);
+        return count;
+    }
+
+    public String checkReadiness() {
+        return (readinessLevel == level.INTERMEDIATE) ? "Ready for job applications!" : "Needs more training.";
+    }
 }
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgramTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgramTest.java
@@ -1,8 +1,6 @@
 package com.codedifferently.lesson16.amiyahjones;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 public class JobReadinessProgramTest {
@@ -19,6 +17,39 @@ public class JobReadinessProgramTest {
         assertEquals(program.getStudentCount(), 1);
     }
 
- 
+    @Test
+    void testGetStudentCount() {
+        //Arrange
+        JobReadinessProgram program = new JobReadinessProgram();
+        program.addStudent("John");
+
+        //Act
+        int actualCount = program.getStudentCount();
+
+        //Assert
+        int expectedCount = 1;
+        assertEquals(expectedCount, actualCount);
+        
+        
+    }
+
+    @Test 
+    void testCheckReadinessBeginner() {
+        JobReadinessProgram student = new JobReadinessProgram(level.BEGINNER);
+        String result = student.checkReadiness();
+        assertEquals("Needs more training.", result);
+    }
+
+    @Test 
+    void testCheckReadinessIntermediate() {
+        JobReadinessProgram student = new JobReadinessProgram(level.INTERMEDIATE);
+        String result = student.checkReadiness();
+        assertEquals("Ready for job applications!", result);
+    }
+
+    @Test 
+    void testCheckReadinessAdvanced() {
+
+    }
     
 }
