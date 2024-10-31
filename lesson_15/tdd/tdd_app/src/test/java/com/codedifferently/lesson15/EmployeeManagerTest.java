cb39e2c044145d0db8c0e0326b0a1b2819fc5128
nilejack <njacksonnorthrup244@gmail.com>
2024-10-31 00:45:48 +0000

    Feat: fixes to EmployeeManagerTest.java

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/EmployeeManagerTest.java
@@ -1,51 +1,47 @@
 package com.codedifferently.lesson15;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class EmployeeManagerTest {
   private EmployeeManager employeeManager;
   private Map<Integer, Employee> employees;
-  Employee employee1;
-  Employee employee2;
+  private Employee employee1;
+  private Employee employee2;
 
   @BeforeEach
   public void setup() {
-    employees = new HashMap<Integer, Employee>();
-    employees.put(1, new Employee(1, "Nile", "SWE", 65000.00));
+    employees = new HashMap<>();
+    employee1 = new Employee(1, "Nile", "SWE", 65000.00);
+    employee2 = new Employee(2, "Chino", "Marketing", 70000.00);
 
     employeeManager = new EmployeeManager();
+    employeeManager.setEmployees(employees);
+    employeeManager.addEmployee(employee1);
+    employeeManager.addEmployee(employee2);
   }
 
   @Test
   public void testEmployeeManagement() {}
 
-  // Create Employee objects
-
   @Test
-  public void addEmployee() { // Use addEmployee to add employees
-    assertThat(employees).hasSize(1);
-    employees.put(2, new Employee(2, "Chino", "Marketing", 70000.00));
+  public void addEmployee() {
     assertThat(employees).hasSize(2);
   }
 
-  @Test // Invoke getEmployeeCount
+  @Test
   public void getEmployee() {
-    int count = employeeManager.getEmployeeCount();
-    assertThat(employees).hasSize(2);
-
     Employee retrievedEmployee = employeeManager.getEmployee(2);
-    assertThat(retrievedEmployee.getName()).hasSize(2);
+    assertThat(retrievedEmployee.getName()).isEqualTo("Chino");
   }
 
   @Test
   public void updateEmployee() {
-    employee1.put(new Employee(1, "Nile", "SWE", 65000.00));
     employee1.setSalary(70000.00);
     employeeManager.updateEmployee(employee1);
 
@@ -55,17 +51,14 @@ public class EmployeeManagerTest {
 
   @Test
   public void getEmployeeCount() {
-    employeeManager.removeEmployee(2);
     int count = employeeManager.getEmployeeCount();
+    assertEquals(2, count);
   }
 
   @Test
   public void getEmployees() {
-    employees.put(1, new Employee(1, "Nile", "SWE", 65000.00));
-    assertThat(employees).hasSize(1);
     Map<Integer, Employee> employeeMap = employeeManager.getEmployees();
-    employeeMap.putAll(employees);
-    assertThat(employeeMap.size()).isEqualTo(1);
+    assertThat(employeeMap.size()).isEqualTo(2);
   }
 
   @Test
@@ -80,7 +73,7 @@ public class EmployeeManagerTest {
   @Test
   public void testGetDetails() {
     String actualDetails = employee1.getDetails();
-    String expectedDetails = "id: 7 name: Nile department: SWE salary: 65000.0";
+    String expectedDetails = "id: 1 name: Nile department: SWE salary: 65000.0";
     assertEquals(expectedDetails, actualDetails);
   }
 }
