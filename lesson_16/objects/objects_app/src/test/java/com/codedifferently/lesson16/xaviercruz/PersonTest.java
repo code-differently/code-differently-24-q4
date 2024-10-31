269b67284e2e65588dbe935fc2a82904b2b10758
XavierCruz5106 <superguyman121@gmail.com>
2024-10-31 13:55:47 +0000

    chore: spotlessApply

--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/xaviercruz/PersonTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/xaviercruz/PersonTest.java
@@ -1,47 +1,55 @@
 package com.codedifferently.lesson16.xaviercruz;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 public class PersonTest {
-  Person person = new Person("That one guy right there", 25, Position.ARSONIST, new ArrayList<>(Arrays.asList("Tall", "Insane", "Funny", "something else here idk")), Color.RED);
+  Person person =
+      new Person(
+          "That one guy right there",
+          25,
+          Position.ARSONIST,
+          new ArrayList<>(Arrays.asList("Tall", "Insane", "Funny", "something else here idk")),
+          Color.RED);
 
   @Test
-  public void testGetName(){
+  public void testGetName() {
     assertEquals("That one guy right there", person.getName());
   }
 
   @Test
-  public void testChangeName(){
+  public void testChangeName() {
     person.changeName("new name right here bruh");
     assertEquals("new name right here bruh", person.getName());
   }
 
   @Test
-  public void testGetAge(){
+  public void testGetAge() {
     assertEquals(25, person.getAge());
   }
 
   @Test
-  public void testGetPosition(){
+  public void testGetPosition() {
     assertEquals(Position.ARSONIST, person.getPosition());
   }
 
   @Test
-  public void testGetTraits(){
-    assertEquals(new ArrayList<>(Arrays.asList("Tall", "Insane", "Funny", "something else here idk")), person.getTraits());
+  public void testGetTraits() {
+    assertEquals(
+        new ArrayList<>(Arrays.asList("Tall", "Insane", "Funny", "something else here idk")),
+        person.getTraits());
   }
 
   @Test
-  public void testGetEyeColor(){
+  public void testGetEyeColor() {
     assertEquals(Color.RED, person.getEyeColor());
   }
 
- @Test
+  @Test
   public void testSetEyeColor() {
     Color color = Color.GREEN;
 
@@ -50,42 +58,80 @@ public class PersonTest {
     assertEquals(Color.GREEN, person.getEyeColor());
   }
 
-
   @Test
-  public void testIncreaseAge(){
+  public void testIncreaseAge() {
     person.increaseAge();
     assertEquals(26, person.getAge());
   }
 
   @Test
-  public void testUpdateAge(){
+  public void testUpdateAge() {
     person.updateAge(99);
     assertEquals(99, person.getAge());
   }
 
   @Test
-  public void testAddToTraitsStringArray_Exception(){
-    String[] newTraits = new String[]{"test", "test", "test", "test", "test", "test", "test"};
-    assertThrows(TooManyTraitsException.class, () -> {
-      person.addToTraits(newTraits);
-    });
+  public void testAddToTraitsStringArray_Exception() {
+    String[] newTraits = new String[] {"test", "test", "test", "test", "test", "test", "test"};
+    assertThrows(
+        TooManyTraitsException.class,
+        () -> {
+          person.addToTraits(newTraits);
+        });
   }
 
   @Test
-  public void testAddToTraitsString_Exception(){
-    Person person2 = new Person("That one guy right there", 25, Position.ARSONIST, new ArrayList<>(Arrays.asList("Tall", "Insane", "Funny", "something else here idk", "test", "test", "test","test","test","test")), Color.RED);
-    assertThrows(TooManyTraitsException.class, () -> {
-      person2.addToTraits("newTrait");
-    });
+  public void testAddToTraitsString_Exception() {
+    Person person2 =
+        new Person(
+            "That one guy right there",
+            25,
+            Position.ARSONIST,
+            new ArrayList<>(
+                Arrays.asList(
+                    "Tall",
+                    "Insane",
+                    "Funny",
+                    "something else here idk",
+                    "test",
+                    "test",
+                    "test",
+                    "test",
+                    "test",
+                    "test")),
+            Color.RED);
+    assertThrows(
+        TooManyTraitsException.class,
+        () -> {
+          person2.addToTraits("newTrait");
+        });
   }
 
   @Test
-  public void testAddToTraitsList_Exception(){
-    Person person2 = new Person("That one guy right there", 25, Position.ARSONIST, new ArrayList<>(Arrays.asList("Tall", "Insane", "Funny", "something else here idk", "test", "test", "test","test","test","test")), Color.RED);
+  public void testAddToTraitsList_Exception() {
+    Person person2 =
+        new Person(
+            "That one guy right there",
+            25,
+            Position.ARSONIST,
+            new ArrayList<>(
+                Arrays.asList(
+                    "Tall",
+                    "Insane",
+                    "Funny",
+                    "something else here idk",
+                    "test",
+                    "test",
+                    "test",
+                    "test",
+                    "test",
+                    "test")),
+            Color.RED);
     var traits = new ArrayList<String>(Arrays.asList("testTrait"));
-    assertThrows(TooManyTraitsException.class, () -> {
-      person2.addToTraits(traits);
-    });
+    assertThrows(
+        TooManyTraitsException.class,
+        () -> {
+          person2.addToTraits(traits);
+        });
   }
-
 }
