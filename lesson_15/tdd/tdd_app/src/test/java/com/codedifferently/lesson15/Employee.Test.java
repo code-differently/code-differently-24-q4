295237a8dc13057425f45b34c7dcd10ba115c261
nilejack <njacksonnorthrup244@gmail.com>
2024-10-31 00:00:39 +0000

    Feat: spotlessApply fix

--- lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Employee.Test.java
+++ lesson_15/tdd/tdd_app/src/test/java/com/codedifferently/lesson15/Employee.Test.java
@@ -1,7 +1,6 @@
 package com.codedifferently.lesson15;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
