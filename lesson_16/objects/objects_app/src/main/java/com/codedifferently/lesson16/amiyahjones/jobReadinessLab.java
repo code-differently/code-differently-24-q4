282eeecf646355d304637961db0ce9d2eb8ea051
AmiyahJo <ajones244@gmail.com>
2024-10-31 14:59:07 +0000

    chore: comment out class objects
    working on creating a test first

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/jobReadinessLab.java
@@ -3,73 +3,70 @@ package main.java.com.codedifferently.lesson16.amiyahjones;
 import java.util.ArrayList;
 
 public class JobReadinessLab {
-    private final int mentorSession;
-    private final String teacherAssistant, socialSupport, jobTutor;
-    private boolean isSuccessful; 
-    ArrayList<String> students;
-    private final level readinessLevel;
+//     private final int mentorSession;
+//     private final String teacherAssistant, socialSupport, jobTutor;
+//     private boolean isSuccessful; 
+//     ArrayList<String> students;
+//     private final level readinessLevel;
 
-    enum level {
-        BEGINNER , 
-        INTERMEDIATE , 
-        ADVANCED
-    }
+//     enum level {
+//         BEGINNER , 
+//         INTERMEDIATE , 
+//         ADVANCED
+//     }
 
-    public JobReadinessLab(String teacherAssistant, int mentorSession){
-        this.mentorSession = mentorSession;
-        this.teacherAssistant = teacherAssistant;
-        socialSupport = "Estelle";
-        jobTutor = "Anthony";
-        students = new ArrayList<>();
-        readinessLevel = level.BEGINNER;
-    }
+//     public JobReadinessLab(String teacherAssistant, int mentorSession){
+//         this.mentorSession = mentorSession;
+//         this.teacherAssistant = teacherAssistant;
+//         socialSupport = "Estelle";
+//         jobTutor = "Anthony";
+//         students = new ArrayList<>();
+//         readinessLevel = level.BEGINNER;
+//     }
 
-   public String checkReadiness() {
-        return (readinessLevel == level.INTERMEDIATE) ? "Ready for job applications!" : "Needs more training.";
-    }
+//    public String checkReadiness() {
+//         return (readinessLevel == level.INTERMEDIATE) ? "Ready for job applications!" : "Needs more training.";
+//     }
 
-   public Boolean verifyIsSuccessful() {
-    if (isSuccessful){
-        System.out.println("Congraulations! You made it through your journey!");
-    } else {
-         System.out.println("At least you got to know what software engineering is like!");
-    }
-    return isSuccessful;
-   }
+//    public Boolean verifyIsSuccessful() {
+//     if (isSuccessful){
+//         System.out.println("Congraulations! You made it through your journey!");
+//     } else {
+//          System.out.println("At least you got to know what software engineering is like!");
+//     }
+//     return isSuccessful;
+//    }
 
-   public int countStudents() {
-    int count = students.size();
-    System.out.println("Total students enrolles: " + count);
-    return count;
-   }
+//    public int countStudents() {
+//     int count = students.size();
+//     System.out.println("Total students enrolles: " + count);
+//     return count;
+//    }
 
-   public void addStudent(String studentName){
-    students.add(studentName);
-        try {
-            throw new StudentLimitExceededException("The classroom is full");
-        }
-   }
+//    public void addStudent(String studentName){
+//     students.add(studentName);
+//    }
 
-   public void printStudents() {
-    for (String student : students) {
-        System.out.println(student);
-    }
-   }
+//    public void printStudents() {
+//     for (String student : students) {
+//         System.out.println(student);
+//     }
+//    }
 
-   public int getMentorSession () {
-    return mentorSession;
-   }
+//    public int getMentorSession () {
+//     return mentorSession;
+//    }
    
-   public String getTeacherAssistant() {
-    return teacherAssistant;
-    }
+//    public String getTeacherAssistant() {
+//     return teacherAssistant;
+//     }
 
-   public String getSocialSupport() {
-        return socialSupport;
-    }
+//    public String getSocialSupport() {
+//         return socialSupport;
+//     }
 
-   public String getJobTutor() {
-        return jobTutor;
-    }
+//    public String getJobTutor() {
+//         return jobTutor;
+//     }
 }
 
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessLabTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessLabTest.java
@@ -1,52 +1,94 @@
 package com.codedifferently.lesson16.amiyahjones;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class JobReadinessLabTest {
 
     @Test
     void testAddStudent() {
-        
+        //Arrange
+        Student student = new student();
+        //Act
+        students.addStudent(student);
+        //Assert
+
     }
 
     @Test
     void testCheckReadiness() {
-        
+        //Arrange
+        //Act
+        //Assert        
     }
 
     @Test
     void testCountStudents() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert        
     }
 
     @Test
     void testGetJobTutor() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert  
+
     }
 
     @Test
     void testGetMentorSession() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert        
     }
 
     @Test
     void testGetSocialSupport() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert 
+
     }
 
     @Test
     void testGetTeacherAssistant() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert 
+
     }
 
     @Test
     void testPrintStudents() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert
+
     }
 
     @Test
     void testVerifyIsSuccessful() {
-        
+        //Arrange
+
+        //Act
+
+        //Assert
+        assertEquals(false , true);
     }
     
 }
