cfdb19d24474de370ceb135de0cda69668e3a626
zionbuch <zabuchanan244@gmail.com>
2024-11-01 18:19:07 +0000

    feat: Added Zion's Bedroom Object and Test to lesson_16

---
+++ lesson_16/objects/objects_app/src/test/bedroomTest.java
@@ -0,0 +1,35 @@
+public class bedroomTest {
+    import org.junit.jupiter.api.Test;
+
+public class BedroomTest {
+
+@Test
+public void tesGetColor(){ 
+Bedroom.getColor();
+var coloris="green";
+}
+@Test
+public void testGetColor() {
+    Bedroom bedroom = new Bedroom("Green", 4, 2, true, Bedroom.MattressSize.KING);
+    String color = bedroom.getwallColor(); //access color of wall
+    var coloris= "green";
+}
+
+@Test 
+public class Bedroom { 
+    private String Bedroom;
+    private int value;
+
+    public Bedroom("Green", 4){ 
+        this.Bedroom=Bedroom;
+        this.value=value;
+    }
+
+    }
+
+public 
+}
+
+
+
+}
