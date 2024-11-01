5ade5d9e0960ba4c3e6d910c7f067ab71957b83e
nilejack <njacksonnorthrup244@gmail.com>
2024-11-01 17:53:24 +0000

    Feat: Fix spotlessApply fix

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Nile'sDog16/Dog.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Nile'sDog16/Dog.java
@@ -1,65 +1,58 @@
 package com.codedifferently.lesson16.Dog;
 
-
-
 public class Dog {
-    private final Enum Colors;
+  private final Enum Colors;
 
-    private Enum getColors() {
-        return Colors;
-    }
-    enum Colors{
-        WHITE,
-        BROWN,
-        BLONDE;
-    }
-      {
-        Colors[] colors = Colors.values();
-        for (var i = 0; i < Colors.length;i ++) {
+  private Enum getColors() {
+    return Colors;
+  }
 
-        }
+  enum Colors {
+    WHITE,
+    BROWN,
+    BLONDE;
+  }
 
-    }
+  {
+    Colors[] colors = Colors.values();
+    for (var i = 0; i < Colors.length; i++) {}
+  }
 
-        
-       
+  // Attributes
+  private int Age;
+  private String Breed;
+  private String Gender;
+  private Boolean isFed, isNotFed;
 
-   //Attributes
-    private int Age;
-    private String Breed;
-    private String Gender;
-    private Boolean isFed, isNotFed;
-    
-    //Constructor
-    public Dog(int Age, String Breed, String Gender, Enum Colors, Boolean isFed) {
-        this.Age = Age;
-        this.Breed = Breed;
-        this.Gender = Gender;
-        this.Colors = Colors;
-        this.isFed = isFed | isNotFed;
-    }
-    public static int myDogsAge(int Age) {
-      return 2;
-    }
-    public static String getbreed(String breed) {
-        return "mutt";
-    }
-    public static String getGender(String Gender) {
-        return "male";
-    }
-    public static Enum getColors(Enum Colors) {
-        return Colors;
-    } 
-    public Boolean isFed(Boolean Fed) {
-           
-        Boolean getFedStatus = isFed, isNotFed;
-           
-           return getFedStatus == Fed; 
-            }
-        
-    }
-   
+  // Constructor
+  public Dog(int Age, String Breed, String Gender, Enum Colors, Boolean isFed) {
+    this.Age = Age;
+    this.Breed = Breed;
+    this.Gender = Gender;
+    this.Colors = Colors;
+    this.isFed = isFed | isNotFed;
+  }
 
+  public static int myDogsAge(int Age) {
+    return 2;
+  }
 
+  public static String getbreed(String breed) {
+    return "mutt";
+  }
 
+  public static String getGender(String Gender) {
+    return "male";
+  }
 
+  public static Enum getColors(Enum Colors) {
+    return Colors;
+  }
+
+  public Boolean isFed(Boolean Fed) {
+
+    Boolean getFedStatus = isFed, isNotFed;
+
+    return getFedStatus == Fed;
+  }
+}
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/Nile'sDogTest16/DogTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/Nile'sDogTest16/DogTest.java
@@ -1,71 +1,64 @@
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
-
-
 
 public class DogTest {
 
+  public int getAge(int Age) {
+    return Age;
+  }
 
-    public int getAge(int Age) {
-        return Age;
-    }
-    @Test
-    public void testGetAge() {
-        int getAge = getAge(2);
-        int myDogsAge = getAge(2);
-        assertThat(getAge(2)).isNotNull();
-    
-    }
-    public String getbreed(String breed) {
-        return breed;
-    }
-        @Test
-        public void testGetBreed() {
-            String getBreed = getbreed(getbreed("Mutt"));
-             assertThat(getbreed("mutt"));
-             
-        }
-        public String getGender(String gender) {
-            return gender;
-        }
+  @Test
+  public void testGetAge() {
+    int getAge = getAge(2);
+    int myDogsAge = getAge(2);
+    assertThat(getAge(2)).isNotNull();
+  }
 
-         @Test
-         public String testGetGender() {
-             String getGender = getGender(getGender("male"));
-               String myDogsGender = testGetGender(); 
-            assertThat(getGender("male"));
-            return testGetGender();
-         }
-         public Enum getColors(Enum Colors) {
-             return Colors;
-         }
-         @Test 
-         private final Enum testGetColors(Enum Colors) {
-           Enum testGetColors = Colors;
-           return Colors;  
-        }
+  public String getbreed(String breed) {
+    return breed;
+  }
 
-         @Test
-         public void testgetColor() {
-        assertThat(getColors(null));
-        }
-        @Test
-         Boolean isFed(Boolean Fed) {
-            var isNotFed = false;
-            var isFed = true;
-           var getFedStatus = Fed | isNotFed;
-           assertThat(getFedStatus).isTrue();
-            assertThat(Fed).isFalse();
-           return getFedStatus == Fed; 
-            }
-            
-        }
-        
-        
-         
-         
-        
+  @Test
+  public void testGetBreed() {
+    String getBreed = getbreed(getbreed("Mutt"));
+    assertThat(getbreed("mutt"));
+  }
 
-    
+  public String getGender(String gender) {
+    return gender;
+  }
 
-    
+  @Test
+  public String testGetGender() {
+    String getGender = getGender(getGender("male"));
+    String myDogsGender = testGetGender();
+    assertThat(getGender("male"));
+    return testGetGender();
+  }
+
+  public Enum getColors(Enum Colors) {
+    return Colors;
+  }
+
+  @Test
+  private final Enum testGetColors(Enum Colors) {
+    Enum testGetColors = Colors;
+    return Colors;
+  }
+
+  @Test
+  public void testgetColor() {
+    assertThat(getColors(null));
+  }
+
+  @Test
+  Boolean isFed(Boolean Fed) {
+    var isNotFed = false;
+    var isFed = true;
+    var getFedStatus = Fed | isNotFed;
+    assertThat(getFedStatus).isTrue();
+    assertThat(Fed).isFalse();
+    return getFedStatus == Fed;
+  }
+}
