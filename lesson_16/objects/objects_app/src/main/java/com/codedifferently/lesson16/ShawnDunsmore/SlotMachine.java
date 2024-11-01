e776a625fa243d9ecc5ffe2b48671e75879d8550
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-11-01 20:52:43 +0000

    Feat: Adds Shawn Dunsmore Jr lesson 16 Added For Loop and Test

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/ShawnDunsmore/SlotMachine.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/ShawnDunsmore/SlotMachine.java
@@ -1,6 +1,7 @@
 package com.codedifferently.lesson16.ShawnDunsmore;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class SlotMachine {
   private int numOfSlots;
@@ -56,7 +57,51 @@ public class SlotMachine {
     return payAmount;
   }
 
+  public ArrayList<String> spin(int money) throws InvalidPayAmountException {
+    if (money < moneyNeeded) {
+      throw new InvalidPayAmountException("Amount inavalid");
+    }
+    Collections.shuffle(iconList);
+    return iconList;
+  }
+
+  public ArrayList<String> spin(int money, int numOfSpins) throws InvalidPayAmountException {
+    if (money < moneyNeeded) {
+      throw new InvalidPayAmountException("Amount inavalid");
+    }
+    for (int i = 0; i < numOfSpins; i++) {
+      Collections.shuffle(iconList);
+    }
+
+    return iconList;
+  }
+
   public int getMoneyNeeded() {
     return moneyNeeded;
   }
+
+  public void setNumOfSlots(int numOfSlots) {
+    this.numOfSlots = numOfSlots;
+  }
+
+  public void setPayAmount(int payAmount) {
+    this.payAmount = payAmount;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setBuyType(BuyType buyType) {
+    this.buyType = buyType;
+  }
+
+  public void setIconList(ArrayList<String> iconList) {
+    this.iconList = iconList;
+  }
+
+  public void setMoneyNeeded(int moneyNeeded) {
+    this.moneyNeeded = moneyNeeded;
+  }
+  
 }
