78efa0f3df242ad2284f4c3fa1d2bacd0bedd6a8
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-11-01 14:48:41 +0000

    feat: added .person to the package; added <String> to ArrayList innstances; added isEmpty to .getHairColor function to check for null and empty arrays and change the hair color to bald; added '|' to seperate information in .getPersonInfo function; created PersonTest, imported nessesary data, and added PersonTest set up; added tests for all .get and .set functions; added exception testing for .getHairColor;

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Person/Person.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Person/Person.java
@@ -1,123 +1,126 @@
-package com.codedifferently.lesson16;
+package com.codedifferently.lesson16.person;
 
 import java.util.ArrayList;
 
 public class Person {
-    
-    private String name;
-    private String sex;
-    private String race;
-    private int age;
-    private boolean alive;
-    private double height;
-    private ArrayList<String> hairColor;
- 
-    
 
-    public Person(String name, String sex, String race, int age, double height, ArrayList hairColor, boolean alive){
-        this.name = name;
-        this.sex = sex;
-        this.race = race;
-        this.age = age;
-        this.height = height;
-        this.hairColor = hairColor;
-        this.alive = alive;
-    }
+  private String name;
+  private String sex;
+  private String race;
+  private int age;
+  private boolean alive;
+  private double height;
+  private ArrayList<String> hairColor;
 
-    public String getName() {
-        return name;
-    }
+  public Person(String name, String sex, String race, int age, double height, ArrayList<String> hairColor, boolean alive) {
+    this.name = name;
+    this.sex = sex;
+    this.race = race;
+    this.age = age;
+    this.height = height;
+    this.hairColor = hairColor;
+    this.alive = alive;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getSex() {
-        return sex;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
+  public String getSex() {
+    return sex;
+  }
 
-    public String getRace() {
-        return race;
-    }
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
 
-    public void setRace(String race) {
-        this.race = race;
-    }
+  public String getRace() {
+    return race;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public void setRace(String race) {
+    this.race = race;
+  }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public double getHeight() {
-        return height;
-    }
+  public void setAge(int age) {
+    this.age = age;
+  }
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
+  public double getHeight() {
+    return height;
+  }
 
-    public ArrayList<String> getHairColor() {
-        if (hairColor == null) {
-            try {
-                throw new IllegalArgumentException("No hair color? This person must be bald!");
-            } catch (IllegalArgumentException e) {
-                hairColor.add("Bald");
-            }
-        }
-        return hairColor;
-    }
+  public void setHeight(double height) {
+    this.height = height;
+  }
 
-    public void setHairColor(ArrayList hairColor) {
-        this.hairColor = hairColor;
+  public ArrayList<String> getHairColor() {
+    if (hairColor == null || hairColor.isEmpty()) {
+      this.hairColor = new ArrayList<>();
+      hairColor.add("Bald");
+      throw new IllegalArgumentException("No hair color? This person must be bald!");
     }
+    return hairColor;
+  }
 
-    public ArrayList<String> getNaturalHairColor() {
-        ArrayList<String> naturalHairColor = new ArrayList<>();
-        ArrayList<String> possibleNaturalColor = new ArrayList<>();
-        possibleNaturalColor.add("Black");
-        possibleNaturalColor.add("Brown");
-        possibleNaturalColor.add("Blonde");
-        possibleNaturalColor.add("Ginger");
-        possibleNaturalColor.add("Auburn");
-        possibleNaturalColor.add("Albino");
-        possibleNaturalColor.add("Grey");
-        
-        for (String color : hairColor) {
-            if (possibleNaturalColor.contains(color)) {
-                naturalHairColor.add(color);
-                break;
-            }
-        }
-        return naturalHairColor;
-    }
+  public void setHairColor(ArrayList<String> hairColor) {
+    this.hairColor = hairColor;
+  }
 
-    public String getLifeStatus() {
-        if (alive == true) {
-            return "Alive";
-        } else {
-            return "Deceased";
-        }
-    }
+  public ArrayList<String> getNaturalHairColor() {
+    ArrayList<String> naturalHairColor = new ArrayList<>();
+    ArrayList<String> possibleNaturalColor = new ArrayList<>();
+    possibleNaturalColor.add("Black");
+    possibleNaturalColor.add("Brown");
+    possibleNaturalColor.add("Blonde");
+    possibleNaturalColor.add("Ginger");
+    possibleNaturalColor.add("Auburn");
+    possibleNaturalColor.add("Albino");
+    possibleNaturalColor.add("Grey");
 
-    public void setLifeStatus(boolean alive) {
-       this.alive = alive;
+    for (String color : hairColor) {
+      if (possibleNaturalColor.contains(color)) {
+        naturalHairColor.add(color);
+        break;
+      }
     }
+    return naturalHairColor;
+  }
 
-    public String getPersonInfo() {
-        return "Name: " + name +
-         "Gender Identity: " + sex +
-         "Race: " + race +
-         "Age: " + age +
-         "Height(In meters): " +  height +
-         "Hair Color: " + hairColor +
-         "Status: " + getLifeStatus();
+  public String getLifeStatus() {
+    if (alive == true) {
+      return "Alive";
+    } else {
+      return "Deceased";
     }
+  }
+
+  public void setLifeStatus(boolean alive) {
+    this.alive = alive;
+  }
+
+  public String getPersonInfo() {
+    return "Name: "
+        + name
+        + "| Gender Identity: "
+        + sex
+        + "| Race: "
+        + race
+        + "| Age: "
+        + age
+        + "| Height(In meters): "
+        + height
+        + "| Hair Color: "
+        + hairColor
+        + "| Status: "
+        + getLifeStatus();
+  }
 }
