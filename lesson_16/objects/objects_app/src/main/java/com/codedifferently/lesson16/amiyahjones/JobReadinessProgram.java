18424f51d1b350f4063f514317c526b3fc5c8f7a
AmiyahJo <ajones244@gmail.com>
2024-11-01 12:33:10 +0000

    feat: adds getMentor and getSocialSupport

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgram.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgram.java
@@ -1,24 +1,34 @@
 package com.codedifferently.lesson16.amiyahjones;
 
 import java.util.ArrayList;
+// import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class JobReadinessProgram {
     private final boolean isSuccessful; 
     ArrayList<String> students;
-    private final level readinessLevel;
+    private final Level readinessLevel;
     private final int lecturePerWeek;
+    private static final HashMap<Integer, String> teacherAssistants;
+    private String assignedTA;
+    private final String mentor;
+    private final String socialSupport;
 
-    enum level {
-        beginner , 
-        intermediate , 
-        advanced
+    enum Level {
+        Beginner , 
+        Intermediate , 
+        Advanced
     }
 
-    public JobReadinessProgram(level readinessLevel, boolean isSuccessful){
+    public JobReadinessProgram(Level readinessLevel, boolean isSuccessful, String assignedTA){
         students = new ArrayList<>();
         this.readinessLevel = readinessLevel;
         this.isSuccessful = isSuccessful;
         lecturePerWeek = 3;
+        this.assignedTA = null;
+        mentor = "Anthony";
+        socialSupport = "Estelle";
     }
     
     public void addStudent(String studentName){
@@ -32,7 +42,7 @@ public class JobReadinessProgram {
     }
     
     public String checkReadiness() {
-        return (readinessLevel == level.beginner) ? "Needs more training." : "Ready for job applications!";
+        return (readinessLevel == Level.Beginner) ? "Needs more training." : "Ready for job applications!";
     }
 
     public Boolean verifyIsSuccessful() {
@@ -48,4 +58,35 @@ public class JobReadinessProgram {
      System.out.println("Number of lectures students have: ");
      return lecturePerWeek;
    }
+
+    public String getMentor() {
+        return mentor;
+    }
+
+    public String getSocialSupport() {
+        return socialSupport;
+    }
+
+   public List<String> teacherAssistants() {
+    return teacherAssistants;
+   }
+    
+   public boolean assignTA(String taName) {
+        if (teacherAssistants.contains(taName)) {
+            this.assignedTA = taName;
+            return true; 
+        } else {
+            System.out.println("Not a TA. Please choose from: " + teacherAssistants);
+            return false; 
+        }
+    }
+
+    public void displayAssignedTA() {
+        if (assignedTA != null) {
+            System.out.println("Assigned Teacher Assistant: " + assignedTA);
+        } else {
+            System.out.println("No Teacher Assistant assigned.");
+        }
+    }
+
 }
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgramTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/amiyahjones/JobReadinessProgramTest.java
@@ -3,14 +3,14 @@ package com.codedifferently.lesson16.amiyahjones;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-import com.codedifferently.lesson16.amiyahjones.JobReadinessProgram.level;
+import com.codedifferently.lesson16.amiyahjones.JobReadinessProgram.Level;
 
 public class JobReadinessProgramTest {
 
     @Test
     void testAddStudent() {
         //Arrange
-        JobReadinessProgram program = new JobReadinessProgram(null);
+        JobReadinessProgram program = new JobReadinessProgram(null, false, null);
 
         //Act
         program.addStudent("John");
@@ -22,7 +22,7 @@ public class JobReadinessProgramTest {
     @Test
     void testGetStudentCount() {
         //Arrange
-        JobReadinessProgram program = new JobReadinessProgram(null, true);
+        JobReadinessProgram program = new JobReadinessProgram(null, true, null);
         program.addStudent("John");
 
         //Act
@@ -37,28 +37,28 @@ public class JobReadinessProgramTest {
 
     @Test 
     void testCheckReadinessBeginner() {
-        JobReadinessProgram student = new JobReadinessProgram(level.advanced, true);
+        JobReadinessProgram student = new JobReadinessProgram(Level.Advanced, true, null);
         String result = student.checkReadiness();
         assertEquals("Needs more training.", result);
     }
 
     @Test 
     void testCheckReadinessIntermediate() {
-        JobReadinessProgram student = new JobReadinessProgram(level.intermediate, true);
+        JobReadinessProgram student = new JobReadinessProgram(Level.Intermediate, true, null);
         String result = student.checkReadiness();
         assertEquals("Ready for job applications!", result);
     }
 
     @Test 
     void testCheckReadinessAdvanced() {
-        JobReadinessProgram student = new JobReadinessProgram(level.advanced, true);
+        JobReadinessProgram student = new JobReadinessProgram(Level.Advanced, true, null);
         String result = student.checkReadiness();
         assertEquals("Ready for job applications!", result);
     }
 
     @Test
-    void testVerifyIfSuccessful(){
-         JobReadinessProgram graduate = new JobReadinessProgram(null, false);
+    void testVerifyIfSuccessful() {
+         JobReadinessProgram graduate = new JobReadinessProgram(null, false, null);
          
          var graduation = graduate.verifyIsSuccessful();
          assertEquals(true, graduation);
@@ -66,9 +66,25 @@ public class JobReadinessProgramTest {
     }
 
     @Test
-    void testLecturePerWeek(){
-       JobReadinessProgram program = new JobReadinessProgram(level.advanced, true);
+    void testLecturePerWeek() {
+       JobReadinessProgram program = new JobReadinessProgram(Level.Advanced, true, null);
        var lecturesAttended = program.lecturePerWeek();
        assertEquals(3, lecturesAttended);
     }
+
+    @Test 
+    void testGetMentor() {
+        JobReadinessProgram program = new JobReadinessProgram(Level.Beginner, true, null);
+        var AssignedMentor = program.getMentor();
+        assertEquals("Anthony", AssignedMentor);
+    }
+
+     @Test 
+    void testGetSocialSupport() {
+        JobReadinessProgram program = new JobReadinessProgram(Level.Beginner, true, null);
+        var SocialSupport = program.getSocialSupport();
+        assertEquals("Estelle", SocialSupport);
+    }
+
+    
 }
