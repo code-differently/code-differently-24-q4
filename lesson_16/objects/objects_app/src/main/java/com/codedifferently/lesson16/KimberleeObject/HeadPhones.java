33c95fc35eb61230ce0f3344e1437b5364026d6f
haldanek <khaldane244@gmail.com>
2024-11-01 02:06:58 +0000

    feats: adds object default testing/pass

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/KimberleeObject/HeadPhones.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/KimberleeObject/HeadPhones.java
@@ -1,5 +1,31 @@
-package main.java.com.codedifferently.lesson16.KimberleeObject;
+package com.codedifferently.lesson16.KimberleeObject;
 
 public class HeadPhones {
-    
+  @SuppressWarnings("unused")
+  private int volume = 0;
+
+  private boolean isPoweredOn = false;
+  private String HeadPhoneColor = "BLACK";
+  private boolean isWireless = true;
+  private String brands = "Beats";
+
+  public int getVolume(int volume) {
+    return volume;
+  }
+
+  public String getHeadPhoneColor() {
+    return HeadPhoneColor;
+  }
+
+  public boolean isPoweredOn() {
+    return isPoweredOn;
+  }
+
+  public boolean isWireless() {
+    return isWireless;
+  }
+
+  public String getBrandsArray() {
+    return brands;
+  }
 }
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/KimberleeObjectTest/HeadPhonesTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/KimberleeObjectTest/HeadPhonesTest.java
@@ -1,5 +1,51 @@
-package test.java.com.codedifferently.lesson16.KimberleeObjectTest;
+package com.codedifferently.lesson16.KimberleeObjectTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import com.codedifferently.lesson16.KimberleeObject.HeadPhones;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class HeadPhonesTest {
-    
+
+  private HeadPhones headphones;
+
+  @BeforeEach
+  public void setUp() {
+    headphones = new HeadPhones();
+  }
+
+  public enum HeadPhoneColor {
+    RED,
+    BLUE,
+    ROSEGOLD,
+    PINK,
+    WHITE,
+    BLACK;
+  }
+
+  public void BrandsArray() {
+    String[] brands = new String[5];
+    brands[0] = "Beats";
+    brands[1] = "Sony";
+    brands[2] = "Bose";
+    brands[3] = "SkullCandy";
+    brands[4] = "Juicy";
+  }
+
+  @Test
+  public void testDefaultState() {
+    assertEquals(0, headphones.getVolume(0), "Volume should be 0 by default.");
+    assertEquals("BLACK", headphones.getHeadPhoneColor(), "Color should be black by default.");
+    assertFalse(headphones.isPoweredOn(), "HeadPhones should be off by default.");
+    assertTrue(headphones.isWireless(), "HeadPhones should be wireless by default.");
+    assertArrayEquals(new String[] {"Beats"}, new String[] {"Beats"});
+  }
+  // Arrange
+
+  // Act
+
+  // Assert
+
 }
