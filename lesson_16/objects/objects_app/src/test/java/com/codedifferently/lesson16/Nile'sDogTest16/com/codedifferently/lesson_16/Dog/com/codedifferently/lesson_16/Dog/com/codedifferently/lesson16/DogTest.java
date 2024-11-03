a4d4ee1e136401d94fcbd5a1cb20f4aae0fa398e
nilejack <njacksonnorthrup244@gmail.com>
2024-11-03 13:45:18 +0000

    Feat: Syntax/incorrect class declaration fix

--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/Nile'sDogTest16/com/codedifferently/lesson_16/Dog/com/codedifferently/lesson_16/Dog/com/codedifferently/lesson16/DogTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/Nile'sDogTest16/com/codedifferently/lesson_16/Dog/com/codedifferently/lesson_16/Dog/com/codedifferently/lesson16/DogTest.java
@@ -1,108 +1,36 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class DogTest {
 
-  // Test the Dog class
-  public class Dog {
-    // Custom exception
-    public static class DogAgeException extends Exception {
-      public DogAgeException(String message) {
-        super(message);
-      }
-    }
-
-    // Enum for dog colors
-    public enum Colors {
-      WHITE,
-      BROWN,
-      BLONDE
-    }
-
-    // Member variables
-    private int age;
-    private String breed;
-    private String gender;
-    private Colors color;
-    private boolean isFed;
-    private List<String> favoriteToys;
-
-    // Constructor
-    public Dog(int age, String breed, String gender, Colors color, boolean isFed)
-        throws DogAgeException {
-      if (age < 0) {
-        throw new DogAgeException("Age cannot be negative.");
-      }
-      this.age = age;
-      this.breed = breed;
-      this.gender = gender;
-      this.color = color;
-      this.isFed = isFed;
-      this.favoriteToys = new ArrayList<>();
-    }
-
-    // Getters
-    public int getAge() {
-      return age;
-    }
-
-    public String getBreed() {
-      return breed;
-    }
-
-    public String getGender() {
-      return gender;
-    }
-
-    public Colors getColor() {
-      return color;
-    }
-
-    public boolean isFed() {
-      return isFed;
-    }
-
-    public String getFavoriteToys() {
-      String favoriteToy = "Squeaky Fish";
-      return favoriteToy;
-    }
-
-    public String addFavoriteToy(String favoriteToy2) {
-      String favoriteToy = "Squeaky Fish";
-      return favoriteToy;
-    }
-  }
-
   @Test
-  public void testGetAge() throws DogTest.Dog.DogAgeException {
+  public void testGetAge() throws Dog.DogAgeException {
     Dog dog = new Dog(2, "Mutt", "male", Dog.Colors.BROWN, true);
     assertThat(dog.getAge()).isEqualTo(2);
   }
 
   @Test
-  public void testGetBreed() throws DogTest.Dog.DogAgeException {
+  public void testGetBreed() throws Dog.DogAgeException {
     Dog dog = new Dog(2, "Mutt", "male", Dog.Colors.BROWN, true);
     assertThat(dog.getBreed()).isEqualTo("Mutt");
   }
 
   @Test
-  public void testGetGender() throws DogTest.Dog.DogAgeException {
+  public void testGetGender() throws Dog.DogAgeException {
     Dog dog = new Dog(2, "Mutt", "male", Dog.Colors.BROWN, true);
     assertThat(dog.getGender()).isEqualTo("male");
   }
 
   @Test
-  void DogTesttestGetColor() throws DogTest.Dog.DogAgeException {
+  public void testGetColor() throws Dog.DogAgeException {
     Dog dog = new Dog(2, "Mutt", "male", Dog.Colors.BROWN, true);
     assertThat(dog.getColor()).isEqualTo(Dog.Colors.BROWN);
   }
 
   @Test
-  public void testIsFed() throws DogTest.Dog.DogAgeException {
+  public void testIsFed() throws Dog.DogAgeException {
     Dog dog = new Dog(2, "Mutt", "male", Dog.Colors.BROWN, true);
     assertThat(dog.isFed()).isTrue();
   }
