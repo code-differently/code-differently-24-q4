1e7b59e9ab1cb402b25128b64ba0ebe5703a3cae
AmiyahJo <ajones244@gmail.com>
2024-11-03 23:45:06 +0000

    feat: adds business checking account chore: savingsAccount whitespace

---
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
@@ -0,0 +1,17 @@
+package com.codedifferently.lesson17.bank;
+
+public class BusinessCheckingAccount extends CheckingAccount{
+    private final boolean isBusinessOwner;
+
+    public BusinessCheckingAccount(String accountNumber, Set<Customer> owners, boolean isBusinessOwner) {
+        super(accountNumber, owners, 0.0);
+        this.isBusinessOwner = isBusinessOwner;
+    }
+
+    public void writeCheck(double amount) {
+        if(!isBusinessOwner) {
+            throw new IllegalArgumentException("Must be a business owner to write a check.");
+        }
+        super.writeCheck(amount);
+    }
+}
--- lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/SavingsAccount.java
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/SavingsAccount.java
@@ -3,7 +3,7 @@ package com.codedifferently.lesson17.bank;
 import java.util.Set;
 
 public class SavingsAccount extends CheckingAccount {
-    public SavingsAccount(String accountNumber, Set <Customer> owners, double initialBalance) {
+    public SavingsAccount(String accountNumber, Set<Customer> owners, double initialBalance) {
         super(accountNumber, owners, initialBalance);
     }
 
