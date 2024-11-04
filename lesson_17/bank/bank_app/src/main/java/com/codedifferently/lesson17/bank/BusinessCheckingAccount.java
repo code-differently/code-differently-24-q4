576826b57dca009157186ac9959666a5f3a992dd
AmiyahJo <ajones244@gmail.com>
2024-11-04 15:52:52 +0000

    feat: adds getBusinessName getter
    within the BusinessCheckingAccount.java

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/BusinessCheckingAccount.java
@@ -22,4 +22,8 @@ public class BusinessCheckingAccount extends CheckingAccount{
         }
         this.businessName = businessName;
     }   
+
+    public String getBusinessName() {
+        return businessName;
+    }
 }
