1ab73e9a8c9d10a135c7a32f979e1c6cb9a99edc
AmiyahJo <ajones244@gmail.com>
2024-11-04 00:29:07 +0000

    feat: adds exception import to savings account

--- lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/SavingsAccount.java
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/SavingsAccount.java
@@ -2,6 +2,8 @@ package com.codedifferently.lesson17.bank;
 
 import java.util.Set;
 
+import com.codedifferently.lesson17.bank.exceptions.CheckNotAllowedException;
+
 public class SavingsAccount extends CheckingAccount {
     public SavingsAccount(String accountNumber, Set<Customer> owners, double initialBalance) {
         super(accountNumber, owners, initialBalance);
