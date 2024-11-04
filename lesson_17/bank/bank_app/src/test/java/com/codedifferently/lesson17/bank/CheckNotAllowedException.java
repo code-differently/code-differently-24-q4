48839ea2c63489ea370b4acf8e63b97deca9ba1b
AmiyahJo <ajones244@gmail.com>
2024-11-04 00:26:25 +0000

    rm: remove checkNotAllowedException from the test/java/bank pathway

--- lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/CheckNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.codedifferently.lesson17.bank;
-
-public class CheckNotAllowedException extends RuntimeException {
-    public CheckNotAllowedException(String message) {
-        super(message);
-    }
-}
