f3df9ad0e585f15c4ec42382f0133911b7124418
AmiyahJo <ajones244@gmail.com>
2024-11-05 15:22:10 +0000

    feat: adds details to moneyOrder
    includes auditLog , and methods of getting the total amount of the money order

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/MoneyOrder.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/MoneyOrder.java
@@ -1,7 +1,25 @@
 package com.codedifferently.lesson17.bank;
 
 public class MoneyOrder {
-    public MoneyOrder(Account sourceAccount, double amount) {
+    private final CheckingAccount sourceAccount;
+    private final double amount;
+    private AuditLog auditLog;
+    
+    public MoneyOrder(CheckingAccount sourceAccount, double amount, AuditLog auditLog) {
+        this.sourceAccount = sourceAccount;
+        this.amount = amount;
+        this.auditLog = auditLog;
+
         sourceAccount.withdraw(amount);
+        
+        auditLog.logTransaction(sourceAccount.getAccountNumber(), -amount, "MoneyOrder");
+    }
+
+    public CheckingAccount getSourceAccount() {
+        return sourceAccount;
+    }
+
+    public double getAmount() {
+        return amount;
     }
 }
