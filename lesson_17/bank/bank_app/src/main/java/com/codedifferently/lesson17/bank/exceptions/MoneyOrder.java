b45c1a599e890a475e8b6e65607b571ab267c10c
nilejack <njacksonnorthrup244@gmail.com>
2024-11-06 02:16:14 +0000

    Feat: Fix/Jacoco fix

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/exceptions/MoneyOrder.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/exceptions/MoneyOrder.java
@@ -1,3 +1,55 @@
 package com.codedifferently.lesson17.bank.exceptions;
 
-public class MoneyOrder {}
+import com.codedifferently.lesson17.bank.Check;
+import com.codedifferently.lesson17.bank.CheckingAccount;
+
+public class MoneyOrder extends Check {
+
+  public MoneyOrder(String checkNumber, double amount, CheckingAccount account) {
+    super(checkNumber, amount, account);
+    if (amount < 0) {
+      throw new IllegalArgumentException("MoneyOrder amount must be positive");
+    }
+    account.withdraw(amount);
+  }
+
+  @Override
+  /**
+   * Deposits the check into an account.
+   *
+   * @param toAccount The account to deposit the check into.
+   */
+  public void depositFunds(CheckingAccount toAccount) {
+    if (getIsVoided()) {
+      throw new CheckVoidedException("MoneyOrder is voided");
+    }
+    // getAccount().withdraw(getAmount());
+    toAccount.deposit(getAmount());
+    voidCheck();
+  }
+
+  @Override
+  public String toString() {
+    return "MoneyOrder{"
+        + "checkNumber='"
+        + getCheckNumber()
+        + '\''
+        + ", amount="
+        + getAmount()
+        + ", account="
+        + ((CheckingAccount) getAccount()).getAccountNumber()
+        + '}';
+  }
+
+  private double getAmount() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  private Object getAccount() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  private String getCheckNumber() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
---
+++ lesson_17/bank/bank_app/src/test/java/com/codedifferently/lesson17/bank/exceptions/MoneyOrderTest.java
@@ -0,0 +1,71 @@
+package com.codedifferently.lesson17.bank.exceptions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import com.codedifferently.lesson17.bank.Check;
+import com.codedifferently.lesson17.bank.CheckingAccount;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MoneyOrderTest<account2> {
+
+  private CheckingAccount account1;
+  private CheckingAccount account2;
+  private MoneyOrder classUnderTest;
+
+  @BeforeEach
+  void setUp() {
+    account1 = new CheckingAccount("123456789", null, 100.0);
+    account2 = new CheckingAccount("987654321", null, 200.0);
+    classUnderTest = new MoneyOrder("123456789", 50.0, account1);
+  }
+
+  @Test
+  void testDepositFunds() {
+    // Act
+    classUnderTest.depositFunds(account2);
+
+    // Assert
+    assertThat(account1.getBalance()).isEqualTo(50.0);
+    assertThat(account2.getBalance()).isEqualTo(250.0);
+  }
+
+  @Test
+  void testDepositFunds_CheckVoided() {
+    // Arrange
+    classUnderTest.voidCheck();
+
+    // Act & Assert
+    assertThatExceptionOfType(CheckVoidedException.class)
+        .isThrownBy(() -> classUnderTest.depositFunds(account2))
+        .withMessage("MoneyOrder is voided");
+  }
+
+  @Test
+  void testHashCode() {
+    // Arrange
+    MoneyOrder otherCheck = new MoneyOrder("123456789", 50.0, account1);
+
+    // Assert
+    assertThat(classUnderTest.hashCode()).isEqualTo(otherCheck.hashCode());
+  }
+
+  @Test
+  void testEquals() {
+    // Arrange
+    Check otherCheck = new MoneyOrder("123456789", 25.0, account1);
+    Check differentCheck = new MoneyOrder("987654321", 25.0, account1);
+
+    // Assert
+    assertThat(classUnderTest.equals(otherCheck)).isTrue();
+    assertThat(classUnderTest.equals(differentCheck)).isFalse();
+  }
+
+  @Test
+  void testToString() {
+    // Assert
+    assertThat(classUnderTest.toString())
+        .isEqualTo("MoneyOrder{checkNumber='123456789', amount=50.0, account=123456789}");
+  }
+}
