050f47b6e4aa705e1a7dcdbaee7ae449d2c31918
nilejack <njacksonnorthrup244@gmail.com>
2024-11-06 02:09:02 +0000

    Feat Adds IlleagalArgument.java

---
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/exceptions/IllegalArgumentException.java
@@ -0,0 +1,9 @@
+package com.codedifferently.lesson17.bank.exceptions;
+
+public class IllegalArgumentException extends RuntimeException {
+  public IllegalArgumentException() {}
+
+  public IllegalArgumentException(String message) {
+    super(message);
+  }
+}
