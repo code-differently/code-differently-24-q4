436623ef14619b00cbaeb176d976deb728e5be16
James Capparell <jjcapparell244@gmail.com>
2024-11-06 10:29:11 -0500

    Delete lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Lunch/InvalidCalorieException.java
    deleting unwanted changes

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/Lunch/InvalidCalorieException.java
+++
@@ -1,7 +0,0 @@
-package com.codedifferently.lesson16.lunch;
-
-public class InvalidCalorieException extends Exception {
-  public InvalidCalorieException(String message) {
-    super(message);
-  }
-}
