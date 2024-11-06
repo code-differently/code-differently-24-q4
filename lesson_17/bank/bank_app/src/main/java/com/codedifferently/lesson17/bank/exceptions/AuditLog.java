1a8de4f711fcef084063ca8e9927f8039c51e7ca
nilejack <njacksonnorthrup244@gmail.com>
2024-11-06 01:44:00 +0000

    Feat: Adds AuditLog

--- lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/exceptions/AuditLog.java
+++ lesson_17/bank/bank_app/src/main/java/com/codedifferently/lesson17/bank/exceptions/AuditLog.java
@@ -1,1 +1,8 @@
+package com.codedifferently.lesson17.bank.exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuditLog {
+  private final List<String> logEntries = new ArrayList<>();
+}
