6722fecb3db7865c4313736db3359b88d24d600c
txtran224 <txtran224@gmail.com>
2024-11-06 22:01:03 -0500

    Feat: Lesson_16 Coin Object and Unit Tests for Tommy Tran (#526)
    * feat:lesson_16 Coins and CoinTest files added
    
    * fix: more bugs
    
    * fix: Coins file added
    
    * feat:lesson_16 Coin object added for Tommy Tran
    
    * fix:lesson16 java test file
    
    * lesson_16
    
    * Delete lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/CoinsTest.Java
    
    * fix:lesson_16 bugs

---
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/coins/Coins.java
@@ -0,0 +1,86 @@
+package com.codedifferently.lesson16.coins;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coins {
+
+  public enum CoinType {
+    PENNY,
+    NICKEL,
+    DIME,
+    QUARTER
+  }
+
+  private final CoinType coinType;
+  private final int coinValue;
+  private final boolean coinIsRare;
+  private final double coinGramWeight;
+  private final int coinAge;
+  private final List<String> material;
+  private static final List<Coins> coinCollection = new ArrayList<>();
+
+  // constructor
+  public Coins(
+      CoinType coinType,
+      int coinValue,
+      Boolean coinIsRare,
+      Double coinGramWeight,
+      int coinAge,
+      List<String> material) {
+    this.coinType = coinType;
+    this.coinValue = coinValue;
+    this.coinIsRare = coinIsRare;
+    this.coinGramWeight = coinGramWeight;
+    this.coinAge = coinAge;
+    this.material = material;
+  }
+
+  // Getters and Setters
+  public CoinType getCoinType() {
+    return coinType;
+  }
+
+  public int getCoinValue() {
+    return coinValue;
+  }
+
+  public boolean isCoinRare() {
+    return coinIsRare;
+  }
+
+  public double getCoinGramWeight() {
+    return coinGramWeight;
+  }
+
+  public int getCoinAge() {
+    return coinAge;
+  }
+
+  public List<String> getMaterial() {
+    List<String> materialsList = new ArrayList<>();
+    for (String mat : material) {
+      materialsList.add(mat);
+    }
+    return materialsList;
+  }
+
+  public static int convertNickelsToDimes(int totalNickels) {
+    if (totalNickels < 1) {
+      throw new IllegalArgumentException("you do not have enough nickles");
+    }
+    return totalNickels / 2;
+  }
+
+  public static List<Coins> setCoinCollection() {
+    Coins penny = new Coins(Coins.CoinType.PENNY, 1, true, 2.5, 1977, List.of("zinc", "copper"));
+    Coins nickel =
+        new Coins(Coins.CoinType.NICKEL, 5, true, 5.0, 1953, List.of("nickel", "copper"));
+    Coins dime = new Coins(CoinType.DIME, 10, true, 10.0, 1957, List.of("nickel", "copper"));
+
+    coinCollection.add(penny);
+    coinCollection.add(nickel);
+    coinCollection.add(dime);
+    return coinCollection;
+  }
+}
