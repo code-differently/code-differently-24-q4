ccded0cc2965712f603388531665b07ba612f2db
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-11-13 21:07:15 +0000

    Adds fixed changes

--- lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/shawndunsmore/InvalidPayAmountException.java
+++ lesson_16/objects/objects_app/src/main/java/com/codedifferently/lesson16/shawndunsmore/InvalidPayAmountException.java
@@ -6,4 +6,3 @@ public class InvalidPayAmountException extends Exception {
     super(message);
   }
 }
-
--- lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/ShawnDunsmore/SlotMachineTest.java
+++ lesson_16/objects/objects_app/src/test/java/com/codedifferently/lesson16/ShawnDunsmore/SlotMachineTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.Collections;
-
 import org.junit.jupiter.api.Test;
 
 public class SlotMachineTest {
