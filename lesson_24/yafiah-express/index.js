7925b81df5efaa0739d6ed835afbf931014ef7ab
yafiaha <callheryaf@gmail.com>
2024-11-20 13:37:33 -0500

    Feat : yafiah Lesson/24 Sign-Up page

---
+++ lesson_24/yafiah-express/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from the "yafiahabdullah" folder
+app.use(express.static(path.join(__dirname, "yafiahabdullah")));
+
+// Serve the index.html on the root route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "yafiahabdullah", "index.html"));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+
