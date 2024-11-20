dbfea7351a0cf6dc9ec3488971d1258daf3001f8
marleomac3 <mlmcwilliams244@gmail.com>
2024-11-20 15:13:35 +0000

    feat: created web server and contact form w/ data submission for copycat site

---
+++ lesson_24/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+app.post("/submit", cors(), async (req, res) => {
+    const {name, email, message} = req.body;
+    res.send(`Contact information received: Name - ${name},   Email - ${email},   Message - ${message}`);
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+});
