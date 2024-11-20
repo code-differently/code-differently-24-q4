9dd8af6a7b75d31adb3e426a0b68f5865c3ba28e
zionbuch <zabuchanan244@gmail.com>
2024-11-20 15:23:53 +0000

    added index.js file

---
+++ lesson_24/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const port = 4000 
+const cors = require("cors")
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+
+app.get("/", cors(), async (req, res) => { 
+    res.send("This is working")
+})
+
+app.listen(port, () => { 
+    console.log(`Listening at http://localhost:${port}`)
+
+})
