54382bb93132bd1902ff7e19dd0d69225acadc6c
AmiyahJo <ajones244@gmail.com>
2024-11-20 21:41:52 +0000

    feat: adds scss files
    refresh in .tsx files to start off

--- lesson_25/amiyahjones/src/components/programs-section/Program.tsx
+++ lesson_25/amiyahjones/src/components/programs-section/Program.tsx
@@ -1,17 +1,1 @@
 import React from 'react';
-
-interface ProgramProps {
-  title: string;
-  description: string;
-}
-
-const Program: React.FC<ProgramProps> = ({ title, description }) => {
-  return (
-    <li className="program">
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-};
-
-export default Program;
