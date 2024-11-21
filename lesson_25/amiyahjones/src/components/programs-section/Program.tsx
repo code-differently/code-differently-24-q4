713b2b3fa015370c43e36023f5425df06b76210a
AmiyahJo <ajones244@gmail.com>
2024-11-21 14:58:21 +0000

    feat: properly adds react content to program and programList items

--- lesson_25/amiyahjones/src/components/programs-section/Program.tsx
+++ lesson_25/amiyahjones/src/components/programs-section/Program.tsx
@@ -1,1 +1,10 @@
 import React from 'react';
+
+export default function Program({title, description}: {title: string; description: string}) {
+  return (
+    <li className="program">
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+};
