99d108a9f90706a47d97963479f1308599dedc07
XavierCruz5106 <superguyman121@gmail.com>
2024-11-21 00:27:04 +0000

    feat: made program component

--- lesson_25/xaviercruz/src/components/program/Program.tsx
+++ lesson_25/xaviercruz/src/components/program/Program.tsx
@@ -1,3 +1,13 @@
-export const Program: React.FC = () => {
-  return <></>;
+interface ProgramProps {
+  name: string;
+  description: string;
+}
+
+export const Program: React.FC<ProgramProps> = props => {
+  return (
+    <li className="program">
+      <h3>${props.name}</h3>
+      <p>${props.description}</p>
+    </li>
+  );
 };
