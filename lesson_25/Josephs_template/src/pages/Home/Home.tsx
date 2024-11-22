f6d854b7af7b6aca328c92e20a8d20464a0d73f0
Joseph <jxcaballero244@gmail.com>
2024-11-22 18:41:31 +0000

    feat: modified home.tsx to clear input when clicking enter. lesson_25 -JosephCaballero

--- lesson_25/Josephs_template/src/pages/Home/Home.tsx
+++ lesson_25/Josephs_template/src/pages/Home/Home.tsx
@@ -1,7 +1,6 @@
 import './Home.scss';
-import React, { createContext, useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-
+import React, {createContext, useContext, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const ProgramContext = createContext<
   | {
@@ -15,56 +14,56 @@ interface Program {
   description: string;
 }
 
-
-
-export const ProgramProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const ProgramProvider: React.FC<{children: React.ReactNode}> = ({
+  children,
+}) => {
   const [programs, setPrograms] = useState<Program[]>([
-  {
-    title: 'Swine Short Loin',
-    description: `Swine short loin burgdoggen ball tip, shank ham hock bacon. Picanha
+    {
+      title: 'Swine Short Loin',
+      description: `Swine short loin burgdoggen ball tip, shank ham hock bacon. Picanha
           strip steak pork, swine boudin ham meatball meatloaf leberkas sausage.
           Turkey beef andouille kielbasa rump pastrami biltong chislic alcatra
           buffalo prosciutto jowl. Pastrami chicken sirloin swine capicola
           landjaeger jowl boudin pork chop shankle bresaola turducken leberkas
           ham.`,
-  },
-  {
-    title: 'Bacon Ipsum',
-    description: `Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin
+    },
+    {
+      title: 'Bacon Ipsum',
+      description: `Bacon ipsum dolor amet leberkas chuck biltong pork loin sirloin
           bresaola rump frankfurter. Shoulder doner strip steak chuck. Short
           ribs venison salami chuck sirloin jowl chislic cupim swine cow. Corned
           beef chuck frankfurter tenderloin venison biltong andouille leberkas
           kielbasa sausage t-bone turducken fatback. Pork picanha buffalo bacon
           tail salami meatball, jowl chislic.`,
-  },
-  {
-    title: 'Picanha Swine Jowl',
-    description: `Picanha swine jowl meatball boudin pastrami bresaola fatback shankle
+    },
+    {
+      title: 'Picanha Swine Jowl',
+      description: `Picanha swine jowl meatball boudin pastrami bresaola fatback shankle
           pork belly cow. Frankfurter ground round shank corned beef chuck.
           Jerky frankfurter fatback capicola hamburger, pork prosciutto bresaola
           ham porchetta rump t-bone pancetta tenderloin. Fatback ham hock
           prosciutto, tenderloin shoulder salami tri-tip leberkas bacon
           turducken chislic venison sausage frankfurter.`,
-  },
-  {
-    title: 'Kevin Chicken T-Bone',
-    description: `Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa cow jowl
+    },
+    {
+      title: 'Kevin Chicken T-Bone',
+      description: `Kevin chicken t-bone spare ribs shankle bacon drumstick kielbasa cow jowl
       doner salami chuck andouille. Rump spare ribs bresaola frankfurter shankle.
       Bresaola ribeye turducken, cow leberkas boudin biltong sirloin filet mignon
       ham pork belly shank. Tenderloin sirloin pancetta pork loin shankle venison 
       turducken boudin. Brisket tenderloin salami ham bresaola burgdoggen.`,
-  },
-]);
-const addProgram = (newProgram: Program) => {
-    setPrograms((prevPrograms) => [...prevPrograms, newProgram]);
+    },
+  ]);
+  const addProgram = (newProgram: Program) => {
+    setPrograms(prevPrograms => [...prevPrograms, newProgram]);
   };
 
   return (
-    <ProgramContext.Provider value={{ programs, addProgram }}>
+    <ProgramContext.Provider value={{programs, addProgram}}>
       {children}
     </ProgramContext.Provider>
   );
-}
+};
 
 export const useProgramContext = () => {
   const context = useContext(ProgramContext);
@@ -74,15 +73,13 @@ export const useProgramContext = () => {
   return context;
 };
 
-
-
-const Programs: React.FC<Program>= ({title, description}) => {
+const Programs: React.FC<Program> = ({title, description}) => {
   return (
     <>
-    <li className='program'>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
+      <li className="program">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </li>
     </>
   );
 };
@@ -96,7 +93,11 @@ const ProgramsList: React.FC = () => {
       </h2>
       <ul className="programs">
         {programs.map((each, index) => (
-          <Programs key={index} title={each.title} description={each.description}/>
+          <Programs
+            key={index}
+            title={each.title}
+            description={each.description}
+          />
         ))}
       </ul>
     </>
@@ -123,42 +124,53 @@ export const Home: React.FC = () => {
           </div>
         </section>
         <section className="programs-section">
-           <h2>
-          Our <em className="highlight">Programs</em>
-        </h2>
-          <ProgramsList/>
-       <Link style={{alignContent: 'center', alignSelf: 'center'}}to= '/OtherPage'> <button>Add More</button></Link>
-          
+          <h2>
+            Our <em className="highlight">Programs</em>
+          </h2>
+          <ProgramsList />
+          <Link
+            style={{alignContent: 'center', alignSelf: 'center'}}
+            to="/OtherPage"
+          >
+            {' '}
+            <button>Add More</button>
+          </Link>
         </section>
       </article>
     </>
   );
- 
 };
 
-export const OtherPage = () =>{
-  
+export const OtherPage = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const { addProgram } = useProgramContext();
+  const {addProgram} = useProgramContext();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (title && description) {
-      addProgram({ title, description });
+      addProgram({title, description});
       setTitle('');
       setDescription('');
     }
   };
 
-  return(
+  return (
     <>
-        <h1 style={{textAlign: 'center'}}>Program Creation Page</h1>
-          <form onSubmit={handleSubmit}>
-            <input onChange={e=> setTitle(e.target.value)}/>
-            <input onChange={e=> setDescription(e.target.value)}/>
-            <button>Submit Program</button>
-          </form>
+      <h1 style={{textAlign: 'center'}}>Program Creation Page</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={title}
+          className="inputField"
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          value={description}
+          className="inputField"
+          onChange={e => setDescription(e.target.value)}
+        />
+        <button>Submit Program</button>
+      </form>
     </>
-  )
-}
+  );
+};
