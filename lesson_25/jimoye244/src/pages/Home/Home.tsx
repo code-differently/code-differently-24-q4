68252163f91534a302f0aa6dd155679bb1210842
Oyeyemi Jimoh <oyeyemijimoh244@gmail.com>
2024-11-22 10:34:52 -0500

    feat: adds hw25 - breaking home page into components by Yemi

---
+++ lesson_25/jimoye244/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.scss';
+import ProgramList from './ProgramList';
+
+export const Home: React.FC = () => {
+  return (
+    <article>
+      <ProgramList />
+    </article>
+  );
+};
