d25818b7dca54389730d38ece5dc8b9504a1ef62
marleomac3 <mlmcwilliams244@gmail.com>
2024-11-22 17:58:55 +0000

    feat: Made Program and ProgramList web components. WIP

---
+++ lesson_25/lj-template/src/pages/Home/ProgramSubmission.tsx
@@ -0,0 +1,11 @@
+import './Home.scss';
+import React, { useState } from 'react';
+
+export const ProgramSubmission: React.FC = () => {
+    const [programName, setProgramName] = useState('');
+    const [programDescription, setProgramDescription] = useState('');
+    return (
+
+    );
+}
+
