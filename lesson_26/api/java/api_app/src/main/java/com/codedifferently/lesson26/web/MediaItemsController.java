c633f313b4860e58166e0a8b211ca672413c3774
Oyeyemi Jimoh <oyeyemijimoh244@gmail.com>
2024-11-24 17:57:15 -0500

    feat: added implementation for addItem, deleteItem and postItem endpoints to Web Controller for lesson26 by Yemi

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -7,8 +7,15 @@ import com.codedifferently.lesson26.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,5 +35,47 @@ public class MediaItemsController {
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
+  }
+
+  @GetMapping("/items/{id}")
+  public GetMediaItemsResponse getItem(@PathVariable UUID id) {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    List<MediaItemResponse> responseItem =
+        items.stream()
+            .map(MediaItemResponse::from) // Transform the original objects
+            .filter(item -> item.getId().equals(id))
+            .toList();
+    var response = GetMediaItemsResponse.builder().items(responseItem).build();
+
+    return response;
+  }
+
+  @DeleteMapping("/items/{id}")
+  public ResponseEntity<Object> deleteItem(@PathVariable UUID id) {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    List<MediaItemResponse> itemToDelete =
+        items.stream()
+            .map(MediaItemResponse::from) // Transform the original objects
+            .filter(item -> item.getId().equals(id))
+            .toList();
+
+    if (!itemToDelete.isEmpty()) {
+      library.removeMediaItem(id, librarian);
+      return ResponseEntity.status(HttpStatus.OK).body("media item deleted successfully");
+    }
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Media deletion not successful");
+  }
+
+  @PostMapping("/items")
+  public ResponseEntity<Object> addItem(@RequestBody MediaItemRequest mediaItemRequest) {
+
+    MediaItem mediaItem = MediaItemRequest.asMediaItem(mediaItemRequest);
+    library.addMediaItem(mediaItem, librarian);
+
+    if (null == mediaItem) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+          .body("media item creation not successful");
+    }
+    return ResponseEntity.status(HttpStatus.CREATED).body("new media item created successfully");
   }
 }
