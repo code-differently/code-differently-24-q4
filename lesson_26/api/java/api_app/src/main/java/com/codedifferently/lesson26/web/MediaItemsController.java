c072e07ff47c4fb38bf50336f9a8e26c3fb4783f
Joseph <jxcaballero244@gmail.com>
2024-11-24 05:25:34 +0000

    fix: working through making the tests all pass. lesson_26 -JosephCaballero

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -1,11 +1,15 @@
 package com.codedifferently.lesson26.web;
 
+import com.codedifferently.lesson26.library.Librarian;
+import com.codedifferently.lesson26.library.Library;
+import com.codedifferently.lesson26.library.MediaItem;
+import com.codedifferently.lesson26.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,11 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.codedifferently.lesson26.library.Librarian;
-import com.codedifferently.lesson26.library.Library;
-import com.codedifferently.lesson26.library.MediaItem;
-import com.codedifferently.lesson26.library.search.SearchCriteria;
 
 @RestController
 @RequestMapping("/items")
@@ -33,51 +32,66 @@ public class MediaItemsController {
     this.librarian = library.getLibrarians().stream().findFirst().orElseThrow();
   }
 
-  
-  
   @GetMapping()
-public ResponseEntity<GetMediaItemsResponse> getItemById() {
-  Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+  public ResponseEntity<GetMediaItemsResponse> getItemById() {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    if (items.isEmpty()) {
+      ResponseEntity.noContent();
+    }
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return ResponseEntity.ok(response);
-}
+  }
 
-@GetMapping(value = "/{id}")
-public ResponseEntity<MediaItemResponse> getItemById(@PathVariable("id") UUID id) {
+  @GetMapping(value = "/{id}")
+  public ResponseEntity<MediaItemResponse> getItemById(@PathVariable("id") UUID id) {
 
-        System.out.println(id.toString());
+    System.out.println(id.toString());
 
-
-        Set<MediaItem> items = library.search(SearchCriteria.builder().id(id.toString()).build());
-
-        Optional<MediaItem> matchedItem = items.stream()
-            .filter(item -> item.getId().equals(id))
-            .findFirst();
-            System.out.println("items");
-            System.out.println(items);
-            System.out.println(matchedItem);
-            System.out.println("mathcedItems");
-        return matchedItem.map(item -> {
-            MediaItemResponse responseItem = MediaItemResponse.from(item);
-            return ResponseEntity.ok(responseItem);
-        }).orElse(ResponseEntity.notFound().build());
-}
+    Set<MediaItem> items = library.search(SearchCriteria.builder().id(id.toString()).build());
+    Optional<MediaItem> matchedItem =
+        items.stream().filter(item -> item.getId().equals(id)).findFirst();
+    System.out.println("items");
+    System.out.println(items);
+    System.out.println(matchedItem);
+    System.out.println("mathcedItems");
+    return matchedItem
+        .map(
+            item -> {
+              MediaItemResponse responseItem = MediaItemResponse.from(item);
+              return ResponseEntity.ok(responseItem);
+            })
+        .orElse(ResponseEntity.notFound().build());
+  }
 
   @PostMapping()
-  public CreateMediaItemResponse postItems(@RequestBody MediaItemRequest request) {
+  public ResponseEntity<?> postItems(@RequestBody(required = true) MediaItemRequest request) {
     MediaItem newMediaItem = MediaItemRequest.asMediaItem(request);
+
+    if (newMediaItem.getTitle() == null
+        || newMediaItem.getType() == null
+        || request.getType() == null) {
+
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(request);
+    }
 
     library.addMediaItem(newMediaItem, librarian);
 
-    MediaItemResponse responseItem = MediaItemResponse.from(newMediaItem);
-    return CreateMediaItemResponse.builder().item(responseItem).build();
+    return ResponseEntity.status(HttpStatus.CREATED).body(request);
   }
 
   @DeleteMapping(value = "/{id}")
-  public Set<MediaItem> deleteItem(@PathVariable("id") UUID id){
+  public ResponseEntity<Void> deleteItem(@PathVariable("id") UUID id) {
+    Set<MediaItem> test1 = library.search(SearchCriteria.builder().id(id.toString()).build());
+
+    Set<MediaItem> item = library.search(SearchCriteria.builder().id(id.toString()).build());
+    if (item.isEmpty()) {
+      ResponseEntity.notFound().build();
+    } else if (!test1.contains(item.iterator().next())) {
+      ResponseEntity.notFound().build();
+    }
     library.removeMediaItem(id, librarian);
-    Set<MediaItem> libraryAltercation = library.search(SearchCriteria.builder().build());
-    return libraryAltercation;
+    // Set<MediaItem> libraryAltercation = library.search(SearchCriteria.builder().build());
+    return ResponseEntity.noContent().build();
   }
 }
