753a15cc797b02c8d4f21c8baa49252ef234fc62
AmiyahJo <ajones244@gmail.com>
2024-11-26 15:33:03 +0000

    fix: server details to load the webpage correctly

--- lesson_27/amiyahjones/vite.config.ts
+++ lesson_27/amiyahjones/vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
-import {URL, fileURLToPath} from 'url';
-import {defineConfig} from 'vite';
+import { URL, fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,8 @@ export default defineConfig({
         replacement: fileURLToPath(new URL('./src', import.meta.url)),
       },
     ],
+  },
+  server: {
+    host: '0.0.0.0',
   },
 });
