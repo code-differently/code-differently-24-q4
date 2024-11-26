b21d9796a74444ab340d4aec2421f0d01350a55c
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-11-26 12:58:50 +0000

    feat: added url parser and json parser along; added data display for contact form;

--- lesson_24/chigazograham/index.js
+++ lesson_24/chigazograham/index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.post('/contact', async (req, res) => {
+    const { name, email, feedback } = req.body;
+    res.send(`
+        <h1>Form Submitted</h1>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Feedback:</strong> ${feedback}</p>
+        <a href="/contact">Go back to the form</a>
+    `);
 });
 
-
-app.post('/results.html', (req, res) => {
-    const currentTime = new Date().toISOString();
-    res.status(200).send(`<div> Not Ready Yet!!! ${currentTime}</div>`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
