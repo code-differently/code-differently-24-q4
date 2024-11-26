8d9ad987d98e9b76778f89dca015fcf9d7bb2ea2
XavierCruz5106 <superguyman121@gmail.com>
2024-11-26 11:25:56 -0500

    fix: fixes server not responding on post.

--- lesson_27/api/src/server.ts
+++ lesson_27/api/src/server.ts
@@ -1,17 +1,19 @@
-import cors from 'cors';
-import fs from 'fs';
-import express, {Express, Request, Response} from 'express';
 import programs from './data/programs.json';
-import { randomUUID, UUID } from 'crypto';
+import cors from 'cors';
+import {randomUUID} from 'crypto';
+import express, {Express, Request, Response} from 'express';
+import fs from 'fs';
+
 import {Program} from '../../types';
 
-const PROGRAMS_FILE = './data/programs.json';
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+const PROGRAMS_FILE = 'src/data/programs.json';
+const UUID_PATTERN =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 const app: Express = express();
 
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
@@ -42,9 +44,11 @@ app.get('/programs', async (req: Request, res: Response<Program[]>) => {
 app.post('/programs', async (req: Request<Partial<Program>>, res: Response) => {
   const newProgram = req.body;
   programs.push({id: randomUUID(), ...newProgram});
-  fs.writeFile(PROGRAMS_FILE, JSON.stringify(programs, null, 2), (err) => {
+  console.log(programs);
+  fs.writeFile(PROGRAMS_FILE, JSON.stringify(programs, null, 2), err => {
     if (err) return console.log(err);
     console.log(`Updated ${PROGRAMS_FILE}`);
+    res.status(200).send('Added Program');
   });
 });
 
