5170ab31c7c5b8ca6c1c6dd36464b1f74c742630
Anthony D. Mays <anthony@morganlatimer.com>
2024-11-26 16:59:32 +0000

    fix: corrects file path to json file.

--- lesson_27/api/src/server.ts
+++ lesson_27/api/src/server.ts
@@ -1,17 +1,20 @@
-import cors from 'cors';
-import { randomUUID } from 'crypto';
-import express, { Express, Request, Response } from 'express';
-import fs from 'fs';
-import { Program } from '../../types';
 import programs from './data/programs.json';
+import cors from 'cors';
+import {randomUUID} from 'crypto';
+import express, {Express, Request, Response} from 'express';
+import fs from 'fs';
+import path from 'path';
 
-const PROGRAMS_FILE = './data/programs.json';
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+import {Program} from '../../types';
+
+const PROGRAMS_FILE = path.resolve(__dirname, './data/programs.json');
+const UUID_PATTERN =
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 const app: Express = express();
 
 app.use(express.static('public'));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
@@ -42,14 +45,18 @@ app.get('/programs', async (req: Request, res: Response<Program[]>) => {
 app.post('/programs', async (req: Request<Partial<Program>>, res: Response) => {
   const newProgram = req.body;
   programs.push({id: randomUUID(), ...newProgram});
-  fs.writeFile(PROGRAMS_FILE, JSON.stringify(programs, null, 2), (err: unknown) => {
-    if (err) {
-      res.status(500).send({ error: 'Failed to write to file.' });
-      return;
+  fs.writeFile(
+    PROGRAMS_FILE,
+    JSON.stringify(programs, null, 2),
+    (err: unknown) => {
+      if (err) {
+        res.status(500).send({error: 'Failed to write to file.'});
+        return;
+      }
+      console.log(`Updated ${PROGRAMS_FILE}`);
+      res.status(201).send();
     }
-    console.log(`Updated ${PROGRAMS_FILE}`);
-    res.status(201).send();
-  });
+  );
 });
 
 const port = 4000;
