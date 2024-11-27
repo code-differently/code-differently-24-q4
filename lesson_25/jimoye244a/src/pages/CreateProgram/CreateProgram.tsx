43fcf9dc9a62cab6dc8b8ebe2d28ede17f3480f4
Oyeyemi Jimoh <oyeyemijimoh244@gmail.com>
2024-11-27 09:18:15 -0500

    feat: updated lesson25 homework to fix program page by Yemi

---
+++ lesson_25/jimoye244a/src/pages/CreateProgram/CreateProgram.tsx
@@ -0,0 +1,32 @@
+import './CreateProgram.scss';
+import React from 'react';
+
+export const CreateProgram: React.FC = () => {
+    return (
+
+      <div className="form-container">
+      <h1 className="form-title">Add New Program</h1>
+      
+      <form  className="form">
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea
+            id="description"
+            name="description"
+          ></textarea>
+        </div>
+
+        <button type="submit" className="submit-btn">Submit</button>
+      </form>
+    </div>
+    );
+};
