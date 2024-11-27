e53c3bdef1cf845efb3e2bca4a3f5bf57cf47ba9
marleomac3 <mlmcwilliams244@gmail.com>
2024-11-27 16:51:27 +0000

    feat: Implemented Web API using REST - LJ Lesson 26

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -1,15 +1,26 @@
 package com.codedifferently.lesson26.web;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.codedifferently.lesson26.library.Librarian;
 import com.codedifferently.lesson26.library.Library;
 import com.codedifferently.lesson26.library.MediaItem;
 import com.codedifferently.lesson26.library.search.SearchCriteria;
-import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+
 
 @RestController
 @CrossOrigin
@@ -26,7 +37,49 @@ public class MediaItemsController {
   public GetMediaItemsResponse getItems() {
     Set<MediaItem> items = library.search(SearchCriteria.builder().build());
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
+
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
+  }
+
+  @GetMapping("/items/{id}")
+  public ResponseEntity<MediaItemResponse> getItem(@PathVariable("id") UUID id) {
+    SearchCriteria searchCriteria = SearchCriteria.builder().id(id.toString()).build();
+
+    Set<MediaItem> foundItem = library.search(searchCriteria);
+
+    if (foundItem.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    MediaItem item = foundItem.iterator().next();
+    MediaItemResponse response = MediaItemResponse.from(item);
+
+    return ResponseEntity.ok(response);
+  }
+
+  @DeleteMapping("/items/{id}")
+  public ResponseEntity<Void> deleteItem(@PathVariable("id") UUID id) {
+    SearchCriteria searchCriteria = SearchCriteria.builder().id(id.toString()).build();
+
+    Set<MediaItem> foundItem = library.search(searchCriteria);
+
+    if (foundItem.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    MediaItem item = foundItem.iterator().next();
+    library.removeMediaItem(item, librarian);
+    return ResponseEntity.noContent().build();
+  }
+
+  @PostMapping("/items")
+  public CreateMediaItemResponse addsItem(@Valid @RequestBody CreateMediaItemRequest request) {
+    MediaItemRequest itemRequest = request.getItem();
+    MediaItem item = MediaItemRequest.asMediaItem(itemRequest);
+    library.addMediaItem(item, librarian);
+
+    CreateMediaItemResponse res = CreateMediaItemResponse.builder().item(getItem(item.getId()).getBody()).build();
+    return res;
   }
 }
