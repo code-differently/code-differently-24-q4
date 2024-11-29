9672cd2166592cd83825733752eeb9c922fff222
Anthony D. Mays <anthony@morganlatimer.com>
2024-11-29 05:52:16 +0000

    chore: add initial thymeleaf + tailwind integration

---
+++ lib/java/fullstack_demo/src/main/java/com/codedifferently/fullstack_demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.codedifferently.fullstack_demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
