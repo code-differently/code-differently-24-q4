a5d1cd2a1a9d51db985cbf5bd8fab6228be0d41f
haldanek <khaldane244@gmail.com>
2024-12-01 04:07:40 +0000

    feat: add first draft of HTTP Methods for API

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -7,9 +7,13 @@ import com.codedifferently.lesson26.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @CrossOrigin
@@ -28,5 +32,46 @@ public class MediaItemsController {
     List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
+  }
+
+  @PostMapping("/items")
+  public MediaItemResponse postItem() {
+    //UUID id = UUID.randomUUID();
+    MediaItemRequest newItem = CreateMediaItemRequest.builder()
+          .id(UUID.randomUUID())
+          .type("Book")
+          .isbn("13")
+          .title("The Fall of the House of Usher")
+          .authors(new String[] {"Edgar Allan Poe"})
+          .edition("1st Edition")
+          .pages(25)
+          .build();
+
+    CreateMediaItemRequest createRequest = CreateMediaItemRequest.builder()
+          .item(newItem)
+          .build();
+
+      library.add(createRequest.getItem());
+    
+    return  MediaItemResponse.from(createRequest.getItem());
+  }
+
+  @GetMapping("/items/:{id}")
+  public GetMediaItemsResponse getItemById(@PathVariable("id") String id) {
+    Set<MediaItem> item = library.search(SearchCriteria.builder().id(id).build());
+    MediaItemResponse responseItemById = item.stream().map(MediaItemResponse::from).toList().findFirst().orElseThrow();
+    var response = GetMediaItemsResponse.builder().item(responseItemById).build();
+    return response;
+  }
+
+  @DeleteMapping("/items/:{id}") 
+  public MediaItemResponse deleteItemById(@PathVariable("id") String id) {
+    Set<MediaItem> itemToDelete = library.search(SearchCriteria.builder().id(id).build());
+    //MediaItemRequest deleteItem = CreateMediaItemRequest.builder().build();
+    CreateMediaItemRequest createDeleteRequest = CreateMediaItemRequest.builder().deleteItem(itemToDelete).build();
+
+    library.delete(createDeleteRequest.getItem());
+
+    return MediaItemResponse.from(createDeleteRequest.getItem());
   }
 }
