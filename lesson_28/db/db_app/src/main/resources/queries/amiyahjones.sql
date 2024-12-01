ccd269ee834247434b67991b60be8d7b4ca4db0a
AmiyahJo <ajones244@gmail.com>
2024-12-01 23:01:55 +0000

    feat: adds template select queries

---
+++ lesson_28/db/db_app/src/main/resources/queries/amiyahjones.sql
@@ -0,0 +1,18 @@
+--  returns the counts of media items by type.
+SELECT 
+*
+FROM 
+1;
+
+-- returns the sum of total pages checked out by guests.
+SELECT 
+*
+FROM 
+1;
+
+-- shows all 5 guests 
+-- and any corresponding records in the checked_out_items table
+SELECT 
+*
+FROM 
+1;
