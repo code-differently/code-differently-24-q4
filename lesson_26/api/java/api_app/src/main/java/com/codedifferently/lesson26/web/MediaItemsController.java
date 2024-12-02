136f96736f8167da9e1140f4f949220640a3b222
Dasiame <dmenglish244@gmail.com>
2024-12-02 15:51:56 +0000

    feat:Adds Dasia's implement the MediaItemsController to enable the API to lesson_26

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -29,4 +31,26 @@ public class MediaItemsController {
     var response = GetMediaItemsResponse.builder().items(responseItems).build();
     return response;
   }
+
+  @GetMapping("/items")
+  public GetMediaItemsResponse postItems() {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
+    return postItems();
+  }
+
+  @GetMapping("/items/:id")
+  public GetMediaItemsResponse getItemsId(@PathVariable String id) {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
+    MediaItemResponse responseItem = items.stream().map(MediaItemResponse::from).iterator().next();
+    return GetMediaItemsResponse.builder().item(responseItem).build();
+  }
+
+  @DeleteMapping(value = "{id}")
+  public void deletebById(@PathVariable String id) {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
+    MediaItem item = items.iterator().next();
+    library.removeMediaItem(item, librarian);
+  }
+  
 }
