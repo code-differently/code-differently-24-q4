311ffcda941dbc88af69303c0483cea64501ccbb
Dasiame <dmenglish244@gmail.com>
2024-12-02 16:31:34 +0000

    feat:Adds lesson_26 work

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -4,13 +4,17 @@ import com.codedifferently.lesson26.library.Librarian;
 import com.codedifferently.lesson26.library.Library;
 import com.codedifferently.lesson26.library.MediaItem;
 import com.codedifferently.lesson26.library.search.SearchCriteria;
+import jakarta.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,24 +36,33 @@ public class MediaItemsController {
     return response;
   }
 
-  @GetMapping("/items")
-  public GetMediaItemsResponse postItems() {
-    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
-    List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
-    return postItems();
+  @PostMapping("/items")
+  public ResponseEntity<?> postItems(@Valid @RequestBody CreateMediaItemRequest request) {
+    MediaItemRequest itemRequest = request.getItem();
+    MediaItem item = MediaItemRequest.asMediaItem(itemRequest);
+    library.addMediaItem(item, librarian);
+    MediaItemResponse response = MediaItemResponse.from(item);
+    return ResponseEntity.ok(CreateMediaItemResponse.builder().item(response).build());
   }
 
-  @GetMapping("/items/:id")
-  public GetMediaItemsResponse getItemsId(@PathVariable String id) {
+  @GetMapping("/items/{id}")
+  public ResponseEntity<GetMediaItemsResponse> getItemsId(@PathVariable String id) {
     Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
+    if (items.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
     MediaItemResponse responseItem = items.stream().map(MediaItemResponse::from).iterator().next();
-    return GetMediaItemsResponse.builder().item(responseItem).build();
+    return ResponseEntity.ok(GetMediaItemsResponse.builder().item(responseItem).build());
   }
 
-  @DeleteMapping(value = "{id}")
-  public void deletebById(@PathVariable String id) {
+  @DeleteMapping("/items/{id}")
+  public ResponseEntity<?> deletebById(@PathVariable String id) {
     Set<MediaItem> items = library.search(SearchCriteria.builder().id(id).build());
     MediaItem item = items.iterator().next();
+    if (items.isEmpty() || item == null) {
+      ResponseEntity.notFound().build();
+    }
     library.removeMediaItem(item, librarian);
+    return ResponseEntity.noContent().build();
   }
 }
