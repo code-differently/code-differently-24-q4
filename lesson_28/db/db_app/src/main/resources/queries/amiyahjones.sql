b10ecb9cc8846b6c8803c65abb3121156e394938
AmiyahJo <ajones244@gmail.com>
2024-12-02 15:27:56 +0000

    feat: sql attempt on select queries

--- lesson_28/db/db_app/src/main/resources/queries/amiyahjones.sql
+++ lesson_28/db/db_app/src/main/resources/queries/amiyahjones.sql
@@ -1,18 +1,22 @@
 --  returns the counts of media items by type.
 SELECT 
-*
+  type, COUNT(*) AS item_count
 FROM 
-1;
+  media_items
+GROUP BY 
+  type;
 
 -- returns the sum of total pages checked out by guests.
 SELECT 
-*
+SUM(pages) AS total_pages
 FROM 
-1;
+guests;
 
 -- shows all 5 guests 
 -- and any corresponding records in the checked_out_items table
 SELECT 
-*
+  guests.id , guests.name , ch.item_id , ch.checkout_date
 FROM 
-1;
+guests 
+INNER JOIN checked_out_items ch ON 
+;
