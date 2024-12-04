fd38c244aadf3a76aa1d77435090f4b9b4562fe6
Anthony D. Mays <anthony@morganlatimer.com>
2024-12-04 13:14:00 -0800

    fix: removes unnecessary route tag
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_25/amiyahjones/src/App.tsx
+++ lesson_25/amiyahjones/src/App.tsx
@@ -1,15 +1,13 @@
-import { Outlet, Route } from 'react-router-dom';
 import './App.scss';
+import {Outlet} from 'react-router-dom';
 
-import { Footer } from './components/footer';
-import { Header } from './components/header';
-import { CreateProgram } from './pages/create-program/CreateProgram';
+import {Footer} from './components/footer';
+import {Header} from './components/header';
 
 function App() {
   return (
     <>
       <Header />
-        <Route path='/new' element={<CreateProgram/>} />
       <div className="main">
         <div className="content">
           <Outlet />
