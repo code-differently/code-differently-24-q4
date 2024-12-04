e131de8dfa569662504b837650928621334f9a64
Xavier Cruz <68714224+XavierCruz5106@users.noreply.github.com>
2024-12-04 16:26:59 -0500

    feat: adds program component, adds dynamic rendering of program components (#602)
    * feat: adds unique folder
    
    * feat: made program component
    
    * Merge branch 'main' of https://github.com/XavierCruz5106/code-differently-24-q4 into feature/lesson_25
    
    * feat: adds programs being added dynamically
    
    * Merge branch 'main' of https://github.com/XavierCruz5106/code-differently-24-q4 into feature/lesson_25
    
    * feat: adds programList component
    
    * feat: removes package-lock.json
    
    * chore: refactor code

---
+++ lesson_25/xaviercruz/src/pages/Home/Home.tsx
@@ -0,0 +1,32 @@
+import './Home.scss';
+import React from 'react';
+
+import {ProgramList} from '@/components/programList/ProgramList';
+
+export const Home: React.FC = () => {
+  return (
+    <article>
+      <section className="hero-section">
+        <div className="hero-overlay"></div>
+        <div className="hero-content">
+          <h2 className="hero-title">
+            Together we can move the needle of{' '}
+            <em className="highlight">diversity in tech.</em>
+          </h2>
+          <div className="hero-text">
+            <span>Code Differently</span> provides hands on training and
+            education through coding classes that gives participants the
+            technical and cognitive skills they need to excel in
+            technology-driven workplaces.
+          </div>
+        </div>
+      </section>
+      <section className="programs-section">
+        <h2>
+          Our <em className="highlight">Programs</em>
+        </h2>
+        <ProgramList />
+      </section>
+    </article>
+  );
+};
