8cb11b39ad2bf01e357523dfb6422de290fc2ea2
Josephcabs <jxcaballero244@gmail.com>
2024-12-05 01:00:06 -0500

    feat: modifies lesson_27/josephs_template to implement the api given to us. made use of the get request and the post request already made for us. Lesson_27 -JosephCaballero (#637)
    * feat: implements the programs api by modifying files inside of josephs_template directory. lesson_27 -JosephCaballero
    
    * fead: adds package.json and makes changes to ProgramList.tsx. Lesson_27 -JosephCaballero
    
    * Merge branch 'main' of https://github.com/Josephcabs/code-differently-24-q4 into feature/lesson_27
    
    * Merge branch 'main' of https://github.com/Josephcabs/code-differently-24-q4 into feature/lesson_27

---
+++ lesson_27/josephs_template/src/pages/Home/components/Program/Program.tsx
@@ -0,0 +1,19 @@
+import './Program.scss';
+import React from 'react';
+
+export interface ProgramProps {
+  title: string;
+  description: string;
+}
+
+export const Program: React.FC<ProgramProps> = ({
+  title,
+  description,
+}: ProgramProps) => {
+  return (
+    <li className="program">
+      <h3>{title}</h3>
+      {description}
+    </li>
+  );
+};
