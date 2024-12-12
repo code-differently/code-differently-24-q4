e14a10b355901d33066dabcf393f0fa66b70339c
AmiyahJo <ajones244@gmail.com>
2024-12-12 20:45:23 +0000

    feat: adds library user model
    refrenced off of the guest model

---
+++ lesson_28/db/db_app/src/main/java/com/codedifferently/lesson28/models/LibraryUserModel.java
@@ -0,0 +1,13 @@
+package com.codedifferently.lesson28.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class LibraryUserModel {
+    public String first_name;
+    public String last_name;
+    @Id public String email;
+}
