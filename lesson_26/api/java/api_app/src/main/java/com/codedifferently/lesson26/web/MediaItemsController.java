95c39defccd3db54d611631097ee4d7f9eb36eb7
Anthony D. Mays <anthony@morganlatimer.com>
2024-12-14 03:49:08 +0000

    chore: apply spotless

--- lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
+++ lesson_26/api/java/api_app/src/main/java/com/codedifferently/lesson26/web/MediaItemsController.java
@@ -1,36 +1,34 @@
 package com.codedifferently.lesson26.web;
 
+import com.codedifferently.lesson26.library.Librarian;
+import com.codedifferently.lesson26.library.Library;
+import com.codedifferently.lesson26.library.MediaItem;
+import com.codedifferently.lesson26.library.search.SearchCriteria;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codedifferently.lesson26.library.Librarian;
-import com.codedifferently.lesson26.library.Library;
-import com.codedifferently.lesson26.library.MediaItem;
-import com.codedifferently.lesson26.library.search.SearchCriteria;
-
 @RestController
 @CrossOrigin
 public class MediaItemsController {
 
-    private final Library library;
-    private final Librarian librarian;
+  private final Library library;
+  private final Librarian librarian;
 
-    public MediaItemsController(Library library) throws IOException {
-        this.library = library;
-        this.librarian = library.getLibrarians().stream().findFirst().orElseThrow();
-    }
+  public MediaItemsController(Library library) throws IOException {
+    this.library = library;
+    this.librarian = library.getLibrarians().stream().findFirst().orElseThrow();
+  }
 
-    @GetMapping("/items")
-    public ResponseEntity<GetMediaItemsResponse> getItems() {
-        Set<MediaItem> items = library.search(SearchCriteria.builder().build());
-        List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
-        var response = GetMediaItemsResponse.builder().items(responseItems).build();
-        return ResponseEntity.ok(response);
-    }
+  @GetMapping("/items")
+  public ResponseEntity<GetMediaItemsResponse> getItems() {
+    Set<MediaItem> items = library.search(SearchCriteria.builder().build());
+    List<MediaItemResponse> responseItems = items.stream().map(MediaItemResponse::from).toList();
+    var response = GetMediaItemsResponse.builder().items(responseItems).build();
+    return ResponseEntity.ok(response);
+  }
 }
