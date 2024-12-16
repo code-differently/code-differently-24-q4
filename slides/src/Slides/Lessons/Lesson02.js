8b1646ecb7559283696246707ba06d1c8b56f558
Anthony D. Mays <anthony@morganlatimer.com>
2024-12-15 18:38:09 -0800

    chore: refactor lessons into separate modules
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

---
+++ slides/src/Slides/Lessons/Lesson02.js
@@ -0,0 +1,73 @@
+import htm from "htm";
+import { createElement } from "react";
+import { Lesson } from "../Layouts/index.js";
+
+const html = htm.bind(createElement);
+
+function Lesson02() {
+    return html`
+        <${Lesson} title="Exploring the Terminal" lessonId="lesson_02" subtitle="Lesson 2">
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide43.png" alt="Slide 43" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide44.png" alt="Slide 44" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide45.png" alt="Slide 45" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide46.png" alt="Slide 46" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide47.png" alt="Slide 47" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide48.png" alt="Slide 48" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide49.png" alt="Slide 49" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide50.png" alt="Slide 50" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide51.png" alt="Slide 51" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide52.png" alt="Slide 52" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide53.png" alt="Slide 53" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide54.png" alt="Slide 54" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide55.png" alt="Slide 55" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide56.png" alt="Slide 56" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide57.png" alt="Slide 57" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide58.png" alt="Slide 58" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide59.png" alt="Slide 59" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide60.png" alt="Slide 60" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide61.png" alt="Slide 61" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide62.png" alt="Slide 62" />
+            </section>
+        <//>`;
+}
+
+export default Lesson02;
---
+++ slides/src/Slides/Lessons/Lesson05.js
@@ -0,0 +1,112 @@
+import htm from "htm";
+import { createElement } from "react";
+import { Lesson } from "../Layouts/index.js";
+
+const html = htm.bind(createElement);
+
+function Lesson05() {
+    return html`
+        <${Lesson} title="Software Dev Life Cycle" lessonId="lesson_05" subtitle="Lesson 5">
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide108.png" alt="Slide 108" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide109.png" alt="Slide 109" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide110.png" alt="Slide 110" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide111.png" alt="Slide 111" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide112.png" alt="Slide 112" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide113.png" alt="Slide 113" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide114.png" alt="Slide 114" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide115.png" alt="Slide 115" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide116.png" alt="Slide 116" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide117.png" alt="Slide 117" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide118.png" alt="Slide 118" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide119.png" alt="Slide 119" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide120.png" alt="Slide 120" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide121.png" alt="Slide 121" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide122.png" alt="Slide 122" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide123.png" alt="Slide 123" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide124.png" alt="Slide 124" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide125.png" alt="Slide 125" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide126.png" alt="Slide 126" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide127.png" alt="Slide 127" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide128.png" alt="Slide 128" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide129.png" alt="Slide 129" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide130.png" alt="Slide 130" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide131.png" alt="Slide 131" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide132.png" alt="Slide 132" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide133.png" alt="Slide 133" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide134.png" alt="Slide 134" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide135.png" alt="Slide 135" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide136.png" alt="Slide 136" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide137.png" alt="Slide 137" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide138.png" alt="Slide 138" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide139.png" alt="Slide 139" />
+            </section>
+            <section>
+                <img class="r-stretch" src="images/24q4-slides/Slide140.png" alt="Slide 140" />
+            </section>
+        <//>`;
+}
+
+export default Lesson05;
