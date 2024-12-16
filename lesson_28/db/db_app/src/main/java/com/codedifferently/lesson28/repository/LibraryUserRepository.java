76c4dafcb95d0c1c5c465d56bcb392d5ea4bda7e
AmiyahJo <ajones244@gmail.com>
2024-12-16 14:07:31 +0000

    feat : adds repository interface definition
    the code inside of the function

--- lesson_28/db/db_app/src/main/java/com/codedifferently/lesson28/repository/LibraryUserRepository.java
+++ lesson_28/db/db_app/src/main/java/com/codedifferently/lesson28/repository/LibraryUserRepository.java
@@ -1,5 +1,12 @@
 package com.codedifferently.lesson28.repository;
 
-public class LibraryUserRepository {
-    
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codedifferently.lesson28.models.LibraryUserModel;
+
+public interface LibraryUserRepository extends CrudRepository<LibraryUserModel, String>{
+  @Override
+  List<LibraryUserModel> findAll();
 }
