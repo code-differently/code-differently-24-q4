cda562016636239ee2e979fcacda8fc68e14f29e
Anthony D. Mays <anthony@morganlatimer.com>
2024-12-18 14:07:02 -0800

    fixes jsdom error
    Signed-off-by: Anthony D. Mays <anthony@morganlatimer.com>

--- lesson_27/template/jest.config.js
+++ lesson_27/template/jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-fixed-jsdom',
   transform: {
     '^.+.tsx?$': ['ts-jest', {useESM: true}],
   },
