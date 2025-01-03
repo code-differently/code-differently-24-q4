fe92f7f1477482b603221c2d97f3a75f20430079
Anthony D. Mays <anthony@morganlatimer.com>
2025-01-03 12:17:36 -0800

    feat: adds postgres repo

--- lib/javascript/fullstack_demo/src/repositories/index.ts
+++ lib/javascript/fullstack_demo/src/repositories/index.ts
@@ -1,12 +1,16 @@
 export type { TodoRepository } from './todo-repository';
 
 import { JsonTodoRespository } from './json-todo-repository';
+import { PostgresTodoRepository } from './postgres-todo-repository';
 import { RedisTodoRepository } from './redis-todo-repository';
 import { TodoRepository } from './todo-repository';
 
 export function createTodoRepository(): TodoRepository {
   if (process.env.DB_TYPE?.toUpperCase() === 'REDIS') {
     return new RedisTodoRepository();
+  }
+  if (process.env.DB_TYPE?.toUpperCase() === 'POSTGRES') {
+    return new PostgresTodoRepository();
   }
   return new JsonTodoRespository();
 }
