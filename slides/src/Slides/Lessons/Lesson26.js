64ad9acb3cc178d844c5434d6e3a0014efa43a8b
Anthony D. Mays <anthony@morganlatimer.com>
2025-01-10 08:24:20 -0800

    wip: replace lesson 26 slides

--- slides/src/Slides/Lessons/Lesson26.js
+++ slides/src/Slides/Lessons/Lesson26.js
@@ -8,49 +8,57 @@ function Lesson26() {
     return html`
         <${Lesson} title="Web APIs using REST" lessonId="lesson_26" subtitle="Lesson 26">
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide834.png" alt="Slide 834" />
+                <p>Now that we’ve spent some time on the <em>frontend</em>, it’s time to switch our focus to the <em>backend</em></p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide835.png" alt="Slide 835" />
+                <p>But first, we need to discuss <em>APIs</em></p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide836.png" alt="Slide 836" />
+                <p>In modern web applications, we break up into programs that handle what the user sees (frontend)...</p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide837.png" alt="Slide 837" />
+                <p>…and programs that handle interacting with data and business logic (backend)</p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide838.png" alt="Slide 838" />
+                <p>An <em>API</em> (Application Programming Interface) is what allows these programs to talk to each other</p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide839.png" alt="Slide 839" />
+                <p>Most APIs rely on the <i>protocol</i> of the web, <em>HTTP</em></p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide840.png" alt="Slide 840" />
+                <p>A <em>client</em> will make a request to an <em>API server</em> and wait for a response containing JSON, XML, CSV, or some other data interchange format</p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide841.png" alt="Slide 841" />
+                <p>This style of API is known as a <em>REST</em><span style=${{color: "red", "font-weight": "bold"}}>*</span> (Representational State Transfer)</p>
             </section>
             <section>
                 <img class="r-stretch" src="images/24q4-slides/Slide842.png" alt="Slide 842" />
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide843.png" alt="Slide 843" />
+                <p><span style=${{color: "red", "font-weight": "bold"}}>Note:</span> Not really, hahahah. It’s really REST-like RPC, but that’s beyond the scope of this course</p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide844.png" alt="Slide 844" />
+                <p>In order to build a REST API, you need the following elements…</p>
+            </section>
+            <section class="ml-bullet-slide">
+                <h3>REST API elements</h3>
+                <ul>
+                    <li class="fragment">A URL scheme that identifies resources the API provides</li>
+                    <li class="fragment">A set of HTTP methods you can conduct on each resource</li>
+                    <li class="fragment">A consistent way to represent the resource (e.g. JSON, XML, CSV, etc)</li>
+                </ul>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide845.png" alt="Slide 845" />
+                <p>Suppose that we want to build an API for library books</p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide846.png" alt="Slide 846" />
+                <p>
+                    On the web, we might host an API server that exposes books at the following url:<br />
+                    http://codedifferently.com<em>/api/books</em>
+                </p>
             </section>
             <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide847.png" alt="Slide 847" />
-            </section>
-            <section>
-                <img class="r-stretch" src="images/24q4-slides/Slide848.png" alt="Slide 848" />
+                <p>We might then support the following operations…</p>
             </section>
             <section>
                 <img class="r-stretch" src="images/24q4-slides/Slide849.png" alt="Slide 849" />
